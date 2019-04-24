55ac68f77a65d44ac94effe992073cbe0feced3b
John Andersen <johnandersenpdx@gmail.com>
2019-04-23 20:05:30 -0700

    service: dfass: Websocket proxy
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- service/dfass/tests/test_dfass.py
+++ service/dfass/tests/test_dfass.py
@@ -9,6 +9,7 @@ from dffml.util.asynctestcase import AsyncTestCase
 from aiohttp import web
 from aiohttp import client
 from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
+import aiohttp.client_exceptions
 import aiohttp
 import asyncio
 
@@ -89,31 +90,23 @@ class ReverseProxyHandlerContext(object):
         if not upstream:
             return web.Response(status=HTTPStatus.NOT_FOUND)
 
-        if 'connection' in headers \
-                and 'upgrade' in headers \
-                and headers['connection'] == 'Upgrade' \
-                and headers['upgrade'] == 'websocket' and req.method == 'GET':
+        if headers.get('connection', '').lower() == 'upgrade' \
+                and headers.get('upgrade', '').lower() == 'websocket' \
+                and req.method == 'GET':
             # Handle websocket proxy
-            ws_server = web.WebSocketResponse()
-            await ws_server.prepare(req)
-            self.logger.info('##### WS_SERVER %s', ws_server)
-
-            client_session = aiohttp.ClientSession(cookies=req.cookies)
-            async with client_session.ws_connect(
-              baseUrl+req.path_qs,
-            ) as ws_client:
-              self.logger.info('##### WS_CLIENT %s', ws_client)
-
-              coro = asyncio.wait([
-                      wsforward(ws_server,ws_client),
-                      wsforward(ws_client,ws_server)
-                  ],
-                  return_when=asyncio.FIRST_COMPLETED)
-              # TODO replace with
-              # https://aiohttp.readthedocs.io/en/stable/web_advanced.html#background-tasks
-              finished,unfinished = await coro
-
-              return ws_server
+            try:
+                async with aiohttp.ClientSession(cookies=req.cookies) as client_session:
+                    async with client_session.ws_connect(upstream) as ws_client:
+                        ws_server = web.WebSocketResponse()
+                        await ws_server.prepare(req)
+                        self.loop.create_task(asyncio.wait([
+                                self.wsforward(ws_server, ws_client),
+                                self.wsforward(ws_client, ws_server)
+                            ],
+                            return_when=asyncio.FIRST_COMPLETED))
+                        return ws_server
+            except aiohttp.client_exceptions.WSServerHandshakeError:
+                return web.Response(status=HTTPStatus.NOT_FOUND)
         else:
             # Handle regular HTTP request proxy
             self.logger.debug('upstream for (%r): %s', upstream,
@@ -164,6 +157,7 @@ class ReverseProxyHandlerContext(object):
         self.app.router.add_route('*', '/{path:.*}', self.handler_proxy)
         self.runner = web.AppRunner(self.app)
         await self.runner.setup()
+        self.loop = asyncio.get_event_loop()
         self.site = web.TCPSite(self.runner, self.address, self.port)
         await self.site.start()
         self.address, self.port = self.site._server.sockets[0].getsockname()
@@ -203,11 +197,33 @@ class TestReverseProxyHandler(AioHTTPTestCase):
     PROXY_PATH = '/route/this'
     UPSTREAM_PATH = '/to/here'
 
-    async def get_application(self):
-        async def hello(request):
+    async def handler(self, request):
+        headers = request.headers
+        if headers.get('connection', '').lower() == 'upgrade' \
+                and headers.get('upgrade', '').lower() == 'websocket' \
+                and request.method == 'GET':
+            ws = web.WebSocketResponse()
+            await ws.prepare(request)
+
+            async for msg in ws:
+                if msg.type == aiohttp.WSMsgType.TEXT:
+                    await ws.send_str(msg.data)
+                elif msg.type == aiohttp.WSMsgType.BINARY:
+                    await ws.send_bytes(msg.data)
+                elif msg.type == aiohttp.WSMsgType.PING:
+                    await ws.ping()
+                elif msg.type == aiohttp.WSMsgType.PONG:
+                    await ws.pong()
+                elif ws.closed:
+                    await ws.close(code=ws.close_code, message=msg.extra)
+
+            return ws
+        else:
             return web.Response(text=request.path)
+
+    async def get_application(self):
         app = web.Application()
-        app.router.add_get(self.UPSTREAM_PATH + '{path:.*}', hello)
+        app.router.add_get(self.UPSTREAM_PATH + '{path:.*}', self.handler)
         return app
 
     @unittest_run_loop
@@ -261,3 +277,25 @@ class TestReverseProxyHandler(AioHTTPTestCase):
                 self.assertEqual(resp.status, HTTPStatus.OK)
                 text = await resp.text()
                 self.assertEqual(self.UPSTREAM_PATH + '/test/joined', text)
+
+    @unittest_run_loop
+    async def test_websocket(self):
+        async with rproxy(self,
+                          self.UPSTREAM_PATH,
+                          subdomain=self.PROXY_SUBDOMAIN,
+                          path=self.PROXY_PATH) as rctx, \
+                aiohttp.ClientSession() as session:
+            url = 'http://%s.%s:%d%s' % \
+                  (self.PROXY_SUBDOMAIN,
+                   self.TEST_ADDRESS,
+                   rctx.port,
+                   self.PROXY_PATH,)
+            LOGGER.debug('rproxy url: %s', url)
+            async with aiohttp.ClientSession() as client_session:
+                async with client_session.ws_connect(url) as ws:
+                    await ws.send_str(self.UPSTREAM_PATH + '/test/joined')
+                    async for msg in ws:
+                        text = msg.data
+                        self.assertEqual(self.UPSTREAM_PATH + '/test/joined',
+                                         text)
+                        await ws.close()
