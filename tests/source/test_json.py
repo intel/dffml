fffca857ae2c21bd86fdc1eab28301aa0e20e858
John Andersen <john.s.andersen@intel.com>
2019-06-03 16:26:57 -0700

    util: testing: Add FileSourceTest
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- tests/source/test_json.py
+++ tests/source/test_json.py
@@ -2,17 +2,11 @@
 # Copyright (c) 2019 Intel Corporation
 from dffml.source.file import FileSourceConfig
 from dffml.source.json import JSONSource
-from dffml.util.testing.source import SourceTest
+from dffml.util.testing.source import FileSourceTest
 from dffml.util.asynctestcase import AsyncTestCase
 
 
-class TestJSONSource(SourceTest, AsyncTestCase):
+class TestJSONSource(FileSourceTest, AsyncTestCase):
 
-    SOURCE = JSONSource
-
-    async def setUpFile(self, fileobj):
-        fileobj.write(b"{}")
-        fileobj.seek(0)
-
-    async def setUpSource(self, fileobj):
-        return JSONSource(FileSourceConfig(filename=fileobj.name))
+    async def setUpSource(self):
+        return JSONSource(FileSourceConfig(filename=self.testfile))
