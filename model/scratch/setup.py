1c2a26a75d3b9a3f7071ef8ae72cfc580f799833
Yash Lamba <yashlamba2000@gmail.com>
2019-06-11 14:54:36 +0530

    Added Linear Regression Model

---
+++ model/scratch/setup.py
@@ -0,0 +1,58 @@
+import os
+import ast
+from io import open
+
+from setuptools import find_packages, setup
+
+self_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(self_path, 'dffml_model_scratch', 'version.py'),
+          'r') as f:
+    for line in f:
+        if line.startswith('VERSION'):
+            version = ast.literal_eval(line.strip().split('=')[-1].strip())
+            break
+
+with open(os.path.join(self_path, 'README.md'), 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+INSTALL_REQUIRES = [
+    ]
+
+setup(
+    name='dffml-model-scratch',
+    version=version,
+    description='',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='John Andersen',
+    author_email='john.s.andersen@intel.com',
+    url='https://github.com/intel/dffml/blob/master/model/scratch/README.md',
+    license='MIT',
+
+    keywords=[
+        '',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    install_requires=INSTALL_REQUIRES,
+
+    packages=find_packages(),
+    entry_points={
+        'dffml.model': [
+            'dnn = dffml_model_scratch.model.misc:Misc',
+        ],
+    },
+)
