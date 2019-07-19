9f28c0e38a166a03a583171690e7c8ff3a475070
Yash Lamba <yashlamba2000@gmail.com>
2019-07-19 09:11:06 +0530

    Implemented linear regression context

--- model/scikit/dffml_model_scikit/sciLR.py
+++ model/scikit/dffml_model_scikit/sciLR.py
@@ -5,8 +5,11 @@ Description of what this model does
 """
 import os
 import abc
+import hashlib
 import numpy as np
-from sklearn import preprocessing
+import pandas as pd
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from typing import AsyncIterator, Tuple, Any, List, Optional, NamedTuple, Dict
 
@@ -31,6 +34,9 @@ class LRContext(ModelContext):
         self.xData = np.array([])
         self.yData = np.array([])
         self.features = self.applicable_features(features)
+        self._features_hash_ = hashlib.sha384((''.join(sorted(self.features))).encode()).hexdigest()
+        self.clf = None
+        self.confidence = None
 
     def applicable_features(self, featuress):
         usable = []
@@ -43,29 +49,32 @@ class LRContext(ModelContext):
         return usable
 
     async def train(self, sources: Sources):
+        data = []
         async for repo in sources.with_features(self.features):
             feature_data = repo.features(self.features
                                          + [self.parent.config.predict])
-            print(feature_data)
+            data.append(feature_data)
+        df = pd.DataFrame(data)
+        xData = np.array(df.drop([self.parent.config.predict], 1))
+        yData = np.array(df[self.parent.config.predict])
+        X_train, X_test, y_train, y_test = train_test_split(xData, yData, test_size=0.2)
+        self.clf = LinearRegression(n_jobs=-1)
+        self.clf.fit(X_train, y_train)
+        self.confidence = self.clf.score(X_test, y_test)
 
     async def accuracy(self, sources: Sources) -> Accuracy:
-        """
-        Evaluates the accuracy of our model after training using the input repos
-        as test data.
-        """
-        # Lies
-        return 1.0
+        if self.confidence is None:
+            raise ValueError('Model Not Trained')
+        return self.confidence
 
-    async def predict(
-        self, repos: AsyncIterator[Repo]
-    ) -> AsyncIterator[Tuple[Repo, Any, float]]:
-        """
-        Uses trained data to make a prediction about the quality of a repo.
-        """
+    async def predict(self, repos: AsyncIterator[Repo]) -> \
+                    AsyncIterator[Tuple[Repo, Any, float]]:
         async for repo in repos:
-            yield repo, self.parent.config.classifications[
-                repo.feature(self.features.names()[0])
-            ], 1.0
+            feature_data = repo.features(self.features)
+            df = pd.DataFrame(feature_data, index=[1])
+            predict = np.array(df)
+            print(self.clf.predict(predict))
+            yield repo, self.clf.predict(predict), self.confidence
 
 
 @entry_point("sciLR")
