a8db9eb66cae375b152bfcaaa5b4e8e0d6916dc9
Yash Lamba <yashlamba2000@gmail.com>
2019-07-20 21:20:21 +0530

    Implemented saving and loading

--- model/scikit/dffml_model_scikit/sciLR.py
+++ model/scikit/dffml_model_scikit/sciLR.py
@@ -11,6 +11,7 @@ import pandas as pd
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.externals import joblib
 from typing import AsyncIterator, Tuple, Any, List, Optional, NamedTuple, Dict
 
 from dffml.repo import Repo
@@ -25,6 +26,7 @@ from dffml.util.cli.arg import Arg
 class LRConfig(ModelConfig, NamedTuple):
     directory: str
     predict: str
+    # scale: bool
 
 
 class LRContext(ModelContext):
@@ -34,9 +36,23 @@ class LRContext(ModelContext):
         self.xData = np.array([])
         self.yData = np.array([])
         self.features = self.applicable_features(features)
-        self._features_hash_ = hashlib.sha384((''.join(sorted(self.features))).encode()).hexdigest()
-        self.clf = None
         self.confidence = None
+        self.clf = None
+
+    def _filename(self):
+        return os.path.join(
+            self.parent.config.directory,
+            hashlib.sha384(self.parent.config.predict.encode()).hexdigest() + '.joblib')
+
+    async def __aenter__(self):
+        if(os.path.isfile(self._filename())):
+            self.clf = joblib.load(self._filename())
+        else:
+            self.clf = LinearRegression(n_jobs=-1)
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        joblib.dump(self.clf, self._filename())
 
     def applicable_features(self, featuress):
         usable = []
@@ -46,7 +62,7 @@ class LRContext(ModelContext):
             if feature.length() != 1:
                 raise ValueError("Linear Regression only supports single values (non-matrix / array)")
             usable.append(feature.NAME)
-        return usable
+        return sorted(usable)
 
     async def train(self, sources: Sources):
         data = []
@@ -56,24 +72,35 @@ class LRContext(ModelContext):
             data.append(feature_data)
         df = pd.DataFrame(data)
         xData = np.array(df.drop([self.parent.config.predict], 1))
+        # xData = preprocessing.scale(xData)
         yData = np.array(df[self.parent.config.predict])
-        X_train, X_test, y_train, y_test = train_test_split(xData, yData, test_size=0.2)
-        self.clf = LinearRegression(n_jobs=-1)
-        self.clf.fit(X_train, y_train)
-        self.confidence = self.clf.score(X_test, y_test)
+        self.clf.fit(xData, yData)
+        joblib.dump(self.clf, self._filename())
 
     async def accuracy(self, sources: Sources) -> Accuracy:
-        if self.confidence is None:
+        if(os.path.isfile(self._filename())):
+            data = []
+            async for repo in sources.with_features(self.features):
+                feature_data = repo.features(self.features
+                                         + [self.parent.config.predict])
+                data.append(feature_data)
+            df = pd.DataFrame(data)
+            xData = np.array(df.drop([self.parent.config.predict], 1))
+            # xData = preprocessing.scale(xData)
+            yData = np.array(df[self.parent.config.predict])
+            self.confidence = self.clf.score(xData, yData)
+        else:
             raise ValueError('Model Not Trained')
         return self.confidence
 
     async def predict(self, repos: AsyncIterator[Repo]) -> \
                     AsyncIterator[Tuple[Repo, Any, float]]:
+        if self.confidence is None:
+            raise ValueError('Model Not Trained')
         async for repo in repos:
             feature_data = repo.features(self.features)
-            df = pd.DataFrame(feature_data, index=[1])
+            df = pd.DataFrame(feature_data, index=[0])
             predict = np.array(df)
-            print(self.clf.predict(predict))
             yield repo, self.clf.predict(predict), self.confidence
 
 
@@ -87,10 +114,10 @@ class LR(Model):
         cls.config_set(
             args,
             above,
-            'directory',
+            "directory",
             Arg(
                 default=os.path.join(
-                    os.path.expanduser('~'), '.cache', 'dffml', 'scikit'
+                    os.path.expanduser("~"), ".cache", "dffml", "scikit"
                 ),
                 help="Directory where state should be saved",
             ),
@@ -98,17 +125,28 @@ class LR(Model):
         cls.config_set(
             args,
             above,
-            'predict',
+            "predict",
             Arg(
                 type=str,
                 help="Label or the value to be predicted",
             ),
         )
-        return args
+        # cls.config_set(
+        #     args,
+        #     above,
+        #     "scale",
+        #     Arg(
+        #         type=bool,
+        #         action="store_true",
+        #         help="Enable dataset scaling"
+        #     ),
+        # )
+        # return args
 
     @classmethod
-    def config(cls, config, *above) -> 'LRConfig':
+    def config(cls, config, *above) -> "LRConfig":
         return LRConfig(
-            directory=cls.config_get(config, above, 'directory'),
-            predict=cls.config_get(config, above, 'predict'),
+            directory=cls.config_get(config, above, "directory"),
+            predict=cls.config_get(config, above, "predict"),
+            # scale=cls.config_get(config, above, "scale"),
         )
