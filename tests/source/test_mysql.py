3897fc3859977b46abce86915ec1963ad3bbc247
sudharsana-kjl <kjlsudharsana@gmail.com>
2019-08-01 21:52:39 +0530

    change port in test_mysql

--- tests/source/test_mysql.py
+++ tests/source/test_mysql.py
@@ -21,7 +21,7 @@ CREATE TABLE `repo_data` (
 
 MYSQL_SOURCE_CONFIG = MysqlSourceConfig(
     host="127.0.0.1",
-    port=3306,
+    port=443,
     user="root",
     password="",
     db="dffml_source",
