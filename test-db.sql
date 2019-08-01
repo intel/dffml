57362f74c3fc8b39e31062548f5cf3abb46c1276
sudharsana-kjl <kjlsudharsana@gmail.com>
2019-08-01 20:44:49 +0530

    add mysql test-db for travis

---
+++ test-db.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS `dffml_source` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `dffml_source`;
+
+DROP TABLE IF EXISTS `repo_data`;
+
+SET character_set_client = utf8mb4 ;
+
+CREATE TABLE `repo_data` (
+  `src_url` varchar(100) NOT NULL,
+  `feature_PetalLength` float DEFAULT NULL,
+  `feature_PetalWidth` float DEFAULT NULL,
+  `feature_SepalLength` float DEFAULT NULL,
+  `feature_SepalWidth` float DEFAULT NULL,
+  `prediction_confidence` float DEFAULT NULL,
+  `prediction_value` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`src_url`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+LOCK TABLES `repo_data` WRITE;
+
+INSERT INTO `repo_data` VALUES ('\'0\'',3.9,1.2,5.8,2.7,0.42,'\'feedface\''),('\'1\'',3.9,1.2,5.8,2.7,0,'\'\'');
+
+UNLOCK TABLES;
+
