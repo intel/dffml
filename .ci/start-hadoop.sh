0bcca2fe4327752d76e33bff21784e3128d0040f
sudharsana-kjl <kjlsudharsana@gmail.com>
2019-08-09 22:13:43 +0530

    dockerfile for hadoop setup

---
+++ .ci/start-hadoop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ 
+# start ssh server
+/etc/init.d/ssh start
+ 
+# format namenode
+$HADOOP_HOME/bin/hdfs namenode -format
+ 
+# start hadoop
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+ 
+# keep container running
+tail -f /dev/null
