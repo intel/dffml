67c139b9f40a31b38f35dac70258ee4a0184386e
John Andersen <john.s.andersen@intel.com>
2019-08-09 16:02:38 -0700

    mysql is being a fucker: line 54: /docker-entrypoint-initdb.d/dump.sql: Permission denied
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- source/mysql/setup.py
+++ source/mysql/setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 from dffml_setup_common import SETUP_KWARGS, IMPORT_NAME
 
+SETUP_KWARGS["tests_require"] = [
+    "docker>=4.0.2"
+    ]
 SETUP_KWARGS["entry_points"] = {
     "dffml.service.cli": [f"misc = {IMPORT_NAME}.misc:Misc"]
 }
--- source/mysql/tests/test_source.py
+++ source/mysql/tests/test_source.py
@@ -9,8 +9,13 @@ from dffml.util.asynctestcase import AsyncTestCase
 
 from dffml_source_mysql.source import MysqlSourceConfig, MysqlSource
 
-DROP_TABLE_QUERY = "DROP TABLE IF EXISTS `repo_data`;"
-CREATE_TABLE_QUERY = """
+from .test_docker import DockerTestCase, DOCKER_ENV
+
+
+class TestMySQLSource(SourceTest, DockerTestCase, AsyncTestCase):
+
+    SQL_SETUP = """
+DROP TABLE IF EXISTS `repo_data`;
 CREATE TABLE `repo_data` (
   `src_url` varchar(100) NOT NULL,
   `feature_PetalLength` float DEFAULT NULL,
@@ -23,26 +28,13 @@ CREATE TABLE `repo_data` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 """
 
-
-class TestMySQLSource(SourceTest, AsyncTestCase):
     async def setUp(self):
-        self.container_name = str(random.random()).replace('.', '')
-        subprocess.check_output(["docker", "run", "--rm", "-d", "--name",
-            self.container_name,
-            "-e", "MYSQL_RANDOM_ROOT_PASSWORD=yes",
-            "-e", "MYSQL_USER=user",
-            "-e", "MYSQL_PASSWORD=pass",
-            "-e", "MYSQL_DATABASE=db",
-            "mariadb"])
-        info = json.loads(subprocess.check_output(["docker", "inspect",
-            self.container_name]))[0]
-        container_ip = info["NetworkSettings"]["IPAddress"]
         self.source_config = MysqlSourceConfig(
-            host=container_ip,
+            host=self.container_ip,
             port=3306,
-            user="user",
-            password="pass",
-            db="db",
+            user=DOCKER_ENV["MYSQL_USER"],
+            password=DOCKER_ENV["MYSQL_PASSWORD"],
+            db=DOCKER_ENV["MYSQL_DATABASE"],
             repo_query="select * from repo_data where src_url=%s",
             update_query="""insert into repo_data (`src_url`, `feature_PetalLength`, `feature_PetalWidth`, `feature_SepalLength`, `feature_SepalWidth`, `prediction_confidence`, `prediction_value`) values (%s,%s,%s,%s,%s,%s,%s) on duplicate key update src_url=%s, feature_PetalLength=%s, feature_PetalWidth=%s, feature_SepalLength=%s, feature_SepalWidth=%s, prediction_confidence=%s, prediction_value=%s""",
             repos_query="select * from repo_data",
