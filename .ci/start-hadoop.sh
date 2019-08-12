e236c248d82df420027ecdffa78dcefbdf012358
sudharsana-kjl <kjlsudharsana@gmail.com>
2019-08-12 09:15:01 +0530

    another approach to setup hadoop

--- .ci/start-hadoop.sh
+++ .ci/start-hadoop.sh
@@ -9,7 +9,7 @@ $HADOOP_HOME/bin/hdfs namenode -format
 # start hadoop
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
-$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+#$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
  
 # keep container running
 tail -f /dev/null
