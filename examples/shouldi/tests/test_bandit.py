fd3f4971719a4d2231d2a1164653ec178657473f
Arvindh Kumar Chandran <arvindhium@gmail.com>
2019-08-16 23:14:04 -0700

    Moved the bandit code to be as part of the safety package in examples

--- examples/shouldi/tests/test_bandit.py
+++ examples/shouldi/tests/test_bandit.py
@@ -1,15 +1,13 @@
 from dffml.df.base import BaseConfig
 from dffml.util.asynctestcase import AsyncTestCase
 
-from dffml_feature_git.feature.operations import run_bandit
+from shouldi.bandit import run_bandit
 import os
 
 
 class TestRunBanditOp(AsyncTestCase):
     async def test_run(self):
-        async with run_bandit.imp(
-            BaseConfig()
-        ) as bandit_latest:
+        async with run_bandit.imp(BaseConfig()) as bandit_latest:
             async with bandit_latest(None, None) as ctx:
                 results = await ctx.run({"repo": {
                     "URL": "dummy",
