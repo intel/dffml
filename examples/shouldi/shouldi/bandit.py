4c01173c74726839f520e0712157b31a055ea7fa
Arvindh Kumar Chandran <arvindhium@gmail.com>
2019-08-20 21:41:08 -0700

    Reformatted the file using black

--- examples/shouldi/shouldi/bandit.py
+++ examples/shouldi/shouldi/bandit.py
@@ -12,25 +12,22 @@ from dffml.df.base import op
 git_repository = Definition(name="git_repository", primitive="Dict[str,str]")
 bandit_output = Definition(name="bandit_output", primitive="Dict[str, Any]")
 
-@op(inputs={
-        'repo': git_repository
-        },
-    outputs={
-        'report': bandit_output
-        },
-)
+
+@op(inputs={"repo": git_repository}, outputs={"report": bandit_output})
 async def run_bandit(repo: Dict[str, str]) -> Dict[str, Any]:
     proc = await asyncio.create_subprocess_exec(
         sys.executable,
-        "-m", "bandit",
-        "-r", "-f", 
+        "-m",
+        "bandit",
+        "-r",
+        "-f",
         "json",
-        repo['directory'],
+        repo["directory"],
         stdin=asyncio.subprocess.PIPE,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
-                
+
     proc.stdin.write(b"\n")
     proc.stdin.close()
     stdout, _stderr = await proc.communicate()
@@ -41,12 +38,11 @@ async def run_bandit(repo: Dict[str, str]) -> Dict[str, Any]:
     t_results = bandit_op["results"]
     high_sev_high_conf = 0
     for item in t_results:
-        if item["issue_confidence"] == "HIGH" and\
-            item["issue_severity"] == "HIGH":
-               high_sev_high_conf += 1
+        if (
+            item["issue_confidence"] == "HIGH"
+            and item["issue_severity"] == "HIGH"
+        ):
+            high_sev_high_conf += 1
     final_result = bandit_op["metrics"]["_totals"]
     final_result["CONFIDENCE.HIGH_AND_SEVERITY.HIGH"] = high_sev_high_conf
-    print(final_result)
-    return {
-            'report': final_result
-            }
+    return {"report": final_result}
--- examples/shouldi/tests/test_bandit.py
+++ examples/shouldi/tests/test_bandit.py
@@ -9,10 +9,12 @@ class TestRunBanditOp(AsyncTestCase):
     async def test_run(self):
         async with run_bandit.imp(BaseConfig()) as bandit_latest:
             async with bandit_latest(None, None) as ctx:
-                results = await ctx.run({"repo": {
-                    "URL": "dummy",
-                    "directory": os.getcwd()
-                    }
-                })
-                self.assertLessEqual(int(results['report']
-                            ["CONFIDENCE.HIGH_AND_SEVERITY.HIGH"]), 5.0)
+                results = await ctx.run(
+                    {"repo": {"URL": "dummy", "directory": os.getcwd()}}
+                )
+                self.assertLessEqual(
+                    int(
+                        results["report"]["CONFIDENCE.HIGH_AND_SEVERITY.HIGH"]
+                    ),
+                    5.0,
+                )
