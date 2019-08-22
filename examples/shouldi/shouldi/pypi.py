87a47fde4bdf89a5b8e75f74cf3b114099ec8554
Arvindh Kumar Chandran <arvindhium@gmail.com>
2019-08-21 23:01:47 -0700

    Created skeleton for the pypi operation

--- examples/shouldi/shouldi/pypi.py
+++ examples/shouldi/shouldi/pypi.py
@@ -5,18 +5,26 @@ from dffml.df.types import Definition
 from dffml.df.base import op
 
 package = Definition(name="package", primitive="str")
+package_json = Definition(name="package_json", primitive="Dict[str, Any]")
 package_version = Definition(name="package_version", primitive="str")
 
 
 @op(
     inputs={"package": package},
-    outputs={"version": package_version},
+    outputs={"response_json": package_json},
     imp_enter={
         "session": (lambda self: aiohttp.ClientSession(trust_env=True))
     },
 )
-async def pypi_latest_package_version(self, package: str) -> Dict[str, Any]:
+async def pypi_package_json(self, package: str) -> Dict[str, Any]:
     url = f"https://pypi.org/pypi/{package}/json"
     async with self.parent.session.get(url) as resp:
         package = await resp.json()
-        return {"version": package["info"]["version"]}
+        return {"response_json": package}
+
+@op(
+    inputs={"response_json": package_json},
+    output={"version": package_version}
+)
+async def pypi_latest_package_version(package_json: Dict["str", Any]) -> str:
+    return {"version": package_json["info"]["version"]}
--- examples/shouldi/tests/test_pypi.py
+++ examples/shouldi/tests/test_pypi.py
@@ -3,6 +3,14 @@ from dffml.util.asynctestcase import AsyncTestCase
 
 from shouldi.pypi import pypi_latest_package_version
 
+class TestPyPiPackageJson(AsyncTestCase):
+    async def test_run(self):
+        async with pypi_package_json.imp(
+            BaseConfig()
+            ) as pypi_package:
+                async with pypi_package(None, None) as ctx:
+                    results = await ctx.run({"package": "insecure-package"})
+                    self.assertEqual(results["response_json"]
 
 class TestPyPiLatestPackageVersion(AsyncTestCase):
     async def test_run(self):
