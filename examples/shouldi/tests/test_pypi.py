e0cf03469b5b6b5cf7c3e83335ad1f857f23eb6f
John Andersen <john.s.andersen@intel.com>
2019-08-23 14:03:22 -0700

    example: shouldi: tests: Save response JSON
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- examples/shouldi/tests/test_pypi.py
+++ examples/shouldi/tests/test_pypi.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+
 from dffml.df.base import BaseConfig
 from dffml.util.asynctestcase import AsyncTestCase
 
@@ -5,37 +8,28 @@ from shouldi.pypi import pypi_package_json
 from shouldi.pypi import pypi_latest_package_version
 from shouldi.pypi import pypi_package_url
 
-from pathlib import Path
-import json
 
+class TestPyPiOperations(AsyncTestCase):
+    PACKAGE = "insecure-package"
+    INT_RESULT_JSON = {}
 
-class TestPyPiPackageJson(AsyncTestCase):
-    async def test_run(self):
+    async def test_000_package_json(self):
         async with pypi_package_json.imp(BaseConfig()) as pypi_package:
             async with pypi_package(None, None) as ctx:
-                results = await ctx.run({"package": "insecure-package"})
+                results = await ctx.run({"package": self.PACKAGE})
                 self.assertIs(type(results["response_json"]), dict)
-                file_path = Path("response.json")
-                file_path.write_text(json.dumps(results["response_json"]))
+                self.INT_RESULT_JSON.update(results["response_json"])
 
-
-class TestPyPiLatestPackageVersion(AsyncTestCase):
-    async def test_run(self):
+    async def test_001_package_version(self):
         async with pypi_latest_package_version.imp(
             BaseConfig()
         ) as pypi_latest:
             async with pypi_latest(None, None) as ctx:
-                file_path = Path("response.json")
-                int_result_json = json.loads(file_path.read_text())
-                results = await ctx.run({"package_json": int_result_json})
+                results = await ctx.run({"package_json": self.INT_RESULT_JSON})
                 self.assertEqual(results["version"], "0.1.0")
 
-
-class TestPyPiPackageUrl(AsyncTestCase):
-    async def test_run(self):
+    async def test_002_package_url(self):
         async with pypi_package_url.imp(BaseConfig()) as pypi_url:
             async with pypi_url(None, None) as ctx:
-                file_path = Path("response.json")
-                int_result_json = json.loads(file_path.read_text())
-                results = await ctx.run({"package_json": int_result_json})
+                results = await ctx.run({"package_json": self.INT_RESULT_JSON})
                 self.assertIn("insecure-package-0.1.0.tar.gz", results["url"])
