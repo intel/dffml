90c76dd89697ac6fe013aeae6d2787c37608eded
Arvindh Kumar Chandran <arvindhium@gmail.com>
2019-08-22 23:04:12 -0700

    Debugged unit_test code for package_json and latest_package_version

--- examples/shouldi/shouldi/pypi.py
+++ examples/shouldi/shouldi/pypi.py
@@ -7,6 +7,7 @@ from dffml.df.base import op
 package = Definition(name="package", primitive="str")
 package_json = Definition(name="package_json", primitive="Dict[str, Any]")
 package_version = Definition(name="package_version", primitive="str")
+package_url = Definition(name="package_url", primitive="str")
 
 
 @op(
@@ -19,12 +20,19 @@ package_version = Definition(name="package_version", primitive="str")
 async def pypi_package_json(self, package: str) -> Dict[str, Any]:
     url = f"https://pypi.org/pypi/{package}/json"
     async with self.parent.session.get(url) as resp:
-        package = await resp.json()
-        return {"response_json": package}
+        package_json = await resp.json()
+        return {"response_json": package_json}
 
 @op(
     inputs={"response_json": package_json},
-    output={"version": package_version}
+    outputs={"version": package_version}
 )
-async def pypi_latest_package_version(package_json: Dict["str", Any]) -> str:
+async def pypi_latest_package_version(package_json: Dict[str, Any]) -> str:
     return {"version": package_json["info"]["version"]}
+
+#@op(
+#    inputs={"response_json": package_json},
+#    outputs={"url": package_url}
+#)
+#async def pypi_package_url(package_json: Dict["str", Any]) -> str:
+
--- examples/shouldi/tests/test_pypi.py
+++ examples/shouldi/tests/test_pypi.py
@@ -1,7 +1,11 @@
 from dffml.df.base import BaseConfig
 from dffml.util.asynctestcase import AsyncTestCase
 
+from shouldi.pypi import pypi_package_json
 from shouldi.pypi import pypi_latest_package_version
+
+from pathlib import Path
+import json
 
 class TestPyPiPackageJson(AsyncTestCase):
     async def test_run(self):
@@ -10,13 +14,20 @@ class TestPyPiPackageJson(AsyncTestCase):
             ) as pypi_package:
                 async with pypi_package(None, None) as ctx:
                     results = await ctx.run({"package": "insecure-package"})
-                    self.assertEqual(results["response_json"]
+                    self.assertIs(type(results["response_json"]), dict)
+                    file_path = Path("response.json")
+                    file_path.write_text(json.dumps(results["response_json"]))
 
 class TestPyPiLatestPackageVersion(AsyncTestCase):
+    #async def setup(self):
+    #    file_path = Path("response.json")
+    #    int_result_json = json.loads(file_path.read_text())
+    #    print(result_json)
     async def test_run(self):
         async with pypi_latest_package_version.imp(
             BaseConfig()
         ) as pypi_latest:
             async with pypi_latest(None, None) as ctx:
-                results = await ctx.run({"package": "insecure-package"})
+                int_result_json = {"info": {"version": "0.1.0"}}
+                results = await ctx.run({"package_json": int_result_json})
                 self.assertEqual(results["version"], "0.1.0")
