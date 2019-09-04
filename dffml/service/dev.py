892f8d63000d972dcc2aae078e85853458293a66
John Andersen <john.s.andersen@intel.com>
2019-09-04 16:15:18 -0700

    service: dev: Run single operation
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- dffml/service/dev.py
+++ dffml/service/dev.py
@@ -1,15 +1,24 @@
 import os
+import uuid
+import json
+import pydoc
 import getpass
+import importlib
 import configparser
 from pathlib import Path
 
 from ..util.skel import Skel
 from ..util.cli.cmd import CMD
-
 from ..version import VERSION
 from ..util.skel import Skel, SkelTemplateConfig
 from ..util.cli.arg import Arg
 from ..util.cli.cmd import CMD
+from ..base import MissingConfig
+from ..util.data import traverse_config_get
+from ..df.types import Input
+from ..df.base import Operation
+from ..df.memory import MemoryOrchestrator
+from ..operation.output import GetSingle
 
 config = configparser.ConfigParser()
 config.read(Path("~", ".gitconfig").expanduser())
@@ -115,6 +124,88 @@ class Skeleton(CMD):
     link = Link
 
 
+class Run(CMD):
+    """
+    Run a single operation
+    """
+
+    arg_operation = Arg("operation", help="Python path to operation")
+
+    async def run(self):
+        # Lookup
+        modname, qualname_separator, qualname = self.operation.partition(":")
+        obj = importlib.import_module(modname)
+        if qualname_separator:
+            for attr in qualname.split("."):
+                obj = getattr(obj, attr)
+                self.logger.debug("Loaded operation: %s(%s)", attr, obj)
+                return await self.run_op(attr, obj)
+
+    def config_get(self, op, key, definition):
+        # TODO De-duplicate code from dffml/base.py
+        try:
+            value = traverse_config_get(self.extra_config, key)
+        except KeyError as error:
+            raise MissingConfig("%s missing %s" % (op.name, key))
+        # TODO Argparse nargs and Arg and primitives need to be unified
+        if "Dict" in definition.primitive:
+            # TODO handle Dict / spec completely
+            self.logger.ciritical(
+                "Dict / spec'd arguments are not yet completely handled"
+            )
+            value = json.loads(value)
+        else:
+            typecast = pydoc.locate(
+                definition.primitive.replace("List[", "").replace("[", "")
+            )
+            # TODO This is a oversimplification of argparse's nargs
+            if definition.primitive.startswith("List["):
+                value = list(map(typecast, value))
+            else:
+                value = typecast(value[0])
+                if typecast is str and value in ["True", "False"]:
+                    raise MissingConfig("%s missing %s" % (op.name, key))
+        return value
+
+    async def run_op(self, name, opimp):
+        # Create an instance of BaseConfigurable and have it parse the inputs
+        # from self.extra_config. Use the op.inputs to know what we should pass
+        # to config_get
+        inputs = []
+        for name, definition in opimp.op.inputs.items():
+            try:
+                inputs.append(
+                    Input(
+                        value=self.config_get(opimp.op, name, definition),
+                        definition=definition,
+                    )
+                )
+            except MissingConfig as error:
+                error.args = (f"{opimp.op.inputs}: {error.args[0]}",)
+                raise error
+        # Run the operation in an orchestrator
+        async with MemoryOrchestrator.basic_config(
+            opimp.imp, GetSingle.imp
+        ) as orchestrator:
+            async with orchestrator() as octx:
+                # TODO Assign a sha384 string as the random string context
+                await octx.ictx.sadd(
+                    str(uuid.uuid4()),
+                    Input(
+                        value=[
+                            definition.name
+                            for definition in opimp.op.outputs.values()
+                        ],
+                        definition=GetSingle.op.inputs["spec"],
+                    ),
+                    *inputs,
+                )
+                async for ctx, results in octx.run_operations():
+                    # TODO There is probably an issue if multiple outputs have
+                    # the same data type that only one will be shown
+                    return results[GetSingle.op.name]
+
+
 class Develop(CMD):
     """
     Development utilities for hacking on DFFML itself
@@ -122,3 +213,4 @@ class Develop(CMD):
 
     create = Create
     skel = Skeleton
+    run = Run
--- examples/shouldi/shouldi/bandit.py
+++ examples/shouldi/shouldi/bandit.py
@@ -15,6 +15,9 @@ bandit_output = Definition(name="bandit_output", primitive="Dict[str, Any]")
 
 @op(inputs={"pkg": package_src_dir}, outputs={"report": bandit_output})
 async def run_bandit(pkg: str) -> Dict[str, Any]:
+    """
+    CLI usage: dffml service dev run -log debug shouldi.bandit:run_bandit -pkg .
+    """
     proc = await asyncio.create_subprocess_exec(
         sys.executable,
         "-m",
@@ -23,13 +26,10 @@ async def run_bandit(pkg: str) -> Dict[str, Any]:
         "-f",
         "json",
         pkg,
-        stdin=asyncio.subprocess.PIPE,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
 
-    proc.stdin.write(b"\n")
-    proc.stdin.close()
     stdout, _stderr = await proc.communicate()
     if len(stdout) == 0:
         raise Exception
