a8bd20ab32bd1f69176b4bd03d98b245cff0df01
sudharsana-kjl <kjlsudharsana@gmail.com>
2019-09-13 06:51:45 +0530

    change hdfscli to hdfs

--- source/hdfs/setup.py
+++ source/hdfs/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 from dffml_setup_common import SETUP_KWARGS, IMPORT_NAME
 
-SETUP_KWARGS["install_requires"] += ["hdfscli>=2.5.8"]
+SETUP_KWARGS["install_requires"] += ["hdfs>=2.5.8"]
 SETUP_KWARGS["tests_require"] = ["docker>=4.0.2"]
 SETUP_KWARGS["entry_points"] = {
     "dffml.source": [f"misc = {IMPORT_NAME}.source:HDFSSource"]
