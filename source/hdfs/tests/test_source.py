0b6dfcb0566e8f5183dda5befab772b4e96b7916
sudharsana-kjl <kjlsudharsana@gmail.com>
2019-10-01 20:46:32 +0530

    modify docker_hdfs set up

--- source/hdfs/tests/test_source.py
+++ source/hdfs/tests/test_source.py
@@ -3,11 +3,11 @@ from dffml_source_hdfs.source import HDFSSourceConfig, HDFSSource
 from dffml.util.testing.source import SourceTest
 from dffml.util.asynctestcase import AsyncTestCase
 from dffml.source.csv import CSVSource, CSVSourceConfig
+# from dffml.source.csv import CSVSource, CSVSourceConfig
 
 
 class TestHDFSSource(SourceTest, AsyncTestCase):
     async def setUpSource(self):
-	print("\n \n HADOOP SOURCE RUNNING")
         return HDFSSource(
             HDFSSourceConfig(
                 host="localhost",
