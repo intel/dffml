e9713adf18d45d9002aa3313d42d248a6d68237f
John Andersen <johnandersenpdx@gmail.com>
2019-10-02 22:10:41 -0700

    service: http: Support for models
    * Introduced context creation API as well.
    
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- service/http/tests/test_routes.py
+++ service/http/tests/test_routes.py
@@ -4,18 +4,30 @@ import asyncio
 import tempfile
 from http import HTTPStatus
 from unittest.mock import patch
-from contextlib import asynccontextmanager
+from contextlib import asynccontextmanager, AsyncExitStack
+from typing import AsyncIterator, Tuple, Dict, Any, List, NamedTuple
 
 import aiohttp
 
 from dffml.repo import Repo
+from dffml.base import BaseConfig
+from dffml.model.model import ModelContext, Model
+from dffml.accuracy import Accuracy
+from dffml.feature import Features, DefFeature
 from dffml.source.memory import MemorySource, MemorySourceConfig
+from dffml.source.source import Sources
 from dffml.source.csv import CSVSourceConfig
-from dffml.util.cli.arg import parse_unknown
+from dffml.util.entrypoint import entry_point
+from dffml.util.cli.arg import Arg, parse_unknown
 from dffml.util.asynctestcase import AsyncTestCase
 
 from dffml_service_http.cli import Server
-from dffml_service_http.routes import OK, SOURCE_NOT_LOADED
+from dffml_service_http.routes import (
+    OK,
+    SOURCE_NOT_LOADED,
+    MODEL_NOT_LOADED,
+    MODEL_NO_SOURCES,
+)
 
 from .common import ServerRunner
 
@@ -24,19 +36,70 @@ class ServerException(Exception):
     pass  # pragma: no cov
 
 
+class FakeModelConfig(NamedTuple):
+    directory: str
+
+
+class FakeModelContext(ModelContext):
+    def __init__(self, parent, features):
+        super().__init__(parent, features)
+        self.trained_on: Dict[str, Repo] = {}
+
+    async def train(self, sources: Sources):
+        async for repo in sources.repos():
+            self.trained_on[repo.src_url] = repo
+
+    async def accuracy(self, sources: Sources) -> Accuracy:
+        accuracy: int = 0
+        async for repo in sources.repos():
+            accuracy += int(repo.src_url)
+        return Accuracy(accuracy)
+
+    async def predict(self, repos: AsyncIterator[Repo]) -> AsyncIterator[Repo]:
+        async for repo in repos:
+            repo.predicted(repo.feature("by_ten") * 10, float(repo.src_url))
+            yield repo
+
+
+@entry_point("fake")
+class FakeModel(Model):
+
+    CONTEXT = FakeModelContext
+
+    @classmethod
+    def args(cls, args, *above) -> Dict[str, Arg]:
+        cls.config_set(args, above, "directory", Arg())
+        return args
+
+    @classmethod
+    def config(cls, config, *above) -> BaseConfig:
+        return FakeModelConfig(
+            directory=cls.config_get(config, above, "directory")
+        )
+
+
+def model_load(loading=None):
+    if loading:
+        return FakeModel
+    return [FakeModel]
+
+
 class TestRoutesRunning:
     async def setUp(self):
-        self._tserver = ServerRunner.patch(Server)
-        self.tserver = await self._tserver.__aenter__()
+        self.exit_stack = AsyncExitStack()
+        await self.exit_stack.__aenter__()
+        self.tserver = await self.exit_stack.enter_async_context(
+            ServerRunner.patch(Server)
+        )
         self.cli = Server(port=0, insecure=True)
         await self.tserver.start(self.cli.run())
         # Set up client
-        self._client = aiohttp.ClientSession()
-        self.session = await self._client.__aenter__()
+        self.session = await self.exit_stack.enter_async_context(
+            aiohttp.ClientSession()
+        )
 
     async def tearDown(self):
-        await self._client.__aexit__(None, None, None)
-        await self._tserver.__aexit__(None, None, None)
+        await self.exit_stack.__aexit__(None, None, None)
 
     @property
     def url(self):
@@ -55,6 +118,30 @@ class TestRoutesRunning:
             if r.status != HTTPStatus.OK:
                 raise ServerException((await r.json())["error"])
             yield r
+
+    @asynccontextmanager
+    async def _add_memory_source(self):
+        self.features = Features(DefFeature("by_ten", int, 1))
+        async with MemorySource(
+            MemorySourceConfig(
+                repos=[
+                    Repo(str(i), data={"features": {"by_ten": i * 10}})
+                    for i in range(0, self.num_repos)
+                ]
+            )
+        ) as source:
+            self.source = self.cli.app["sources"][self.slabel] = source
+            async with source() as sctx:
+                self.sctx = self.cli.app["source_contexts"][self.slabel] = sctx
+                yield
+
+    @asynccontextmanager
+    async def _add_fake_model(self):
+        async with FakeModel(BaseConfig()) as model:
+            self.model = self.cli.app["models"][self.mlabel] = model
+            async with model(self.features) as mctx:
+                self.mctx = self.cli.app["model_contexts"][self.mlabel] = mctx
+                yield
 
 
 class TestRoutesService(TestRoutesRunning, AsyncTestCase):
@@ -112,6 +199,12 @@ class TestRoutesList(TestRoutesRunning, AsyncTestCase):
             self.assertIn("json", body)
             self.assertIn("memory", body)
 
+    async def test_models(self):
+        with patch.object(Model, "load", new=model_load):
+            async with self.get("/list/models") as r:
+                body = await r.json()
+                self.assertIn("fake", body)
+
 
 class TestRoutesConfigure(TestRoutesRunning, AsyncTestCase):
     async def test_source(self):
@@ -131,6 +224,10 @@ class TestRoutesConfigure(TestRoutesRunning, AsyncTestCase):
                     label_column="label",
                 ),
             )
+            with self.subTest(context="salaryctx"):
+                async with self.get("/context/source/salary/salaryctx") as r:
+                    self.assertEqual(await r.json(), OK)
+                    self.assertIn("salaryctx", self.cli.app["source_contexts"])
 
     async def test_source_error(self):
         config = parse_unknown("--source-file", "dataset.csv")
@@ -138,43 +235,110 @@ class TestRoutesConfigure(TestRoutesRunning, AsyncTestCase):
             async with self.post("/configure/source/csv/salary", json=config):
                 pass  # pramga: no cov
 
-    async def test_source_not_found(self):
+    async def test_source_context_source_not_found(self):
         with self.assertRaisesRegex(
-            ServerException, "source feed face not found"
+            ServerException, f"salary source not found"
         ):
-            async with self.post(
-                "/configure/source/feed face/salary", json={}
-            ):
+            async with self.get("/context/source/salary/salaryctx") as r:
                 pass  # pramga: no cov
+
+    async def test_model(self):
+        with tempfile.TemporaryDirectory() as tempdir, patch.object(
+            Model, "load", new=model_load
+        ):
+            config = parse_unknown("--model-directory", tempdir)
+            async with self.post(
+                "/configure/model/fake/salary", json=config
+            ) as r:
+                self.assertEqual(await r.json(), OK)
+                self.assertIn("salary", self.cli.app["models"])
+                self.assertEqual(
+                    self.cli.app["models"]["salary"].config,
+                    FakeModelConfig(directory=tempdir),
+                )
+                with self.subTest(context="salaryctx"):
+                    # Define the features
+                    features = Features(
+                        DefFeature("Years", int, 1),
+                        DefFeature("Experiance", int, 1),
+                    )
+                    exported_features = features.export()
+                    # Check that we can send shorthand version of feature_def
+                    for name, feature_def in exported_features.items():
+                        del feature_def["name"]
+                    # Create the context
+                    async with self.post(
+                        "/context/model/salary/salaryctx",
+                        json=exported_features,
+                    ) as r:
+                        self.assertEqual(await r.json(), OK)
+                        self.assertIn(
+                            "salaryctx", self.cli.app["model_contexts"]
+                        )
+                        self.assertEqual(
+                            self.cli.app["model_contexts"][
+                                "salaryctx"
+                            ].features.export(),
+                            features.export(),
+                        )
+
+    async def test_model_bad_features(self):
+        with tempfile.TemporaryDirectory() as tempdir, patch.object(
+            Model, "load", new=model_load
+        ):
+            config = parse_unknown("--model-directory", tempdir)
+            async with self.post(
+                "/configure/model/fake/salary", json=config
+            ) as r:
+                self.assertEqual(await r.json(), OK)
+                with self.assertRaisesRegex(
+                    ServerException, "Incorrect format for features"
+                ):
+                    async with self.post(
+                        "/context/model/salary/salaryctx", json=[]
+                    ) as r:
+                        pass  # pramga: no cov
+
+    async def test_model_config_error(self):
+        # Should be directory, not folder
+        config = parse_unknown("--model-folder", "mymodel_dir")
+        with patch.object(Model, "load", new=model_load):
+            with self.assertRaisesRegex(ServerException, "missing.*directory"):
+                async with self.post(
+                    "/configure/model/fake/salary", json=config
+                ):
+                    pass  # pramga: no cov
+
+    async def test_model_context_model_not_found(self):
+        with self.assertRaisesRegex(
+            ServerException, f"salary model not found"
+        ):
+            features = Features()
+            async with self.post(
+                "/context/model/salary/salaryctx", json=features.export()
+            ) as r:
+                pass  # pramga: no cov
+
+    async def test_not_found(self):
+        for check in ["source", "model"]:
+            with self.subTest(check=check):
+                with self.assertRaisesRegex(
+                    ServerException, f"{check} feed face not found"
+                ):
+                    async with self.post(
+                        f"/configure/{check}/feed face/label", json={}
+                    ):
+                        pass  # pramga: no cov
 
 
 class TestRoutesSource(TestRoutesRunning, AsyncTestCase):
-    @asynccontextmanager
-    async def _add_memory_source(self):
-
-        async with MemorySource(
-            MemorySourceConfig(
-                repos=[
-                    Repo(str(i), data={"features": {"by_ten": i * 10}})
-                    for i in range(0, self.num_repos)
-                ]
-            )
-        ) as source:
-            self.source = self.cli.app["sources"][self.label] = source
-            async with source() as sctx:
-                self.sctx = self.cli.app["source_contexts"][self.label] = sctx
-                yield
-
     async def setUp(self):
         await super().setUp()
-        self.label: str = "mydataset"
+        self.slabel: str = "mydataset"
         self.num_repos: int = 100
-        self.add_memory_source = self._add_memory_source()
-        await self.add_memory_source.__aenter__()
-
-    async def tearDown(self):
-        await super().tearDown()
-        await self.add_memory_source.__aexit__(None, None, None)
+        self.add_memory_source = await self.exit_stack.enter_async_context(
+            self._add_memory_source()
+        )
 
     async def test_source_not_found(self):
         with self.assertRaisesRegex(
@@ -185,16 +349,16 @@ class TestRoutesSource(TestRoutesRunning, AsyncTestCase):
 
     async def test_repo(self):
         for i in range(0, self.num_repos):
-            async with self.get(f"/source/{self.label}/repo/{i}") as r:
+            async with self.get(f"/source/{self.slabel}/repo/{i}") as r:
                 self.assertEqual(
-                    await r.json(), self.source.config.repos[i].dict()
+                    await r.json(), self.source.config.repos[i].export()
                 )
 
     async def test_update(self):
         key = "1"
         new_repo = Repo(key, data={"features": {"by_ten": 10}})
         async with self.post(
-            f"/source/{self.label}/update/{key}", json=new_repo.dict()
+            f"/source/{self.slabel}/update/{key}", json=new_repo.export()
         ) as r:
             self.assertEqual(await r.json(), OK)
         self.assertEqual((await self.sctx.repo(key)).feature("by_ten"), 10)
@@ -204,12 +368,12 @@ class TestRoutesSource(TestRoutesRunning, AsyncTestCase):
         self.assertIn("repos", response)
         for src_url, repo in response["repos"].items():
             self.assertEqual(
-                repo, self.source.config.repos[int(src_url)].dict()
+                repo, self.source.config.repos[int(src_url)].export()
             )
 
     async def test_repos(self):
         chunk_size = self.num_repos
-        async with self.get(f"/source/{self.label}/repos/{chunk_size}") as r:
+        async with self.get(f"/source/{self.slabel}/repos/{chunk_size}") as r:
             response = await r.json()
             self._check_iter_response(response)
             self.assertEqual(response["iterkey"], None)
@@ -223,7 +387,7 @@ class TestRoutesSource(TestRoutesRunning, AsyncTestCase):
     async def test_repos_iterkey(self):
         chunk_size = 7
         got_repos = {}
-        async with self.get(f"/source/{self.label}/repos/{chunk_size}") as r:
+        async with self.get(f"/source/{self.slabel}/repos/{chunk_size}") as r:
             response = await r.json()
             self._check_iter_response(response)
             iterkey = response["iterkey"]
@@ -231,7 +395,7 @@ class TestRoutesSource(TestRoutesRunning, AsyncTestCase):
             got_repos.update(response["repos"])
         while iterkey is not None:
             async with self.get(
-                f"/source/{self.label}/repos/{iterkey}/{chunk_size}"
+                f"/source/{self.slabel}/repos/{iterkey}/{chunk_size}"
             ) as r:
                 response = await r.json()
                 self._check_iter_response(response)
@@ -249,6 +413,99 @@ class TestRoutesSource(TestRoutesRunning, AsyncTestCase):
         iterkey = "feedface"
         with self.assertRaisesRegex(ServerException, "iterkey not found"):
             async with self.get(
-                f"/source/{self.label}/repos/{iterkey}/{chunk_size}"
+                f"/source/{self.slabel}/repos/{iterkey}/{chunk_size}"
+            ) as r:
+                pass  # pramga: no cov
+
+
+class TestModel(TestRoutesRunning, AsyncTestCase):
+    async def setUp(self):
+        await super().setUp()
+        self.mlabel: str = "mymodel"
+        self.slabel: str = "mydataset"
+        self.num_repos: int = 100
+        self.add_memory_source = await self.exit_stack.enter_async_context(
+            self._add_memory_source()
+        )
+        self.add_fake_model = await self.exit_stack.enter_async_context(
+            self._add_fake_model()
+        )
+
+    async def test_model_not_found(self):
+        with self.assertRaisesRegex(
+            ServerException, list(MODEL_NOT_LOADED.values())[0]
+        ):
+            async with self.post(f"/model/non-existant/accuracy", json=[]):
+                pass  # pramga: no cov
+
+    async def test_no_sources(self):
+        for method in ["train", "accuracy"]:
+            with self.subTest(method=method):
+                with self.assertRaisesRegex(
+                    ServerException, list(MODEL_NO_SOURCES.values())[0]
+                ):
+                    async with self.post(
+                        f"/model/{self.mlabel}/{method}", json=[]
+                    ):
+                        pass  # pramga: no cov
+
+    async def test_source_not_found(self):
+        for method in ["train", "accuracy"]:
+            with self.subTest(method=method):
+                with self.assertRaisesRegex(
+                    ServerException, list(SOURCE_NOT_LOADED.values())[0]
+                ):
+                    async with self.post(
+                        f"/model/{self.mlabel}/train", json=["non-existant"]
+                    ):
+                        pass  # pramga: no cov
+
+    async def test_train(self):
+        async with self.post(
+            f"/model/{self.mlabel}/train", json=[self.slabel]
+        ) as r:
+            self.assertEqual(await r.json(), OK)
+        for i in range(0, self.num_repos):
+            self.assertIn(str(i), self.mctx.trained_on)
+
+    async def test_accuracy(self):
+        async with self.post(
+            f"/model/{self.mlabel}/accuracy", json=[self.slabel]
+        ) as r:
+            self.assertEqual(
+                await r.json(),
+                {"accuracy": float(sum(range(0, self.num_repos)))},
+            )
+
+    async def test_predict(self):
+        repos: Dict[str, Repo] = {
+            repo.src_url: repo.export() async for repo in self.sctx.repos()
+        }
+        async with self.post(
+            f"/model/{self.mlabel}/predict/0", json=repos
+        ) as r:
+            i: int = 0
+            response = await r.json()
+            for src_url, repo_data in response["repos"].items():
+                repo = Repo(src_url, data=repo_data)
+                self.assertEqual(int(repo.src_url), i)
+                self.assertEqual(
+                    repo.feature("by_ten"), repo.prediction().value / 10
+                )
+                self.assertEqual(
+                    float(repo.src_url), repo.prediction().confidence
+                )
+                i += 1
+            self.assertEqual(i, self.num_repos)
+
+    async def test_predict_chunk_size_unsupported(self):
+        repos: Dict[str, Repo] = {
+            repo.src_url: repo.export() async for repo in self.sctx.repos()
+        }
+        with self.assertRaisesRegex(
+            ServerException, "Multiple request iteration not yet supported"
+        ):
+            async with self.post(
+                f"/model/{self.mlabel}/predict/7", json=repos
             ) as r:
                 pass  # pramga: no cov
