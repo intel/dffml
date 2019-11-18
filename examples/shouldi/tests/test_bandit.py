9f4be210672534db725ceec14ca4b3883e6fe282
John Andersen <john.s.andersen@intel.com>
2019-11-17 19:37:16 -0800

    Update test_bandit.py

--- examples/shouldi/tests/test_bandit.py
+++ examples/shouldi/tests/test_bandit.py
@@ -8,6 +8,6 @@ from shouldi.bandit import run_bandit
 class TestRunBanditOp(AsyncTestCase):
     async def test_run(self):
         results = await run_bandit(os.getcwd())
-        self.assertLessEqual(
-            int(results["report"]["CONFIDENCE.HIGH_AND_SEVERITY.HIGH"]), 5.0
+        self.assertEqual(
+            type(results["report"]["CONFIDENCE.HIGH_AND_SEVERITY.HIGH"]), int
         )
