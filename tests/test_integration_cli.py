4fa0d5ace259600886f2cfd624e09c7746308f95
John Andersen <john.s.andersen@intel.com>
2019-11-25 18:38:04 -0800

    test: integration: dataflow: Diagram and Merge
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- tests/test_integration_cli.py
+++ tests/test_integration_cli.py
@@ -2,20 +2,59 @@
 This file contains integration tests. We use the CLI to exercise functionality of
 various DFFML classes and constructs.
 """
+import re
+import os
 import io
+import json
 import inspect
 import pathlib
+import asyncio
 import contextlib
+import unittest.mock
 
+from dffml.df.types import Operation, DataFlow
 from dffml.cli.cli import CLI
+from dffml.service.dev import Develop
+from dffml.util.packaging import is_develop
+from dffml.util.entrypoint import load
 from dffml.util.asynctestcase import AsyncTestCase
 
 from .test_cli import non_existant_tempfile
 
 
+def relative_path(*args):
+    """
+    Returns a pathlib.Path object with the path relative to this file.
+    """
+    target = pathlib.Path(__file__).parents[0] / args[0]
+    for path in list(args)[1:]:
+        target /= path
+    return target
+
+
+@contextlib.contextmanager
+def relative_chdir(*args):
+    """
+    Change directory to a location relative to the location of this file.
+    """
+    target = relative_path(*args)
+    orig_dir = os.getcwd()
+    try:
+        os.chdir(target)
+        yield target
+    finally:
+        os.chdir(orig_dir)
+
+
 class IntegrationCLITestCase(AsyncTestCase):
+    REQUIRED_PLUGINS = []
+
     async def setUp(self):
         super().setUp()
+        if not all(map(is_develop, self.REQUIRED_PLUGINS)):
+            self.skipTest(
+                f"Required plugins: {', '.join(self.REQUIRED_PLUGINS)} must be installed in development mode"
+            )
         self.stdout = io.StringIO()
         self._stack = contextlib.ExitStack().__enter__()
 
@@ -94,3 +133,119 @@ class TestMerge(IntegrationCLITestCase):
             )
             + "\n",
         )
+
+
+class TestDevelop(IntegrationCLITestCase):
+
+    REQUIRED_PLUGINS = ["shouldi"]
+
+    async def test_export(self):
+        stdout = io.StringIO()
+        # Use shouldi's dataflow for tests
+        with relative_chdir("..", "examples", "shouldi"):
+            with unittest.mock.patch("sys.stdout.buffer.write") as write:
+                await Develop.cli("export", "shouldi.cli:DATAFLOW")
+            DataFlow._fromdict(**json.loads(write.call_args[0][0]))
+
+
+class TestDataFlow(IntegrationCLITestCase):
+
+    REQUIRED_PLUGINS = ["shouldi", "dffml-config-yaml", "dffml-feature-git"]
+
+    async def setUp(self):
+        await super().setUp()
+        # Use shouldi's dataflow for tests
+        self.DATAFLOW = list(
+            load(
+                "shouldi.cli:DATAFLOW",
+                relative=relative_path("..", "examples", "shouldi"),
+            )
+        )[0]
+
+    async def test_diagram_default(self):
+        filename = self.mktempfile() + ".json"
+        pathlib.Path(filename).write_text(json.dumps(self.DATAFLOW.export()))
+        with contextlib.redirect_stdout(self.stdout):
+            await CLI.cli(
+                "dataflow", "diagram", filename,
+            )
+        stdout = self.stdout.getvalue()
+        # Check that a subgraph is being made for each operation
+        self.assertTrue(re.findall(r"subgraph.*run_bandit", stdout))
+        # Check that all stages are included
+        for check in ["Processing", "Output", "Cleanup"]:
+            self.assertIn(f"{check} Stage", stdout)
+
+    async def test_diagram_simple(self):
+        filename = self.mktempfile() + ".json"
+        pathlib.Path(filename).write_text(json.dumps(self.DATAFLOW.export()))
+        with contextlib.redirect_stdout(self.stdout):
+            await CLI.cli(
+                "dataflow", "diagram", "-simple", filename,
+            )
+        # Check that a subgraph is not being made for each operation
+        self.assertFalse(
+            re.findall(r"subgraph.*run_bandit", self.stdout.getvalue())
+        )
+
+    async def test_diagram_single_stage(self):
+        filename = self.mktempfile() + ".json"
+        pathlib.Path(filename).write_text(json.dumps(self.DATAFLOW.export()))
+        with contextlib.redirect_stdout(self.stdout):
+            await CLI.cli(
+                "dataflow", "diagram", "-stages", "processing", "--", filename,
+            )
+        stdout = self.stdout.getvalue()
+        # Check that the single stage is not its own subgraph
+        for check in ["Processing", "Output", "Cleanup"]:
+            self.assertNotIn(f"{check} Stage", stdout)
+
+    async def test_diagram_multi_stage(self):
+        filename = self.mktempfile() + ".json"
+        pathlib.Path(filename).write_text(json.dumps(self.DATAFLOW.export()))
+        with contextlib.redirect_stdout(self.stdout):
+            await CLI.cli(
+                "dataflow",
+                "diagram",
+                "-stages",
+                "processing",
+                "output",
+                "--",
+                filename,
+            )
+        stdout = self.stdout.getvalue()
+        # Check that the single stage is not its own subgraph
+        for check in ["Processing", "Output"]:
+            self.assertIn(f"{check} Stage", stdout)
+        for check in ["Cleanup"]:
+            self.assertNotIn(f"{check} Stage", stdout)
+
+    async def test_merge(self):
+        # Write out shouldi dataflow
+        orig = self.mktempfile() + ".json"
+        pathlib.Path(orig).write_text(json.dumps(self.DATAFLOW.export()))
+        # Import from feature/git
+        transform_to_repo = Operation.load("dffml.mapping.create")
+        lines_of_code_by_language, lines_of_code_to_comments = list(
+            load(
+                "dffml_feature_git.feature.operations:lines_of_code_by_language",
+                "dffml_feature_git.feature.operations:lines_of_code_to_comments",
+                relative=relative_path("..", "feature", "git"),
+            )
+        )
+        # Create new dataflow
+        override = DataFlow.auto(
+            transform_to_repo,
+            lines_of_code_by_language,
+            lines_of_code_to_comments,
+        )
+        # TODO Modify and compare against yaml in docs example
+        # Write out override dataflow
+        created = self.mktempfile() + ".json"
+        pathlib.Path(created).write_text(json.dumps(override.export()))
+        # Merge the two
+        with contextlib.redirect_stdout(self.stdout):
+            await CLI.cli(
+                "dataflow", "merge", orig, created,
+            )
+        DataFlow._fromdict(**json.loads(self.stdout.getvalue()))
