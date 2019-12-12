89ada4f0effc4c2105b133668b8aba768a2f64a1
John Andersen <johnandersenpdx@gmail.com>
2019-12-12 00:42:55 -0800

    release: Version 0.3.1
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- feature/auth/setup.py
+++ feature/auth/setup.py
@@ -19,7 +19,7 @@ with open(os.path.join(self_path, "README.md"), "r", encoding="utf-8") as f:
     readme = f.read()
 
 INSTALL_REQUIRES = [] + (
-    ["dffml>=0.3.0"]
+    ["dffml>=0.3.1"]
     if not any(
         list(
             map(
--- model/scikit/setup.py
+++ model/scikit/setup.py
@@ -14,7 +14,7 @@ INSTALL_REQUIRES = [
     "joblib>=0.13.2",
     "pandas>=0.25.0",
 ] + (
-    ["dffml>=0.3.0"]
+    ["dffml>=0.3.1"]
     if not any(
         list(
             map(
