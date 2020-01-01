3ff9c72c37bc6932dc7bd071ecba5f15bfa23fdf
0dust <himanshutripathi366@gmail.com>
2020-01-02 00:38:05 +0530

    updated test_scikit.py

--- model/scikit/tests/test_scikit.py
+++ model/scikit/tests/test_scikit.py
@@ -103,9 +103,9 @@ class TestScikitModel:
                     elif self.MODEL_TYPE is "REGRESSION":
                         correct = FEATURE_DATA_REGRESSION[int(repo.src_url)][3]
                         self.assertGreater(
-                            prediction, correct - (correct * 0.20)
+                            prediction, correct - (correct * 0.40)
                         )
-                        self.assertLess(prediction, correct + (correct * 0.20))
+                        self.assertLess(prediction, correct + (correct * 0.40))
 
 
 FEATURE_DATA_CLASSIFICATION = [
