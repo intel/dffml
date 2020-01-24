7bf5d7226f08febc8f15a85c16bfd7cfd6840e6e
0dust <himanshutripathi366@gmail.com>
2020-01-25 00:27:45 +0530

    change version to 2.1.0

--- model/tensorflow/setup.py
+++ model/tensorflow/setup.py
@@ -18,7 +18,7 @@ with open(
 with open(os.path.join(self_path, "README.md"), "r", encoding="utf-8") as f:
     readme = f.read()
 
-INSTALL_REQUIRES = ["tensorflow>=2.0.0"] + (
+INSTALL_REQUIRES = ["tensorflow==2.1.0"] + (
     ["dffml>=0.3.1"]
     if not any(
         list(
