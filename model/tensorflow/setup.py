83a7d4b3229c955bc91c4b7ffc94498dffb5e8cf
John Andersen <johnandersenpdx@gmail.com>
2020-02-10 23:26:34 -0800

    release: Version 0.3.3
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- model/tensorflow/setup.py
+++ model/tensorflow/setup.py
@@ -19,7 +19,7 @@ with open(os.path.join(self_path, "README.md"), "r", encoding="utf-8") as f:
     readme = f.read()
 
 INSTALL_REQUIRES = ["tensorflow>=2.0.0"] + (
-    ["dffml>=0.3.1"]
+    ["dffml>=0.3.3"]
     if not any(
         list(
             map(
