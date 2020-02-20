06e40a8400d8dc76a832e77a5b744305d5bc628a
0dust <himanshutripathi366@gmail.com>
2020-02-21 04:03:17 +0530

    remove venv

--- .ci/run.sh
+++ .ci/run.sh
@@ -11,11 +11,6 @@ PYTHON=${PYTHON:-"python3.7"}
 TEMP_DIRS=()
 
 function run_plugin() {
-  # Create a virtualenv
-  venv_dir="$(mktemp -d)"
-  TEMP_DIRS+=("${venv_dir}")
-  "${PYTHON}" -m venv "${venv_dir}"
-  source "${venv_dir}/bin/activate"
   "${PYTHON}" -m pip install -U pip twine
 
   "${PYTHON}" -m pip install -U "${SRC_ROOT}"
@@ -60,9 +55,6 @@ function run_plugin() {
     "${PYTHON}" -m pip install -r requirements.txt
     "${PYTHON}" -m unittest discover
     cd "${SRC_ROOT}"
-
-    # Deactivate venv
-    deactivate
 
     # Create the docs
     "${PYTHON}" -m pip install -U -e "${SRC_ROOT}[dev]"
