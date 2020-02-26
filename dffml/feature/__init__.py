890803f102e160453ca8ef0937bf82d6a0bbcb66
John Andersen <johnandersenpdx@gmail.com>
2020-02-26 10:02:11 -0800

    feature: Remove non-working doctests
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/feature/__init__.py
+++ dffml/feature/__init__.py
@@ -1,18 +1,1 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2019 Intel Corporation
-"""
-All features registered to the dffml.feature entry point using setuptools are
-derived from the Feature class. To add a feature, create a module which has a
-setup.py which specifies where to find your Feature subclass within your module.
-
->>> setup(
->>>     name='myfeatures',
-...
->>>     entry_points={
->>>         'dffml.feature': [
->>>             'numfiles = myfeatures:NumFilesFeature',
->>>         ],
->>>     },
->>> )
-"""
 from .feature import Data, Feature, Features, LoggingDict, DefFeature
