c676b52038834c74b1b1ed0fc04da465292a8092
John Andersen <johnandersenpdx@gmail.com>
2020-02-29 21:43:26 -0800

    docs: conf: Move doctest header to own file
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ docs/doctest_header.py
@@ -0,0 +1,26 @@
+# This file is used as a header in every file that is created to run each
+# example when the doctests are run.
+import os
+import sys
+import shutil
+import atexit
+import inspect
+import asyncio
+import tempfile
+import functools
+
+# Create a temporary directory for test to run in
+DOCTEST_TEMPDIR = tempfile.mkdtemp()
+# Remove it when the test exits
+atexit.register(functools.partial(shutil.rmtree, DOCTEST_TEMPDIR))
+# Change the current working directory to the temporary directory
+os.chdir(DOCTEST_TEMPDIR)
+
+from dffml_model_scikit import *
+
+from dffml import *
+from dffml.base import *
+from dffml.df.base import *
+from dffml.df.types import *
+from dffml.df.memory import *
+from dffml.util.net import *
