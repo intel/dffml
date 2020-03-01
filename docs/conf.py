1f5fa5b41c21d05618430254c59f8254e4d7322b
John Andersen <johnandersenpdx@gmail.com>
2020-02-29 21:43:26 -0800

    docs: conf: Move doctest header to own file
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import pathlib
 
 sys.path.insert(0, os.path.abspath("."))
 from dffml.version import VERSION
@@ -86,29 +87,6 @@ html_static_path = ["_static"]
 
 napoleon_numpy_docstring = True
 
-doctest_global_setup = """
-import os
-import sys
-import shutil
-import atexit
-import inspect
-import asyncio
-import tempfile
-import functools
-
-# Create a temporary directory for test to run in
-DOCTEST_TEMPDIR = tempfile.mkdtemp()
-# Remove it when the test exits
-atexit.register(functools.partial(shutil.rmtree, DOCTEST_TEMPDIR))
-# Change the current working directory to the temporary directory
-os.chdir(DOCTEST_TEMPDIR)
-
-from dffml_model_scikit import *
-
-from dffml import *
-from dffml.base import *
-from dffml.df.base import *
-from dffml.df.types import *
-from dffml.df.memory import *
-from dffml.util.net import *
-"""
+doctest_global_setup = (
+    pathlib.Path(__file__).parent / "doctest_header.py"
+).read_text()
