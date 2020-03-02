509915e61a7d2f0e3815742b431e8129bc71b58b
John Andersen <johnandersenpdx@gmail.com>
2020-03-02 12:21:40 -0800

    df: operation: Add example for run_dataflow
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/operation/dataflow.py
+++ dffml/operation/dataflow.py
@@ -23,25 +23,63 @@ class RunDataFlowConfig:
 )
 async def run_dataflow(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
     """
-        Starts a subflow `self.config.dataflow` and runs `inputs` in it.
+    Starts a subflow ``self.config.dataflow`` and adds ``inputs`` in it.
 
-        Parameters:
-            inputs: Dict[str,Any] ->
-                eg: {
-                    "ctx_str" : [
-                        {
-                            "value":val1,
-                            "defintion":defintion1
-                        },
-                        {
-                            "value":val2,
-                            "defintion":defintion2
-                        }
-                    ]
-                }
-        Returns:
-            Dict[str,Any] -> maps context strings in inputs to output after running
-                            through dataflow
+    Parameters
+    ----------
+    inputs : dict
+        The inputs to add to the subflow. These should be a key value mapping of
+        the context string to the inputs which should be seeded for that context
+        string.
+
+    Returns
+    -------
+    dict
+        Maps context strings in inputs to output after running through dataflow.
+
+    Examples
+    --------
+
+    >>> URL = Definition(name="URL", primitive="string")
+    >>>
+    >>> subflow = DataFlow.auto(GetSingle)
+    >>> subflow.definitions[URL.name] = URL
+    >>> subflow.seed.append(
+    ...     Input(
+    ...         value=[URL.name],
+    ...         definition=GetSingle.op.inputs["spec"]
+    ...     )
+    ... )
+    >>>
+    >>> dataflow = DataFlow.auto(run_dataflow, GetSingle)
+    >>> dataflow.configs[run_dataflow.imp.op.name] = RunDataFlowConfig(subflow)
+    >>> dataflow.seed.append(
+    ...     Input(
+    ...         value=[run_dataflow.imp.op.outputs["results"].name],
+    ...         definition=GetSingle.op.inputs["spec"]
+    ...     )
+    ... )
+    >>>
+    >>> async def main():
+    ...     async for ctx, results in MemoryOrchestrator.run(dataflow, {
+    ...         "run_subflow": [
+    ...             Input(
+    ...                 value={
+    ...                     "dffml": [
+    ...                         {
+    ...                             "value": "https://github.com/intel/dffml",
+    ...                             "definition": URL.name
+    ...                         }
+    ...                     ]
+    ...                 },
+    ...                 definition=run_dataflow.imp.op.inputs["inputs"]
+    ...             )
+    ...         ]
+    ...     }):
+    ...         print(results)
+    >>>
+    >>> asyncio.run(main())
+    {'flow_results': {'dffml': {'URL': 'https://github.com/intel/dffml'}}}
     """
     inputs_created = {}
     definitions = self.config.dataflow.definitions
