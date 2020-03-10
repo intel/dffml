1c515f66f542c5a6815220c51e32475cfb876774
NMNDV <45735998+Naman1233@users.noreply.github.com>
2020-03-10 11:40:03 +0530

    Added necessary files from local

---
+++ dffml/service/visualizer/setup.py
@@ -0,0 +1,16 @@
+import os
+import importlib.util
+from setuptools import setup
+
+# Boilerplate to load commonalities
+spec = importlib.util.spec_from_file_location(
+    "setup_common", os.path.join(os.path.dirname(__file__), "setup_common.py")
+)
+common = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(common)
+
+common.KWARGS["entry_points"] = {
+    "dffml.service.cli": [f"misc = {common.IMPORT_NAME}.misc:MiscService"]
+}
+
+setup(**common.KWARGS)
