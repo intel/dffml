2312ab7108112d4dde8a505313bc3b161a9c9538
John Andersen <johnandersenpdx@gmail.com>
2020-03-10 15:31:27 -0700

    skel: model: Convert to SimpleModel
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/skel/model/REPLACE_IMPORT_PACKAGE_NAME/misc.py
+++ dffml/skel/model/REPLACE_IMPORT_PACKAGE_NAME/misc.py
@@ -1,60 +1,122 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2019 Intel Corporation
-"""
-Description of what this model does
-"""
-from typing import AsyncIterator, Tuple, Any, List
+import pathlib
+import statistics
+from typing import AsyncIterator, Tuple, Any, Type, List
 
-from dffml.record import Record
-from dffml.source.source import Sources
-from dffml.feature import Features
-from dffml.model.accuracy import Accuracy
-from dffml.model.model import ModelContext, Model
-from dffml.util.entrypoint import entrypoint
-from dffml.base import config
+from dffml import (
+    config,
+    field,
+    entrypoint,
+    SimpleModel,
+    ModelNotTrained,
+    Accuracy,
+    Feature,
+    Features,
+    Sources,
+    Record,
+)
+
+
+def matrix_subtract(one, two):
+    return [
+        one_element - two_element for one_element, two_element in zip(one, two)
+    ]
+
+
+def matrix_multiply(one, two):
+    return [
+        one_element * two_element for one_element, two_element in zip(one, two)
+    ]
+
+
+def squared_error(y, line):
+    return sum(map(lambda element: element ** 2, matrix_subtract(y, line)))
+
+
+def coeff_of_deter(y, regression_line):
+    y_mean_line = [statistics.mean(y)] * len(y)
+    squared_error_mean = squared_error(y, y_mean_line)
+    squared_error_regression = squared_error(y, regression_line)
+    return 1 - (squared_error_regression / squared_error_mean)
+
+
+def best_fit_line(x, y):
+    mean_x = statistics.mean(x)
+    mean_y = statistics.mean(y)
+    m = (mean_x * mean_y - statistics.mean(matrix_multiply(x, y))) / (
+        (mean_x ** 2) - statistics.mean(matrix_multiply(x, x))
+    )
+    b = mean_y - (m * mean_x)
+    regression_line = [m * x + b for x in x]
+    accuracy = coeff_of_deter(y, regression_line)
+    return (m, b, accuracy)
 
 
 @config
 class MiscModelConfig:
-    # This model never uses the directory, but chances are if you want to save
-    # and load data from disk you will need to
-    directory: str
-    classifications: List[str]
-    features: Features
+    predict: Feature = field("Label or the value to be predicted")
+    features: Features = field("Features to train on. For SLR only 1 allowed")
+    directory: pathlib.Path = field(
+        "Directory where state should be saved",
+        default=pathlib.Path("~", ".cache", "dffml", "miscmodel"),
+    )
 
 
-class MiscModelContext(ModelContext):
-    """
-    Model wraping model_name API
-    """
+@entrypoint("miscmodel")
+class MiscModel(SimpleModel):
+    # The configuration class needs to be set as the CONFIG property
+    CONFIG: Type[MiscModelConfig] = MiscModelConfig
+    # Simple Linear Regression only supports training on a single feature.
+    # Do not define NUM_SUPPORTED_FEATURES if you support arbitrary numbers of
+    # features.
+    NUM_SUPPORTED_FEATURES: int = 1
+    # We only support single dimensional values, non-matrix / array
+    # Do not define SUPPORTED_LENGTHS if you support arbitrary dimensions
+    SUPPORTED_LENGTHS: List[int] = [1]
 
-    async def train(self, sources: Sources):
-        """
-        Train using records as the data to learn from.
-        """
-        pass
+    async def train(self, sources: Sources) -> None:
+        # X and Y data
+        x = []
+        y = []
+        # Go through all records that have the feature we're training on and the
+        # feature we want to predict. Since our model only supports 1 feature,
+        # the self.features list will only have one element at index 0.
+        async for record in sources.with_features(
+            self.features + [self.config.predict.NAME]
+        ):
+            x.append(record.feature(self.features[0]))
+            y.append(record.feature(self.config.predict.NAME))
+        # Use self.logger to report how many records are being used for training
+        self.logger.debug("Number of input records: %d", len(x))
+        # Save m, b, and accuracy
+        self.storage["regression_line"] = best_fit_line(x, y)
 
     async def accuracy(self, sources: Sources) -> Accuracy:
-        """
-        Evaluates the accuracy of our model after training using the input records
-        as test data.
-        """
-        # Lies
-        return 1.0
+        # Load saved regression line
+        regression_line = self.storage.get("regression_line", None)
+        # Ensure the model has been trained before we try to make a prediction
+        if regression_line is None:
+            raise ModelNotTrained("Train model before assessing for accuracy.")
+        # Accuracy is the last element in regression_line, which is a list of
+        # three values: m, b, and accuracy.
+        return Accuracy(regression_line[2])
 
     async def predict(
         self, records: AsyncIterator[Record]
     ) -> AsyncIterator[Tuple[Record, Any, float]]:
-        """
-        Uses trained data to make a prediction about the quality of a record.
-        """
+        # Load saved regression line
+        regression_line = self.storage.get("regression_line", None)
+        # Ensure the model has been trained before we try to make a prediction
+        if regression_line is None:
+            raise ModelNotTrained("Train model before prediction.")
+        # Expand the regression_line into named variables
+        m, b, accuracy = regression_line
+        # Iterate through each record that needs a prediction
         async for record in records:
-            yield record, self.parent.config.classifications[
-                record.feature(self.parent.config.features.names()[0])
-            ], 1.0
-
-
-@entrypoint("misc")
-class MiscModel(Model):
-
-    CONTEXT = MiscModelContext
+            # Grab the x data from the record
+            x = record.feature(self.features[0])
+            # Calculate y
+            y = m * x + b
+            # Set the calculated value with the estimated accuracy
+            record.predicted(self.config.predict.NAME, y, accuracy)
+            # Yield the record to the caller
+            yield record
