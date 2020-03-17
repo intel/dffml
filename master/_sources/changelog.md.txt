3e047ace77cdea038ed42607eafa67f224606a07
John Andersen <johnandersenpdx@gmail.com>
2020-03-17 19:26:11 +0000

    docs: Tue Mar 17 19:26:11 UTC 2020
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- master/_sources/changelog.md.txt
+++ master/_sources/changelog.md.txt
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Docstrings and doctestable examples to `record.py`.
 - Inputs can be validated using operations
   - `validate` parameter in `Input` takes `Operation.instance_name`
+- Logistic Regression with SAG optimizer
 - Test tensorflow DNNEstimator documentation exaples in CI
 - Add python code for tensorflow DNNEstimator
 ### Fixed
---
+++ master/webui/precache-manifest.92a47105e58d59b3127e1a73813f372b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e078f1563703c8346f861efe64c0ce4",
+    "url": "/dffml/master/webui/index.html"
+  },
+  {
+    "revision": "92326e4d9f5cd15acf51",
+    "url": "/dffml/master/webui/static/js/2.11db232e.chunk.js"
+  },
+  {
+    "revision": "5c957da43e9d0adeb8bb75acf8ca36cc",
+    "url": "/dffml/master/webui/static/js/2.11db232e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "909d8bf32e89c81efdba",
+    "url": "/dffml/master/webui/static/js/main.42ea8103.chunk.js"
+  },
+  {
+    "revision": "1b58fa33c84c4e9c5341",
+    "url": "/dffml/master/webui/static/js/runtime-main.03e80777.js"
+  }
+]);
