82033fc13b265e18db1cde0b8b592bf083dfde18
@sk-ip <sudhanshukumar5459@gmail.com>
2020-04-05 18:31:46 +0530

    reformat conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -87,8 +87,10 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+
 def setup(app):
     app.add_javascript("copybutton.js")
+
 
 # -- Extension configuration -------------------------------------------------
 
