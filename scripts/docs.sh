a2c11e8fbfaa43bf6411fd8ef4d445db7632a354
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-04-06 07:46:55 +0000

    add link to downlod copybutton.js file

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -11,4 +11,5 @@ python3.7 scripts/docs.py
 python3.7 -c 'import os, pkg_resources; [e.load() for e in pkg_resources.iter_entry_points("console_scripts") if e.name.startswith("sphinx-build")][0]()' -b html docs pages \
   || (echo "[ERROR] Failed run sphinx, is it installed (pip install -U .[dev])?" 1>&2 ; exit 1)
 cp -r docs/images pages/
+wget -P pages/_static "https://raw.githubusercontent.com/python/python-docs-theme/master/python_docs_theme/static/copybutton.js"
 touch pages/.nojekyll
