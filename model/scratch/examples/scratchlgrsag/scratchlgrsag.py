315c849fb1ec2d2df2c4033b5b1dac58d2ef13e6
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-04-10 22:46:09 +0530

    docs: model: scratch: Python code examples
    Fixes: #434

---
+++ model/scratch/examples/scratchlgrsag/scratchlgrsag.py
@@ -0,0 +1,19 @@
+from dffml import CSVSource, Features, DefFeature
+from dffml.noasync import train, accuracy, predict
+from dffml_model_scratch.logisticregression import LogisticRegression
+
+model = LogisticRegression(
+    features=Features(DefFeature("f1", float, 1)),
+    predict=DefFeature("ans", int, 1),
+)
+
+# Train the model
+train(model, "dataset.csv")
+
+# Assess accuracy (alternate way of specifying data source)
+print("Accuracy:", accuracy(model, CSVSource(filename="dataset.csv")))
+
+# Make prediction
+for i, features, prediction in predict(model, {"f1": 0.8, "ans": 0}):
+    features["ans"] = prediction["ans"]["value"]
+    print(features)
