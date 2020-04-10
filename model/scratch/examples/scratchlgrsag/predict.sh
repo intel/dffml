e319c83c5ef7b0efcd2bdcf589e223e368a9575e
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-04-10 07:32:23 +0000

    add file for prediction

---
+++ model/scratch/examples/scratchlgrsag/predict.sh
@@ -0,0 +1,8 @@
+echo -e 'f1,ans\n0.8,0\n' | \
+  dffml predict all \
+  -model scratchlgrsag \
+  -model-features f1:float:1 \
+  -model-predict ans:int:1 \
+  -sources f=csv \
+  -source-filename /dev/stdin \
+  -log debug
