6ad7edfa3644d8304d9e7d5939a68862bda64023
Yash Varshney <b18038@students.iitmandi.ac.in>
2020-04-12 14:11:57 +0000

    update it to compressed version

--- examples/shouldi/tests/test_dependency_check.py
+++ examples/shouldi/tests/test_dependency_check.py
@@ -15,14 +15,14 @@ class TestRunDependencyCheckOp(AsyncTestCase):
         "d87ab7b623e17c85d763fd9bf810fc6de7d7c001facf238266bb316586081732cfd4b08d9fbaa83655cbdf9a4f497ac9",
     )
     @cached_download_unpack_archive(
-        "https://github.com/jeremylong/DependencyCheck/archive/v5.3.2.tar.gz",
+        "https://dl.bintray.com/jeremy-long/owasp/dependency-check-5.3.2-release.zip",
         pathlib.Path(__file__).parent
         / "downloads"
-        / "dependency_check.tar.gz",
+        / "dependency_check.zip",
         pathlib.Path(__file__).parent
         / "downloads"
         / "dependency_check-download",
-        "9f08a8e106e065cdd5d54f5b968c684250d9a188edb26a36ce49815a2494969a365508bbea704da1676aefa3b6409a20",
+        "02652657e658193369ccd38c000dfbdcbafdcbe991467a8d4f4ef6845ec7af1eae6e2739df6ec851b2d5684fede77c5b",
     )
     @cached_download_unpack_archive(
         "https://github.com/ReactiveX/RxJava/archive/v2.2.16.tar.gz",
@@ -33,7 +33,7 @@ class TestRunDependencyCheckOp(AsyncTestCase):
     async def test_run(self, java, dependency_check, RxJava):
         with prepend_to_path(
             java / "jdk-14" / "bin",
-            dependency_check / "DependencyCheck-5.3.2",
+            dependency_check / "dependency-check" / "bin",
         ):
             results = await run_dependency_check(str(RxJava / "RxJava-2.2.16"))
             self.assertEqual(type(results["report"]["total_CVE"]), 99)
