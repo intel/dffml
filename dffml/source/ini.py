da182690ad2ade8700a835f2d6dae0ae0afdb1ea
@sk-ip <sudhanshukumar5459@gmail.com>
2020-04-14 22:26:19 +0530

    fix comments and remove conflict line

--- dffml/source/ini.py
+++ dffml/source/ini.py
@@ -10,7 +10,6 @@ from ..util.entrypoint import entrypoint
 @config
 class INISourceConfig:
     filename: str
-    feature: str = field("Name of the feature the data will be referenced as")
     readwrite: bool = False
     allowempty: bool = False
 
@@ -24,40 +23,41 @@ class INISource(FileSource, MemorySource):
     CONFIG = INISourceConfig
 
     async def load_fd(self, ifile):
-        # creating an instance of configparser
+        # Creating an instance of configparser
         parser = ConfigParser()
-        # read from a file object
+        # Read from a file object
         parser.read_file(ifile)
-        # get all the sections present in the file
+        # Get all the sections present in the file
         sections = parser.sections()
 
         self.mem = {}
 
-        # go over each section
+        # Go over each section
         for section in sections:
-            # get the name and value pair under each section
+            # Get the name and value pair under each section
             for name, value in parser.items(section):
+                # Each section used as a record
                 self.mem[str(section)] = Record(
-                    str(section),  # each section used as a record
-                    data={"features": {str(name): str(value)}},
+                    str(section), data={"features": {str(name): str(value)}},
                 )
 
         self.logger.debug("%r loaded %d sections", self, len(self.mem))
 
     async def dump_fd(self, fd):
-        # create an instance of configparser
+        # Create an instance of configparser
         parser = ConfigParser()
 
-        # go over each section and record in mem
+        # Go over each section and record in mem
         for section, record in self.mem.items():
-            # get each section data as a dict
+            # Get each section data as a dict
             section_data = record.features()
             if section not in parser.keys():
-                # if section does not exist add new section
+                # If section does not exist add new section
                 parser.add_section(section)
-            parser[section] = section_data  # set section data
+            # Set section data
+            parser[section] = section_data
 
-        # write to the fileobject
+        # Write to the fileobject
         parser.write(fd)
 
         self.logger.debug("%r saved %d sections", self, len(self.mem))
