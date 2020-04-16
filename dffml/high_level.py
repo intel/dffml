06fa17abde322fb9b02c4e2f4b92929dd952e39f
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-04-17 00:06:17 +0530

    high level: Add load function
    Fixes: #564

--- dffml/high_level.py
+++ dffml/high_level.py
@@ -3,7 +3,7 @@ High level abstraction interfaces to DFFML. These are probably going to be used
 in a lot of quick and dirty python files.
 """
 import pathlib
-from typing import Union, Dict, Any
+from typing import Union, Dict, Any, AsyncIterator
 
 from .record import Record
 from .source.source import Sources, BaseSource
@@ -258,3 +258,60 @@ async def predict(
                 )
                 if update:
                     await sctx.update(record)
+
+
+async def load(source: BaseSource, *args: str) -> AsyncIterator[Record]:
+    """
+    Yields records from a source.
+
+    Yields all the records from the source, if record keys are given then only
+    those records are yielded.
+
+    Parameters
+    ----------
+    source : BaseSource
+        Data source to use. See :doc:`/plugins/dffml_source` for sources and
+        options.
+    *args : str
+        Records to be returned. If empty, all the records in a source will be returned.
+
+    Returns
+    -------
+    asynciterator
+        :py:class:`Record <dffml.record.Record>` object
+
+    Examples
+    --------
+
+    >>> source = CSVSource(filename="save.csv", allowempty=True, readwrite=True)
+    >>>
+    >>> async def main():
+    ...     await save(
+    ...         source,
+    ...         Record("1", data={"features": {"A": 0, "B": 1}}),
+    ...         Record("2", data={"features": {"A": 3, "B": 4}}),
+    ...     )
+    ...
+    ...     # All records in source
+    ...     async for record in load(source):
+    ...         print(record.export())
+    ...
+    ...     # For specific records in a source
+    ...     async for record in load(source, "1"):
+    ...         print(record.export())
+    >>>
+    >>> asyncio.run(main())
+    {'key': '1', 'features': {'A': 0, 'B': 1}, 'extra': {}}
+    {'key': '2', 'features': {'A': 3, 'B': 4}, 'extra': {}}
+    {'key': '1', 'features': {'A': 0, 'B': 1}, 'extra': {}}
+    """
+    async with source:
+        async with source() as sctx:
+            if args:
+                # If specific records are to be loaded
+                for record in args:
+                    yield await sctx.record(record)
+            else:
+                # All the records are loaded
+                async for record in sctx.records():
+                    yield record
