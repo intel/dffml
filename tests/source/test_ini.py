9c23be941a0a00dac855653e0ce9f5ba641c5e8a
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-04-18 08:13:22 +0000

    make single test

--- tests/source/test_ini.py
+++ tests/source/test_ini.py
@@ -9,13 +9,11 @@ from dffml.source.ini import INISource, INISourceConfig
 
 class TestINISource(AsyncTestCase):
     async def test_load_fd(self):
-        # Test the loading function
         with tempfile.TemporaryDirectory() as testdir:
+            self.testfile = os.path.join(testdir, "testfile.ini")
             # Create a source
             source = INISource(
-                filename=os.path.join(testdir, "testfile.ini"),
-                allowempty=True,
-                readwrite=True,
+                filename=self.testfile, allowempty=True, readwrite=True,
             )
             # Save some data in the source
             await save(
@@ -23,36 +21,10 @@ class TestINISource(AsyncTestCase):
                 Record("section1", data={"features": {"A": 1, "B": 2,}}),
                 Record("section2", data={"features": {"C": 3, "D": 4,}}),
             )
-            # Load the data
-            source.load("ini")
+            # Load all the records
+            records = [record async for record in load(source)]
 
-            # Test whether the data is loaded as expected or not
-            self.assertIsInstance(source.mem, dict)
-            self.assertIsInstance(source.mem["section1"], Record)
-            self.assertDictEqual(
-                source.mem["section1"].features(), {"A": 1, "B": 2,}
-            )
-            self.assertDictEqual(
-                source.mem["section2"].features(), {"C": 3, "D": 4,}
-            )
-
-    async def test_dump_fd(self):
-        with tempfile.TemporaryDirectory() as testdir:
-            self.testfile = os.path.join(testdir, "testfile.ini")
-            source = INISource(
-                filename=self.testfile, allowempty=True, readwrite=True,
-            )
-            dump_data = {
-                "section1": Record(
-                    "section1", data={"features": {"A": 1, "B": 2,}}
-                ),
-                "section2": Record(
-                    "section2", data={"features": {"C": 3, "D": 4,}}
-                ),
-            }
-            source.mem = dump_data
-            with open(self.testfile, "w") as tfile:
-                await source.dump_fd(tfile)
-
-            allrecords = [record async for record in load(source)]
-            print(allrecords)
+            self.assertIsInstance(records, list)
+            self.assertEqual(len(records), 2)
+            self.assertDictEqual(records[0].features(), {"A": 1, "B": 2})
+            self.assertDictEqual(records[1].features(), {"C": 3, "D": 4})
