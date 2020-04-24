9094320b90e0140066c0198ba00a5ad4370a0795
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-04-24 20:38:23 +0530

    source: ini: Source for parsing .ini files
    Fixes: #551

---
+++ dffml/source/ini.py
@@ -0,0 +1,66 @@
+from configparser import ConfigParser
+
+from ..base import config
+from ..record import Record
+from .file import FileSource
+from .memory import MemorySource
+from ..util.data import parser_helper
+from ..util.entrypoint import entrypoint
+
+
+@config
+class INISourceConfig:
+    filename: str
+    readwrite: bool = False
+    allowempty: bool = False
+
+
+@entrypoint("ini")
+class INISource(FileSource, MemorySource):
+    """
+    Source to read files in .ini format.
+    """
+
+    CONFIG = INISourceConfig
+
+    async def load_fd(self, ifile):
+        # Creating an instance of configparser
+        parser = ConfigParser()
+        # Read from a file object
+        parser.read_file(ifile)
+        # Get all the sections present in the file
+        sections = parser.sections()
+
+        self.mem = {}
+
+        # Go over each section
+        for section in sections:
+            # Get data under each section as a dict
+            temp_dict = {}
+            for k, v in parser.items(section):
+                temp_dict[k] = parser_helper(v)
+            # Each section used as a record
+            self.mem[str(section)] = Record(
+                str(section), data={"features": temp_dict},
+            )
+
+        self.logger.debug("%r loaded %d sections", self, len(self.mem))
+
+    async def dump_fd(self, fd):
+        # Create an instance of configparser
+        parser = ConfigParser()
+
+        # Go over each section and record in mem
+        for section, record in self.mem.items():
+            # Get each section data as a dict
+            section_data = record.features()
+            if section not in parser.keys():
+                # If section does not exist add new section
+                parser.add_section(section)
+            # Set section data
+            parser[section] = section_data
+
+        # Write to the fileobject
+        parser.write(fd)
+
+        self.logger.debug("%r saved %d sections", self, len(self.mem))
