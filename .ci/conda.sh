0a4cca91545d4d92d7e86c4757efd2f0fc39d4f3
0dust <himanshutripathi366@gmail.com>
2020-04-29 23:38:46 +0530

    add conda

--- .ci/conda.sh
+++ .ci/conda.sh
@@ -2,16 +2,14 @@
 set -ex
 
 SRC_ROOT=${SRC_ROOT:-"${PWD}"}
-CONTAINER_NAME='anaconda_docker'
+CONTAINER_NAME='conda_docker'
 if [ $(docker inspect --format='{{.State.Running}}' ${CONTAINER_NAME} 2>/dev/null) = "true" ]; then
   echo "Container already running"
   echo $PWD
-  docker exec ${CONTAINER_NAME} "${SRC_ROOT}/.ci/run.sh";
+  docker exec ${CONTAINER_NAME} ./.ci/run.sh;
 else
   echo "Starting new container"
   echo $PWD
   echo $SRC_ROOT
-  ls
-#   cat "${SRC_ROOT}/.ci/run.sh"
-  sudo docker run --name ${CONTAINER_NAME} --rm -w ${SRC_ROOT} --entrypoint /bin/bash continuumio/anaconda3 -c ../.ci/run.sh ;
+  sudo docker run --name $CONTAINER_NAME --rm -ti -v $SRC_ROOT:$SRC_ROOT -w $SRC_ROOT --entrypoint /bin/bash continuumio/anaconda3 -c ./.ci/run.sh
 fi
