19ed864fd2cb3e874762a987a5bb55dead15560e
sudharsana-kjl <kjlsudharsana@gmail.com>
2020-05-10 07:51:43 +0530

    modify dffml/source/file.py

--- dffml/source/file.py
+++ dffml/source/file.py
@@ -18,7 +18,7 @@ from ..util.entrypoint import entrypoint
 
 @config
 class FileSourceConfig:
-    filename: pathlib.Path
+    filename: str
     tag: str = "untagged"
     readwrite: bool = False
     allowempty: bool = False
@@ -52,95 +52,99 @@ class FileSource(BaseSource):
     async def _empty_file_init(self):
         return {}
 
-    async def _open(self):
-        if not self.config.filename.is_file() or self.config.filename.is_dir():
-            if self.config.allowempty:
-                self.logger.debug(
-                    ("%r is not a file, " % (self.config.filename,))
-                    + "initializing memory to empty dict"
-                )
-                self.mem = await self._empty_file_init()
-                return
+        async def _open(self):
+            if not os.path.exists(self.config.filename) or os.path.isdir(
+                    self.config.filename
+            ):
+                if self.config.allowempty:
+                    self.logger.debug(
+                        ("%r is not a file, " % (self.config.filename,))
+                        + "initializing memory to empty dict"
+                    )
+                    self.mem = await self._empty_file_init()
+                    return
+                else:
+                    raise FileNotFoundError(
+                        errno.ENOENT,
+                        os.strerror(errno.ENOENT),
+                        self.config.filename,
+                    )
+            if self.config.filename[::-1].startswith((".gz")[::-1]):
+                opener = gzip.open(self.config.filename, self.READMODE_COMPRESSED)
+            elif self.config.filename[::-1].startswith((".bz2")[::-1]):
+                opener = bz2.open(self.config.filename, self.READMODE_COMPRESSED)
+            elif self.config.filename[::-1].startswith(
+                    (".xz")[::-1]
+            ) or self.config.filename[::-1].startswith((".lzma")[::-1]):
+                opener = lzma.open(self.config.filename, self.READMODE_COMPRESSED)
+            elif self.config.filename[::-1].startswith((".zip")[::-1]):
+                opener = self.zip_opener_helper()
             else:
-                raise FileNotFoundError(
-                    errno.ENOENT,
-                    os.strerror(errno.ENOENT),
-                    self.config.filename,
-                )
-        if self.config.filename.suffix == ".gz":
-            opener = gzip.open(self.config.filename, self.READMODE_COMPRESSED)
-        elif self.config.filename.suffix == ".bz2":
-            opener = bz2.open(self.config.filename, self.READMODE_COMPRESSED)
-        elif self.config.filename.suffix == ".xz" or self.config.filename.suffix == ".lzma":
-            opener = lzma.open(self.config.filename, self.READMODE_COMPRESSED)
-        elif self.config.filename.suffix == ".zip":
-            opener = self.zip_opener_helper()
-        else:
-            opener = open(self.config.filename, self.READMODE)
-        with opener as fd:
-            await self.load_fd(fd)
+                opener = open(self.config.filename, self.READMODE)
+            with opener as fd:
+                await self.load_fd(fd)
 
-    async def _close(self):
-        if self.config.readwrite:
-            if self.config.filename.suffix == ".gz":
-                close = gzip.open(
-                    self.config.filename, self.WRITEMODE_COMPRESSED
-                )
-            elif self.config.filename.suffix == ".bz2":
-                close = bz2.open(
-                    self.config.filename, self.WRITEMODE_COMPRESSED
-                )
-            elif self.config.filename.suffix == ".xz" or self.config.filename.suffix == ".lzma":
-                close = lzma.open(
-                    self.config.filename, self.WRITEMODE_COMPRESSED
-                )
-            elif self.config.filename.suffix == ".zip":
-                close = self.zip_closer_helper()
-            else:
-                close = open(self.config.filename, "w+")
-            with close as fd:
-                await self.dump_fd(fd)
+        async def _close(self):
+            if self.config.readwrite:
+                if self.config.filename[::-1].startswith((".gz")[::-1]):
+                    close = gzip.open(
+                        self.config.filename, self.WRITEMODE_COMPRESSED
+                    )
+                elif self.config.filename[::-1].startswith((".bz2")[::-1]):
+                    close = bz2.open(
+                        self.config.filename, self.WRITEMODE_COMPRESSED
+                    )
+                elif self.config.filename[::-1].startswith(
+                        (".xz")[::-1]
+                ) or self.config.filename[::-1].startswith((".lzma")[::-1]):
+                    close = lzma.open(
+                        self.config.filename, self.WRITEMODE_COMPRESSED
+                    )
+                elif self.config.filename[::-1].startswith((".zip")[::-1]):
+                    close = self.zip_closer_helper()
+                else:
+                    close = open(self.config.filename, "w+")
+                with close as fd:
+                    await self.dump_fd(fd)
 
-    @contextmanager
-    def zip_opener_helper(self):
-        with zipfile.ZipFile(self.config.filename) as archive:
-            with archive.open(
-                self.__class__.__qualname__, mode=self.READMODE
-            ) as zip_fd:
-                with io.TextIOWrapper(zip_fd, write_through=True) as fd:
-                    yield fd
+        @contextmanager
+        def zip_opener_helper(self):
+            with zipfile.ZipFile(self.config.filename) as archive:
+                with archive.open(
+                        self.__class__.__qualname__, mode=self.READMODE
+                ) as zip_fd:
+                    with io.TextIOWrapper(zip_fd, write_through=True) as fd:
+                        yield fd
 
-    @contextmanager
-    def zip_closer_helper(self):
-        with zipfile.ZipFile(
-            self.config.filename, self.WRITEMODE, compression=zipfile.ZIP_BZIP2
-        ) as archive:
-            with archive.open(
-                self.__class__.__qualname__,
-                mode=self.WRITEMODE,
-                force_zip64=True,
-            ) as zip_fd:
-                with io.TextIOWrapper(zip_fd, write_through=True) as fd:
-                    yield fd
+        @contextmanager
+        def zip_closer_helper(self):
+            with zipfile.ZipFile(
+                    self.config.filename, self.WRITEMODE, compression=zipfile.ZIP_BZIP2
+            ) as archive:
+                with archive.open(
+                        self.__class__.__qualname__,
+                        mode=self.WRITEMODE,
+                        force_zip64=True,
+                ) as zip_fd:
+                    with io.TextIOWrapper(zip_fd, write_through=True) as fd:
+                        yield fd
 
-    @abc.abstractmethod
-    async def load_fd(self, fd):
-        pass  # pragma: no cover
+        @abc.abstractmethod
+        async def load_fd(self, fd):
+            pass  # pragma: no cover
 
-    @abc.abstractmethod
-    async def dump_fd(self, fd):
-        pass  # pragma: no cover
+        @abc.abstractmethod
+        async def dump_fd(self, fd):
+            pass  # pragma: no cover
 
+    @config
+    class BinaryFileSourceConfig(FileSourceConfig):
+        pass
 
-@config
-class BinaryFileSourceConfig(FileSourceConfig):
-    pass
-
-
-@entrypoint("binaryfile")
-class BinaryFileSource(FileSource):
-    CONFIG = BinaryFileSourceConfig
-    READMODE: str = "rb"
-    WRITEMODE: str = "wb"
-    READMODE_COMPRESSED: str = "rb"
-    WRITEMODE_COMPRESSED: str = "wb"
+    @entrypoint("binaryfile")
+    class BinaryFileSource(FileSource):
+        CONFIG = BinaryFileSourceConfig
+        READMODE: str = "rb"
+        WRITEMODE: str = "wb"
+        READMODE_COMPRESSED: str = "rb"
+        WRITEMODE_COMPRESSED: str = "wb"
