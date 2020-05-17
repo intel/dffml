1802c730cc599c724ab54fb1e3a4fe6a3a368499
John Andersen <johnandersenpdx@gmail.com>
2020-05-16 18:15:22 -0700

    operation: binsec: Remove bad rpmfind link
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- operations/binsec/tests/test_operations.py
+++ operations/binsec/tests/test_operations.py
@@ -30,7 +30,6 @@ class TestRunner(AsyncTestCase):
             "https://download.clearlinux.org/releases/10540/clear/x86_64/os/Packages/sudo-setuid-1.8.17p1-34.x86_64.rpm": {
                 "./usr/bin/sudo": True
             },
-            "https://rpmfind.net/linux/fedora/linux/updates/29/Everything/x86_64/Packages/g/gzip-1.9-9.fc29.x86_64.rpm": {},
             "https://archives.fedoraproject.org/pub/archive/fedora/linux/releases/20/Everything/x86_64/os/Packages/c/curl-7.32.0-3.fc20.x86_64.rpm": {
                 "./usr/bin/curl": False
             },
