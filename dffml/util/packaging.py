b3397853e3bea09a46e3ad1cc9aef13c32dee5f3
John Andersen <johnandersenpdx@gmail.com>
2020-05-16 22:54:53 -0700

    util: packaging: Check for module dir in syspath
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/util/packaging.py
+++ dffml/util/packaging.py
@@ -3,12 +3,17 @@ import pathlib
 from typing import Union
 
 
-def is_develop(pacakge_name: str) -> Union[bool, pathlib.Path]:
+def is_develop(package_name: str) -> Union[bool, pathlib.Path]:
     """
     Returns True if the package is installed in development mode.
     """
     for syspath in map(pathlib.Path, sys.path):
-        egg_link = syspath / f"{pacakge_name}.egg-link"
+        # Check if egg-link is present
+        egg_link = syspath / f"{package_name}.egg-link"
         if egg_link.is_file():
             return pathlib.Path(egg_link.read_text().split("\n")[0])
+        # Check if path entry is parent of module directory itself
+        module_dir = syspath / f"{package_name.replace('-', '_')}"
+        if module_dir.is_dir():
+            return syspath
     return False
