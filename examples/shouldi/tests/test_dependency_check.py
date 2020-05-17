75b698d7f01473464ed57daae0297ad7f7d8d859
John Andersen <johnandersenpdx@gmail.com>
2020-05-17 09:07:17 -0700

    shouldi: tests: dependency check: Move binaries to binaries.py
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- examples/shouldi/tests/test_dependency_check.py
+++ examples/shouldi/tests/test_dependency_check.py
@@ -1,36 +1,21 @@
 import pathlib
 
-from dffml import (
-    prepend_to_path,
-    cached_download_unpack_archive,
-    AsyncTestCase,
-)
+from dffml import prepend_to_path, AsyncTestCase
 
 from shouldi.java.dependency_check import run_dependency_check
 
+from .binaries import (
+    cached_openjdk,
+    cached_dependency_check,
+    cached_target_rxjava,
+)
+
 
 class TestRunDependencyCheckOp(AsyncTestCase):
-    @cached_download_unpack_archive(
-        "https://download.java.net/openjdk/jdk14/ri/openjdk-14+36_linux-x64_bin.tar.gz",
-        pathlib.Path(__file__).parent / "downloads" / "java.tar.gz",
-        pathlib.Path(__file__).parent / "downloads" / "java-download",
-        "d87ab7b623e17c85d763fd9bf810fc6de7d7c001facf238266bb316586081732cfd4b08d9fbaa83655cbdf9a4f497ac9",
-    )
-    @cached_download_unpack_archive(
-        "https://dl.bintray.com/jeremy-long/owasp/dependency-check-5.3.2-release.zip",
-        pathlib.Path(__file__).parent / "downloads" / "dependency_check.zip",
-        pathlib.Path(__file__).parent
-        / "downloads"
-        / "dependency_check-download",
-        "02652657e658193369ccd38c000dfbdcbafdcbe991467a8d4f4ef6845ec7af1eae6e2739df6ec851b2d5684fede77c5b",
-    )
-    @cached_download_unpack_archive(
-        "https://github.com/ReactiveX/RxJava/archive/v2.2.16.tar.gz",
-        pathlib.Path(__file__).parent / "downloads" / "RxJava.tar.gz",
-        pathlib.Path(__file__).parent / "downloads" / "RxJava-download",
-        "2a15b4eb165e36a3de35e0d53f90b99bb328e3c18b7ef4f0a6c253d3898e794dec231fc726e154f339151eb8cf5ee5bb",
-    )
-    async def test_run(self, java, dependency_check, RxJava):
+    @cached_openjdk
+    @cached_dependency_check
+    @cached_target_rxjava
+    async def test_run(self, java, dependency_check, rxjava):
         with prepend_to_path(
             java / "jdk-14" / "bin",
             dependency_check / "dependency-check" / "bin",
@@ -41,5 +26,5 @@ class TestRunDependencyCheckOp(AsyncTestCase):
                 / "bin"
                 / "dependency-check.sh"
             ).chmod(0o755)
-            results = await run_dependency_check(str(RxJava / "RxJava-2.2.16"))
+            results = await run_dependency_check(str(rxjava / "RxJava-2.2.16"))
             self.assertEqual(results["report"]["total_CVE"], 3)
