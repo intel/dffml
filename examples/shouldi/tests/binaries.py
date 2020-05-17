4842e127cc4f76dd477a9d3896b08a3befbf2f6b
John Andersen <johnandersenpdx@gmail.com>
2020-05-17 07:44:57 -0700

    shouldi: tests: cli: use: Move binaries to binaries.py
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ examples/shouldi/tests/binaries.py
@@ -0,0 +1,18 @@
+import pathlib
+from dffml import cached_download_unpack_archive
+
+downloads = pathlib.Path(__file__).parent / "downloads"
+
+cached_node = cached_download_unpack_archive(
+    "https://nodejs.org/dist/v14.2.0/node-v14.2.0-linux-x64.tar.xz",
+    downloads / "node.tar.gz",
+    downloads / "node-download",
+    "fa2a9dfa4d0f99a0cc3ee6691518c026887677a0d565b12ebdcf9d78341db2066427c9970c41cbf72776a370bbb42729",
+)
+
+cached_target_javascript_algorithms = cached_download_unpack_archive(
+    "https://github.com/trekhleb/javascript-algorithms/archive/ba2d8dc4a8e27659c1420fe52390cb7981df4a94.tar.gz",
+    downloads / "javascript_algo.tar.gz",
+    downloads / "javascript_algo-download",
+    "36b3ce51780ee6ea8dcec266c9d09e3a00198868ba1b041569950b82cf45884da0c47ec354dd8514022169849dfe8b7c",
+)
--- examples/shouldi/tests/test_cli_use.py
+++ examples/shouldi/tests/test_cli_use.py
@@ -2,13 +2,11 @@ import io
 import pathlib
 from unittest.mock import patch
 
-from dffml import (
-    prepend_to_path,
-    cached_download_unpack_archive,
-    AsyncTestCase,
-)
+from dffml import prepend_to_path, AsyncTestCase
 
 from shouldi.cli import ShouldI
+
+from .binaries import cached_node, cached_target_javascript_algorithms
 
 
 class TestCLIUse(AsyncTestCase):
@@ -22,20 +20,8 @@ class TestCLIUse(AsyncTestCase):
             output = stdout.getvalue()
         self.assertIn("high=1", output)
 
-    @cached_download_unpack_archive(
-        "https://nodejs.org/dist/v14.2.0/node-v14.2.0-linux-x64.tar.xz",
-        pathlib.Path(__file__).parent / "downloads" / "node.tar.gz",
-        pathlib.Path(__file__).parent / "downloads" / "node-download",
-        "fa2a9dfa4d0f99a0cc3ee6691518c026887677a0d565b12ebdcf9d78341db2066427c9970c41cbf72776a370bbb42729",
-    )
-    @cached_download_unpack_archive(
-        "https://github.com/trekhleb/javascript-algorithms/archive/ba2d8dc4a8e27659c1420fe52390cb7981df4a94.tar.gz",
-        pathlib.Path(__file__).parent / "downloads" / "javascript_algo.tar.gz",
-        pathlib.Path(__file__).parent
-        / "downloads"
-        / "javascript_algo-download",
-        "36b3ce51780ee6ea8dcec266c9d09e3a00198868ba1b041569950b82cf45884da0c47ec354dd8514022169849dfe8b7c",
-    )
+    @cached_node
+    @cached_target_javascript_algorithms
     async def test_use_javascript(self, node, javascript_algo):
         with prepend_to_path(node / "node-v14.2.0-linux-x64" / "bin",):
             with patch("sys.stdout", new_callable=io.StringIO) as stdout:
