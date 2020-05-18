0e0ffd4f2420f9ff69812331544aa579b895ee14
John Andersen <johnandersenpdx@gmail.com>
2020-05-18 03:41:08 +0000

    docs: Mon May 18 03:41:08 UTC 2020
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ master/webui/precache-manifest.b75a3ca9c6c8b9e82b82470844c67100.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb44ef17025500131b99f21dfbc52f8a",
+    "url": "/dffml/master/webui/index.html"
+  },
+  {
+    "revision": "15bd184d42b0fb9a44ab",
+    "url": "/dffml/master/webui/static/js/2.8ea53da7.chunk.js"
+  },
+  {
+    "revision": "5c957da43e9d0adeb8bb75acf8ca36cc",
+    "url": "/dffml/master/webui/static/js/2.8ea53da7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbd6037a8fbd6b04f7cd",
+    "url": "/dffml/master/webui/static/js/main.6b1a183a.chunk.js"
+  },
+  {
+    "revision": "1b58fa33c84c4e9c5341",
+    "url": "/dffml/master/webui/static/js/runtime-main.03e80777.js"
+  }
+]);
