356ab9d95a2d81fc8e8ab664fb5dfa167424382a
Saksham Arora <50872172+sakshamarora1@users.noreply.github.com>
2020-05-20 04:50:24 +0530

    cli: edit: Edit records using DataFlowSource
    - Split edit into edit record and edit all
    - Added dffml edit all command that edits all the records in any
      source using DataFlowSource
    
    Fixes: #635
    Fixes: #636
    
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ dffml/operation/math.py
@@ -0,0 +1,70 @@
+from ..df.types import Definition
+from ..df.base import op
+
+# Definitions
+multiplicand_def = Definition(name="multiplicand_def", primitive="generic")
+multiplier_def = Definition(name="multiplier_def", primitive="generic")
+product_def = Definition(name="product", primitive="generic")
+
+
+@op(
+    name="multiply",
+    inputs={"multiplicand": multiplicand_def, "multiplier": multiplier_def},
+    outputs={"product": product_def},
+)
+async def multiply(multiplicand, multiplier):
+    """
+    Multiply record values
+
+    Parameters
+    ++++++++++
+    multiplicand : generic
+        An arithmetic type value.
+    multiplier : generic
+        An arithmetic type value.
+
+    Returns
+    +++++++
+    dict
+        A dict containing the product.
+
+    Examples
+    ++++++++
+
+    The following example shows how to use multiply.
+
+    >>> import asyncio
+    >>> from dffml import *
+    >>> 
+    >>> dataflow = DataFlow.auto(multiply, GetSingle)
+    >>> dataflow.seed.append(
+    ...    Input(
+    ...        value=[multiply.op.outputs["product"].name,],
+    ...        definition=GetSingle.op.inputs["spec"],
+    ...    )
+    ... )
+    >>> inputs = [
+    ...    Input(
+    ...        value=12,
+    ...        definition=multiply.op.inputs["multiplicand"],
+    ...    ),
+    ...    Input(
+    ...        value=3,
+    ...        definition=multiply.op.inputs["multiplier"],
+    ...    ),
+    ... ]
+    >>> 
+    >>> async def main():
+    ...     async for ctx, results in MemoryOrchestrator.run(dataflow, inputs):
+    ...         print(results)
+    >>> 
+    >>> asyncio.run(main())
+    {'product': 36}
+    """
+    if isinstance(multiplicand, (list, tuple,)):
+        value = type(multiplicand)(
+            [element * multiplier for element in multiplicand]
+        )
+    else:
+        value = multiplicand * multiplier
+    return {"product": value}
