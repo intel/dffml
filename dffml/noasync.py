bb81689d33be18fb4c1953e64a6a2a4a4227e59b
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-05-21 21:22:47 +0530

    noasync: Expose high level save and load
    Expose high_level save and load functions from noasync. So that they can
    be used without being asynchronous.
    
    Fixes: #659
    
    Signed-off-by: Sudhanshu <Sudhanshukumar5459@gmail.com>
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/noasync.py
+++ dffml/noasync.py
@@ -66,3 +66,51 @@ predict.__doc__ = (
         "    >>> from dffml import *\n    >>> from dffml.noasync import *\n",
     )
 )
+
+
+def save(*args, **kwargs):
+    return asyncio.run(high_level.save(*args, **kwargs))
+
+
+save.__doc__ = (
+    high_level.save.__doc__.replace("await ", "")
+    .replace("async ", "")
+    .replace("asyncio.run(main())", "main()")
+    .replace("    >>> import asyncio\n", "")
+    .replace(
+        "    >>> from dffml import *\n",
+        "    >>> from dffml import *\n    >>> from dffml.noasync import *\n",
+    )
+)
+
+
+def load(*args, **kwargs):
+    async_gen = high_level.load(*args, **kwargs).__aiter__()
+
+    loop = asyncio.new_event_loop()
+
+    def cleanup():
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
+
+    while True:
+        try:
+            yield loop.run_until_complete(async_gen.__anext__())
+        except StopAsyncIteration:
+            cleanup()
+            return
+        except:
+            cleanup()
+            raise
+
+
+load.__doc__ = (
+    high_level.load.__doc__.replace("await ", "")
+    .replace("async ", "")
+    .replace("asyncio.run(main())", "main()")
+    .replace("    >>> import asyncio\n", "")
+    .replace(
+        "    >>> from dffml import *\n",
+        "    >>> from dffml import *\n    >>> from dffml.noasync import *\n",
+    )
+)
