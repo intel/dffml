591a397d273b26a07c85c6fe93859b21ba3e87e8
John Andersen <johnandersenpdx@gmail.com>
2020-05-22 15:09:43 -0700

    tests: df: create: Refactor
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- tests/df/test_df_create.py
+++ tests/df/test_df_create.py
@@ -16,57 +16,50 @@ from dffml import (
     AsyncTestCase,
 )
 
-OP_DEF_STRING = """
+ECHO_STRING = """
 def echo_string(input_string: str) -> str:
     return "Echo: " + input_string
 """
 
 
 class TestDataflowCreate(AsyncTestCase):
-    async def test_create_from_path(self):
+    @staticmethod
+    @contextlib.asynccontextmanager
+    async def make_dataflow(ops, operations, seed):
         # Create temp dir and write op to ops.py
         with tempfile.TemporaryDirectory() as tmpdirname:
             # Change directory into the tempdir
             with chdir(tmpdirname):
                 # Write out op to op.py
-                operation_file_path = pathlib.Path(tmpdirname, "ops.py")
-                operation_file_path.write_text(OP_DEF_STRING)
-                # We make the name the path relative to our cwd
-                operation_qualname = "ops:echo_string"
-                dataflow_file_path = pathlib.Path(tmpdirname, "dataflow.json")
-                # $ dffml dataflow create  \
-                #    ops:echo_string get_single
+                pathlib.Path(tmpdirname, "ops.py").write_text(ops)
+                # $ dffml dataflow create $operations -seed $seed
                 with io.StringIO() as dataflow:
                     with contextlib.redirect_stdout(dataflow):
                         await CLI.cli(
-                            "dataflow",
-                            "create",
-                            *[operation_qualname, "get_single"],
-                            "-seed",
-                            "ops.echo_string.outputs.result,=get_single_spec",
+                            "dataflow", "create", *operations, "-seed", *seed
                         )
-                    test_dataflow = DataFlow._fromdict(
-                        **json.loads(dataflow.getvalue())
-                    )
-                # Make sure the operation is in the dataflow
-                self.assertIn(operation_qualname, test_dataflow.operations)
-                # Definitions for shorthand access
-                idef = test_dataflow.operations[operation_qualname].inputs[
-                    "input_string"
-                ]
-                odef = test_dataflow.operations[operation_qualname].outputs[
-                    "result"
-                ]
-                # Run the dataflow
-                async for ctx_str, results in run(
-                    test_dataflow,
-                    [
-                        Input(
-                            value="Irregular at magic school", definition=idef,
-                        )
-                    ],
-                ):
-                    self.assertIn(odef.name, results)
-                    self.assertEqual(
-                        results[odef.name], "Echo: Irregular at magic school",
-                    )
+                    yield DataFlow._fromdict(**json.loads(dataflow.getvalue()))
+
+    async def test_single(self):
+        operation_qualname = "ops:echo_string"
+        async with self.make_dataflow(
+            ECHO_STRING,
+            [operation_qualname, "get_single"],
+            ["ops.echo_string.outputs.result,=get_single_spec"],
+        ) as dataflow:
+            # Make sure the operation is in the dataflow
+            self.assertIn(operation_qualname, dataflow.operations)
+            # Definitions for shorthand access
+            idef = dataflow.operations[operation_qualname].inputs[
+                "input_string"
+            ]
+            odef = dataflow.operations[operation_qualname].outputs["result"]
+            # Run the dataflow
+            async for ctx_str, results in run(
+                dataflow,
+                [Input(value="Irregular at magic school", definition=idef,)],
+            ):
+                self.assertIn(odef.name, results)
+                self.assertEqual(
+                    results[odef.name], "Echo: Irregular at magic school",
+                )
