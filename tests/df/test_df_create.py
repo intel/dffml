7bb4f6ec1965aada483ec14db11381d22e601bce
John Andersen <johnandersenpdx@gmail.com>
2020-05-23 17:15:04 -0700

    tests: df: create: Test for entrypoint load style async gen
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- tests/df/test_df_create.py
+++ tests/df/test_df_create.py
@@ -1,7 +1,9 @@
 import io
+import sys
 import json
 import pathlib
 import tempfile
+import importlib
 import contextlib
 
 from dffml.cli.cli import CLI
@@ -21,6 +23,14 @@ def echo_string(input_string: str) -> str:
     return "Echo: " + input_string
 """
 
+ECHO_STRINGS = """
+from typing import AsyncIterator
+
+async def echo_strings(input_string: str) -> AsyncIterator[str]:
+    for i in range(0, 5):
+        yield f"Echo({i}): {input_string}"
+"""
+
 
 class TestDataflowCreate(AsyncTestCase):
     @staticmethod
@@ -32,6 +42,11 @@ class TestDataflowCreate(AsyncTestCase):
             with chdir(tmpdirname):
                 # Write out op to op.py
                 pathlib.Path(tmpdirname, "ops.py").write_text(ops)
+                # Reload conents
+                sys.path.insert(0, tmpdirname)
+                module = importlib.import_module("ops")
+                importlib.reload(module)
+                sys.path.pop(0)
                 # $ dffml dataflow create $operations -seed $seed
                 with io.StringIO() as dataflow:
                     with contextlib.redirect_stdout(dataflow):
@@ -62,4 +77,32 @@ class TestDataflowCreate(AsyncTestCase):
                 self.assertIn(odef.name, results)
                 self.assertEqual(
                     results[odef.name], "Echo: Irregular at magic school",
+                )
+
+    async def test_gen(self):
+        operation_qualname = "ops:echo_strings"
+        async with self.make_dataflow(
+            ECHO_STRINGS,
+            [operation_qualname, "get_multi"],
+            ["ops.echo_strings.outputs.result,=get_multi_spec"],
+        ) as dataflow:
+            # Make sure the operation is in the dataflow
+            self.assertIn(operation_qualname, dataflow.operations)
+            # Definitions for shorthand access
+            idef = dataflow.operations[operation_qualname].inputs[
+                "input_string"
+            ]
+            odef = dataflow.operations[operation_qualname].outputs["result"]
+            # Run the dataflow
+            async for ctx_str, results in run(
+                dataflow,
+                [Input(value="Irregular at magic school", definition=idef,)],
+            ):
+                self.assertIn(odef.name, results)
+                self.assertListEqual(
+                    results[odef.name],
+                    [
+                        f"Echo({i}): Irregular at magic school"
+                        for i in range(0, 5)
+                    ],
                 )
