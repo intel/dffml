e2cc209dc6b32dc0f0cc000c867dc733b7f82c61
John Andersen <johnandersenpdx@gmail.com>
2020-06-02 09:27:36 -0700

    tests: secret: ini: Close and re-open before attemping get
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- tests/secret/test_ini.py
+++ tests/secret/test_ini.py
@@ -1,5 +1,7 @@
 import io
 import os
+import pathlib
+import inspect
 import tempfile
 import contextlib
 
@@ -14,14 +16,33 @@ class TestINISecret(AsyncTestCase):
             prefix="secret_", suffix=".ini"
         )
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.secret_file.close()
+
     async def test_set_get(self):
-        secret_store = INISecret(filename=self.secret_file.name)
-        async with secret_store() as secret_ctx:
-            await secret_ctx.set(name="foo", value="bar")
-            await secret_ctx.set(name="steins", value="gate")
-            value = await secret_ctx.get(name="foo")
-            self.assertEqual(value, "bar")
-            value = await secret_ctx.get(name="steins")
-            self.assertEqual(value, "gate")
-            value = await secret_ctx.get(name="non_existent")
-            self.assertFalse(value)
+        async with INISecret(filename=self.secret_file.name) as secret_store:
+            async with secret_store() as secret_ctx:
+                await secret_ctx.set(name="foo", value="bar")
+                await secret_ctx.set(name="steins", value="gate")
+
+        contents = pathlib.Path(self.secret_file.name).read_text().strip()
+
+        self.assertIn(
+            "[secrets]", contents,
+        )
+        self.assertIn(
+            "steins = gate", contents,
+        )
+        self.assertIn(
+            "foo = bar", contents,
+        )
+
+        async with INISecret(filename=self.secret_file.name) as secret_store:
+            async with secret_store() as secret_ctx:
+                value = await secret_ctx.get(name="foo")
+                self.assertEqual(value, "bar")
+                value = await secret_ctx.get(name="steins")
+                self.assertEqual(value, "gate")
+                value = await secret_ctx.get(name="non_existent")
+                self.assertFalse(value)
