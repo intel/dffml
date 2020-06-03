700367f026f61aee28ebb4970b216cb6307cb30a
John Andersen <johnandersenpdx@gmail.com>
2020-06-03 14:25:13 -0700

    source: df: Create orchestrator context
    By creating the orchestrator context within the source context, we get
    about a 6x speedup.
    
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/source/df.py
+++ dffml/source/df.py
@@ -4,6 +4,7 @@ from typing import Type, AsyncIterator
 from dffml.base import config, BaseConfig
 from dffml.configloader.configloader import BaseConfigLoader
 from dffml.df.types import Definition, DataFlow, Input
+from dffml.df.base import BaseOrchestrator
 from dffml.feature import Features
 from dffml.record import Record
 from dffml.source.source import BaseSource, BaseSourceContext
@@ -12,10 +13,11 @@ from dffml.df.memory import MemoryOrchestrator
 
 
 @config
-class DataFlowSourceConfig(BaseConfig):
+class DataFlowSourceConfig:
     source: BaseSource
     dataflow: DataFlow
     features: Features
+    orchestrator: BaseOrchestrator = MemoryOrchestrator.withconfig({})
 
 
 class DataFlowSourceContext(BaseSourceContext):
@@ -24,8 +26,7 @@ class DataFlowSourceContext(BaseSourceContext):
 
     async def records(self) -> AsyncIterator[Record]:
         async for record in self.sctx.records():
-            async for ctx, result in MemoryOrchestrator.run(
-                self.parent.config.dataflow,
+            async for ctx, result in self.octx.run(
                 [
                     Input(
                         value=record.feature(feature.name),
@@ -34,7 +35,7 @@ class DataFlowSourceContext(BaseSourceContext):
                         ),
                     )
                     for feature in self.parent.config.features
-                ],
+                ]
             ):
                 if result:
                     record.evaluated(result)
@@ -52,9 +53,14 @@ class DataFlowSourceContext(BaseSourceContext):
                     exported = await loader.loadb(dataflow_path.read_bytes())
                 self.parent.config.dataflow = DataFlow._fromdict(**exported)
 
+        self.octx = await self.parent.orchestrator(
+            self.parent.config.dataflow
+        ).__aenter__()
+
         return self
 
     async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.octx.__aexit__(exc_type, exc_value, traceback)
         await self.sctx.__aexit__(exc_type, exc_value, traceback)
 
 
@@ -68,7 +74,9 @@ class DataFlowSource(BaseSource):
 
     async def __aenter__(self) -> "DataFlowSource":
         self.source = await self.config.source.__aenter__()
+        self.orchestrator = await self.config.orchestrator.__aenter__()
         return self
 
     async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.orchestrator.__aexit__(exc_type, exc_value, traceback)
         await self.source.__aexit__(exc_type, exc_value, traceback)
