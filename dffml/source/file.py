bc54c25fee57b0530598a2e0caf7ce3954c06235
Yash Lamba <yashlamba2000@gmail.com>
2020-06-08 18:02:16 +0530

    source: file: Updated open to avoid additional lines on windows

--- dffml/source/file.py
+++ dffml/source/file.py
@@ -105,7 +105,7 @@ class FileSource(BaseSource):
             elif self.config.filename.suffix == ".zip":
                 close = self.zip_closer_helper()
             else:
-                close = open(self.config.filename, "w+")
+                close = open(self.config.filename, "w+", newline="")
             with close as fd:
                 await self.dump_fd(fd)
 
