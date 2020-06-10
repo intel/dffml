5490802bb689c7c0e7aeb175c2a95abdef94a1e9
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-06-10 13:34:18 +0000

    model:autosklearn: Add autosklearn model
    Co-authored-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ model/autosklearn/dffml_model_autosklearn/__init__.py
@@ -0,0 +1,21 @@
+"""
+.. warning:
+
+    Follow these instructions before running the above install
+    command to ensure that ``auto-sklearn`` installs correctly
+
+**Ubuntu Installation**
+
+To provide a C++11 building environment and the lateste SWIG version on Ubuntu, run:
+
+.. code-block:: console
+
+    $ sudo apt-get install build-essential swig
+
+Install other PyPi dependencies with
+
+.. code-block:: console
+    $ curl https://raw.githubusercontent.com/automl/auto-sklearn/master/requirements.txt | xargs -n 1 -L 1 pip install
+
+For more information about installation visit https://automl.github.io/auto-sklearn/master/installation.html#installation
+"""
