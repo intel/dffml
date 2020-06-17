1e586f3ddeca2efaa78d0ce383a48fead05cf850
Sudharsana K J L <kjlsudharsana@gmail.com>
2020-06-17 21:01:22 +0530

    Update model/tensorflow/tests/test_dnnc.py
    Co-authored-by: John Andersen <johnandersenpdx@gmail.com>

--- model/tensorflow/tests/test_dnnc.py
+++ model/tensorflow/tests/test_dnnc.py
@@ -72,7 +72,7 @@ class TestDNN(AsyncTestCase):
                 "--model-features",
                 "starts_with_a:int:1",
                 "-model-directory",
-                tempfile.TemporaryDirectory().name,
+                self.model_dir.name,
             )
         )
         self.assertEqual(config.directory, self.model_dir.name)
