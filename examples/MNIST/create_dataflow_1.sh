1da089666646ce5cb9714ef7bc398e58f30d7608
John Andersen <johnandersenpdx@gmail.com>
2020-06-24 15:48:22 -0700

    docs: usage: mnist: Fix dataflows
    Fixes: #741
    Fixes: #735
    
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- examples/MNIST/create_dataflow_1.sh
+++ examples/MNIST/create_dataflow_1.sh
@@ -1,7 +1,7 @@
 dffml dataflow create resize multiply associate_definition -configloader yaml \
     -seed \
-      '[280,280,3]'=dffml_operations_image.operations.resize.inputs.old_dim \
-      '[28,28]'=dffml_operations_image.operations.resize.inputs.new_dim \
+      '[280,280,3]'=resize.inputs.old_dim \
+      '[28,28]'=resize.inputs.new_dim \
       '{"image": "product"}'=associate_spec \
       0.00392156862=multiplier_def \
     -flow \
