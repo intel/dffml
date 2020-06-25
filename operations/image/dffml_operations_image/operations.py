dc1a5eb48ddcceee25a4f932768621862ef46078
sakshamarora1 <sakshamarora1001@gmail.com>
2020-06-25 18:58:37 +0530

    operations: image: Update resize commit and add new flatten operation
     - Added new flatten operation
     - Updated resize operation to have more parameters
     - Updated the MNIST use case to use flatten and updated resize operation
    
    Signed-off-by: sakshamarora1 <sakshamarora1001@gmail.com>

--- operations/image/dffml_operations_image/operations.py
+++ operations/image/dffml_operations_image/operations.py
@@ -1,36 +1,46 @@
 from typing import List
 import cv2
+import numpy as np
 
 from dffml.df.base import op
 
 
 @op
+async def flatten(array: List[int]) -> List[int]:
+    return np.array(array).flatten()
+
+
+@op
 async def resize(
-    data: List[int], old_dim: List[int], new_dim: List[int]
+    src: List[int],
+    dsize: List[int],
+    fx: float = None,
+    fy: float = None,
+    interpolation: int = None,
 ) -> List[int]:
     """
     Resizes image array to the specified new dimensions
 
     - If the new dimensions are in 2D, the image is converted to grayscale.
 
-    - To enlarge the image (old dimensions < new dimensions),
+    - To enlarge the image (src dimensions < dsize),
         it will resize the image with INTER_CUBIC interpolation.
 
-    - To shrink the image (old dimensions > new dimensions),
+    - To shrink the image (src dimensions > dsize),
         it will resize the image with INTER_AREA interpolation
     """
-    image = data.reshape(old_dim)
-    if len(new_dim) == 2:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if len(dsize) == 2:
+        src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     else:
-        new_dim = new_dim[:2]
-    if new_dim > old_dim:
-        resized_image = cv2.resize(
-            image, tuple(new_dim), interpolation=cv2.INTER_CUBIC
-        )
-    else:
-        resized_image = cv2.resize(
-            image, tuple(new_dim), interpolation=cv2.INTER_AREA
-        )
+        dsize = dsize[:2]
+    dsize = tuple(dsize)
+    if interpolation is None:
+        if dsize > src.shape:
+            interpolation = cv2.INTER_CUBIC
+        else:
+            interpolation = cv2.INTER_AREA
 
-    return resized_image.flatten()
+    parameters = {k: v for k, v in locals().items() if v is not None}
+
+    resized_image = cv2.resize(**parameters)
+    return resized_image
