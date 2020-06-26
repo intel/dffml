b6b40672e2e45a4c28d66fa6ea2845ab99f9bced
0dust <himanshutripathi366@gmail.com>
2020-06-26 08:25:38 +0530

    model: transformers: qa: Change default logging dir of SummaryWriter

--- model/transformers/examples/qa/train.sh
+++ model/transformers/examples/qa/train.sh
@@ -5,6 +5,7 @@ dffml train \
   -model-model_name_or_path bert-base-cased \
   -model-output_dir qamodel/checkpoints \
   -model-cache_dir qamodel/cache \
+  -model-log_dir qamodel/log \
   -sources s=op \
   -source-opimp dffml_model_transformers.qa.utils:parser \
   -source-args train.json True \
