115517c4dd6404989c564dc275521f356aa0d731
John Andersen <johnandersenpdx@gmail.com>
2020-07-06 16:32:09 +0000

    docs: Mon Jul  6 16:32:09 UTC 2020
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- master/_sources/changelog.md.txt
+++ master/_sources/changelog.md.txt
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 ### Added
+- Operations plugin for NLP
 - Support for default value in a Definition
 - Transformers Question Answering model
 - Source for reading images in directories
---
+++ master/webui/precache-manifest.c23dd606d968678bd894649554428900.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5402a1188eee10cb2ecb80ca9442fa14",
+    "url": "/dffml/master/webui/index.html"
+  },
+  {
+    "revision": "eee6ce0a342084d5c5d0",
+    "url": "/dffml/master/webui/static/js/2.e8316981.chunk.js"
+  },
+  {
+    "revision": "5c957da43e9d0adeb8bb75acf8ca36cc",
+    "url": "/dffml/master/webui/static/js/2.e8316981.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a210b0272efcab9b0dbd",
+    "url": "/dffml/master/webui/static/js/main.11a567f5.chunk.js"
+  },
+  {
+    "revision": "1b58fa33c84c4e9c5341",
+    "url": "/dffml/master/webui/static/js/runtime-main.03e80777.js"
+  }
+]);
