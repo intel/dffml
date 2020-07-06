926623cf80ee4ca940f93a5dd5306a1c31b9aad6
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-07-06 22:40:12 +0530

    Update dffml/operation/model.py
    Co-authored-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/operation/model.py
+++ dffml/operation/model.py
@@ -96,7 +96,6 @@ async def model_predict(self, features: Dict[str, Any]) -> Dict[str, Any]:
     {'model_predictions': {'Salary': {'confidence': 1.0, 'value': 50.0}}}
     """
 
-
     async with MemorySource(records=[Record("", data={"features": features})]) as source:
         async with source() as sctx:
             async for record in self.mctx.predict(sctx):
