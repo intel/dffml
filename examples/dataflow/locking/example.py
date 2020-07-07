572d5d8eb2ced67e907886b68ab8ceb73a7677fe
John Andersen <johnandersenpdx@gmail.com>
2020-07-06 20:05:27 -0700

    docs: tutorials: dataflows: locking: Make consise
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- examples/dataflow/locking/example.py
+++ examples/dataflow/locking/example.py
@@ -1,65 +1,45 @@
 import asyncio
-from dffml import Definition, DataFlow, Input, op, run
+from dffml import Definition, DataFlow, Input, op
+from dffml.noasync import run
 
-OBJ = Definition(name="obj", primitive="Any")
-LOCKED_OBJ = Definition(name="locked_obj", primitive="Any", lock=True)
+OBJ = Definition(name="obj", primitive="mapping")
+LOCKED_OBJ = Definition(name="locked_obj", primitive="mapping", lock=True)
 SLEEP_TIME = Definition(name="sleep_time", primitive="int")
-INTEGER = Definition(name="intefer", primitive="int")
-
-TEST_OUT = []
-
-
-class TestObj:
-    def __init__(self):
-        self.i = -1
+INTEGER = Definition(name="integer", primitive="int")
 
 
 @op(inputs={"obj": OBJ, "sleep_for": SLEEP_TIME, "i": INTEGER})
-async def run_me(obj, sleep_for, i) -> None:
-    obj.i = i
+async def run_me(obj: dict, sleep_for: int, i: int) -> None:
+    obj["i"] = i
     await asyncio.sleep(sleep_for)
-    s = f"set i = {i}, got i = {obj.i}"
-    print(s)
-    TEST_OUT.append([i, obj.i])
+    print(f"set i = {i}, got i = {obj['i']}")
 
 
-async def main():
-    dataflow = DataFlow(
-        operations={"run1": run_me.op, "run2": run_me.op},
-        implementations={run_me.op.name: run_me.imp},
-    )
-
-    print("Running dataflow without locked object")
-    obj = TestObj()
-    dataflow.seed = [
-        Input(value=obj, definition=OBJ),
+print("Running dataflow without locked object")
+for ctx, result in run(
+    DataFlow(run_me),
+    [
+        Input(value={"1": -1}, definition=OBJ),
+        Input(value=0.1, definition=SLEEP_TIME),
         Input(value=0.2, definition=SLEEP_TIME),
-        Input(value=2, definition=INTEGER),
         Input(value=1, definition=INTEGER),
-    ]
-    async for ctx, result in run(dataflow):
-        pass
+        Input(value=2, definition=INTEGER),
+    ],
+):
+    pass
 
-    print("Running dataflow with locked object")
-    run_me.op = run_me.op._replace(
-        inputs={"obj": LOCKED_OBJ, "sleep_for": SLEEP_TIME, "i": INTEGER}
-    )
-    dataflow = DataFlow(
-        operations={"run1": run_me.op, "run2": run_me.op},
-        implementations={run_me.op.name: run_me.imp},
-    )
-    obj = TestObj()
-    dataflow.seed = [
-        Input(value=obj, definition=LOCKED_OBJ),
+print("Running dataflow with locked object")
+run_me.op = run_me.op._replace(
+    inputs={"obj": LOCKED_OBJ, "sleep_for": SLEEP_TIME, "i": INTEGER}
+)
+for ctx, result in run(
+    DataFlow(run_me),
+    [
+        Input(value={"1": -1}, definition=LOCKED_OBJ),
+        Input(value=0.1, definition=SLEEP_TIME),
         Input(value=0.2, definition=SLEEP_TIME),
-        Input(value=2, definition=INTEGER),
         Input(value=1, definition=INTEGER),
-    ]
-    async for ctx, result in run(dataflow):
-        pass
-
-    for x in TEST_OUT[4:]:
-        assert x[0] == x[1]
-
-
-asyncio.run(main())
+        Input(value=2, definition=INTEGER),
+    ],
+):
+    pass
