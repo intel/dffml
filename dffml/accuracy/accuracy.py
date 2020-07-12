c8e99994bc0054ca8be8661560a5cc5d5073c542
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-07-12 17:55:40 +0000

    model: accuracy: Add new accuracy plugin

---
+++ dffml/accuracy/accuracy.py
@@ -0,0 +1,57 @@
+import abc
+from typing import AsyncIterator, List
+
+from ..record import Record
+from ..feature import Feature
+from ..util.entrypoint import base_entry_point
+from ..base import (
+    config,
+    BaseDataFlowFacilitatorObjectContext,
+    BaseDataFlowFacilitatorObject,
+)
+
+
+@config
+class AccuracyConfig:
+    pass
+
+
+class AccuracyContext(abc.ABC, BaseDataFlowFacilitatorObjectContext):
+    def __init__(self, parent: "Accuracy") -> None:
+        self.parent = parent
+
+    @abc.abstractmethod
+    async def score(
+        self,
+        prediction_records: AsyncIterator[Record],
+        prediction_feature: List[Feature],
+    ) -> float:
+        """
+        Abstract method to get the score
+
+        Parameters
+        ----------
+        prediction : AsyncIterator[Record]
+            The predicted record, which we get by calling the predict method of a model
+
+        features : List[Feature]
+            The features on which the predtion was done
+
+        Returns
+        -------
+        float
+            The score value
+        """
+        raise NotImplementedError()
+
+
+@base_entry_point("dffml.accuracy", "accuracy")
+class Accuracy(BaseDataFlowFacilitatorObject):
+    """
+    Abstract base class which should be derived from
+    and implmented using various machine learning
+    frameworks or concepts.
+    """
+
+    CONFIG = AccuracyConfig
+    CONTEXT = AccuracyContext
