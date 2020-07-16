3e52e82aa872e5d0927343fe2d959da26d936bc9
John Andersen <johnandersenpdx@gmail.com>
2020-07-16 15:21:05 -0700

    plugins: Only install plugins with deps
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/plugins.py
+++ dffml/plugins.py
@@ -4,6 +4,8 @@ independent as possible (no relative imports)
 """
 import os
 import sys
+import contextlib
+import importlib.util
 
 
 def inpath(binary):
@@ -49,11 +51,22 @@ CORE_PLUGINS += [
 ]
 
 # Dependencies of plugins and how to check if they exist on the system or not
+def check_daal4py():
+    spec = None
+    with contextlib.suppress(ModuleNotFoundError):
+        spec = importlib.util.find_spec("daal4py")
+    return bool(spec is not None)
+
+
 CORE_PLUGIN_DEPS = {
     ("model", "vowpalWabbit"): {"cmake": lambda: inpath("cmake")},
     ("model", "autosklearn"): {
         "swig": lambda: inpath("swig"),
         "cython": lambda: inpath("cython"),
+    },
+    ("model", "daal4py"): {
+        # Must be installed already via conda, do not provide a pypi package yet
+        "daal4py": check_daal4py
     },
 }
 
@@ -61,26 +74,46 @@ CORE_PLUGIN_DEPS = {
 ALTERNATIVES = {"configloader": "config"}
 
 # Build a dict of plugin_type_name (aka model, config): list(package_names)
-PACKAGE_NAMES_BY_PLUGIN = {
-    (plugin_type + ("s" if not plugin_type.endswith("s") else "")): [
+def package_names_by_plugin(validation=None):
+    by_plugin = {
+        (plugin_type + ("s" if not plugin_type.endswith("s") else "")): [
+            "dffml-%s-%s"
+            % (
+                ALTERNATIVES.get(plugin_type, plugin_type),
+                name.replace("_", "-"),
+            )
+            for sub_plugin_type, name in CORE_PLUGINS
+            if sub_plugin_type == plugin_type
+            and (not validation or validation(sub_plugin_type, name))
+        ]
+        for plugin_type, plugin_name in CORE_PLUGINS
+        if plugin_type != "examples"
+    }
+    # Operations used to be named features
+    by_plugin["operations"].extend(by_plugin["features"])
+    del by_plugin["features"]
+
+    # All packages
+    by_plugin["all"] = [
         "dffml-%s-%s"
         % (ALTERNATIVES.get(plugin_type, plugin_type), name.replace("_", "-"),)
-        for sub_plugin_type, name in CORE_PLUGINS
-        if sub_plugin_type == plugin_type
+        for plugin_type, name in CORE_PLUGINS
+        if plugin_type != "examples"
+        and (not validation or validation(plugin_type, name))
     ]
-    for plugin_type, _ in CORE_PLUGINS
-    if plugin_type != "examples"
-}
-# Operations used to be named featues
-PACKAGE_NAMES_BY_PLUGIN["operations"].extend(
-    PACKAGE_NAMES_BY_PLUGIN["features"]
-)
-del PACKAGE_NAMES_BY_PLUGIN["features"]
 
-# All packages
-PACKAGE_NAMES_BY_PLUGIN["all"] = [
-    "dffml-%s-%s"
-    % (ALTERNATIVES.get(plugin_type, plugin_type), name.replace("_", "-"),)
-    for plugin_type, name in CORE_PLUGINS
-    if plugin_type != "examples"
-]
+    return by_plugin
+
+
+PACKAGE_NAMES_BY_PLUGIN = package_names_by_plugin()
+
+# Same as PACKAGE_NAMES_BY_PLUGIN but only with plugins that have all their
+# pre-install dependencies met
+PACKAGE_NAMES_BY_PLUGIN_INSTALLABLE = package_names_by_plugin(
+    lambda plugin_type, plugin_name: all(
+        map(
+            lambda check: check(),
+            CORE_PLUGIN_DEPS.get((plugin_type, plugin_name), {}).values(),
+        )
+    )
+)
