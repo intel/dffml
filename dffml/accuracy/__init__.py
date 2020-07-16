b7898039d96ce835b718ccb04b7bde1831198f52
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-07-16 12:16:08 +0000

    dffml: accuracy: init: Add init file

---
+++ dffml/accuracy/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 Intel Corporation
+"""
+This the accuracy plugin
+"""
+from .accuracy import AccuracyConfig, AccuracyContext, AccuracyScorer
+from .simple_accuracy import SimpleAccuracy
