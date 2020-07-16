920adc266937bd353fa1fc430d19797e43921274
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-07-16 12:19:27 +0000

    dffml: setup: Add the accuracy plugin

--- setup.py
+++ setup.py
@@ -158,5 +158,9 @@ setup(
         "dffml.model": ["slr = dffml.model.slr:SLRModel"],
         # Secrets
         "dffml.secret": ["ini = dffml.secret.ini:INISecret"],
+        # Accuracy
+        "dffml.accuracy": [
+            "accuracy = dffml.accuracy.accuracy:AccuracyScorer"
+        ],
     },
 )
