97f0643f1b04958e58a32f25e10aa4a72fd9b497
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-07-18 15:31:06 +0000

    dffml: accuracy: accuracy: Update the score method signature

--- dffml/accuracy/accuracy.py
+++ dffml/accuracy/accuracy.py
@@ -11,6 +11,10 @@ from ..base import (
 )
 
 
+class InvalidNumberOfFeaturesError(Exception):
+    pass
+
+
 @config
 class AccuracyConfig:
     pass
@@ -22,16 +26,14 @@ class AccuracyContext(abc.ABC, BaseDataFlowFacilitatorObjectContext):
 
     @abc.abstractmethod
     async def score(
-        self,
-        prediction_records: AsyncIterator[Record],
-        prediction_feature: List[Feature],
+        self, records: AsyncIterator[Record], features: List[Feature],
     ) -> float:
         """
         Abstract method to get the score
 
         Parameters
         ----------
-        prediction : AsyncIterator[Record]
+        records : AsyncIterator[Record]
             The predicted record, which we get by calling the predict method of a model
 
         features : List[Feature]
@@ -49,8 +51,7 @@ class AccuracyContext(abc.ABC, BaseDataFlowFacilitatorObjectContext):
 class AccuracyScorer(BaseDataFlowFacilitatorObject):
     """
     Abstract base class which should be derived from
-    and implmented using various machine learning
-    frameworks or concepts.
+    and implmented using various accuracy scorer.
     """
 
     CONFIG = AccuracyConfig
