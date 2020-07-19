4e4cd8c78019aa6342af8e16f77fddb69c3eb0af
0dust <himanshutripathi366@gmail.com>
2020-07-19 10:22:58 +0530

    operations: nlp: Add tensorflow as inter dependency for CI tests

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -104,3 +104,7 @@ if [[ "x${PLUGIN}" == "xoperations/nlp" ]]; then
   conda install -y -c conda-forge spacy
   python -m spacy download en_core_web_sm
 fi
+
+if [[ "x${PLUGIN}" == "xexamples/nlp" ]]; then
+  python -m pip install -U -e "./model/tensorflow"
+fi
