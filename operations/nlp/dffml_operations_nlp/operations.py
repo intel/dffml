f84700892462e0f6e6f81d834eb2477f0d3479ff
0dust <himanshutripathi366@gmail.com>
2020-07-22 19:25:38 +0530

    operations: nlp: Added new NLP operations

--- operations/nlp/dffml_operations_nlp/operations.py
+++ operations/nlp/dffml_operations_nlp/operations.py
@@ -1,10 +1,26 @@
-from typing import List
+from typing import List, Dict, Tuple
 
 import spacy
+from spacy.attrs import ENT_IOB
 from spacy.lang.en import English
 from spacy.lang.en.stop_words import STOP_WORDS
+from sklearn.feature_extraction.text import (
+    CountVectorizer,
+    TfidfVectorizer,
+    TfidfTransformer,
+)
 
 from dffml.df.base import op
+
+
+def _load_model(spacy_model: str):
+    try:
+        nlp = spacy.load(spacy_model)
+    except OSError:
+        raise Exception(
+            f"Can't find model {spacy_model}. Try running `python -m spacy download {spacy_model}"
+        )
+    return nlp
 
 
 @op
@@ -37,15 +53,145 @@ async def remove_stopwords(
 
 @op
 async def get_embedding(text: str, spacy_model: str) -> List[float]:
-    try:
-        nlp = spacy.load(spacy_model)
-    except OSError:
-        raise Exception(
-            f"Can't find model {spacy_model}. Try running `python -m spacy download {spacy_model}"
-        )
-
+    nlp = _load_model(spacy_model)
     tokens = nlp(text)
     embedding = []
     for token in tokens:
         embedding.append(token.vector)
     return embedding
+
+
+@op
+async def pos_tagger(
+    text: str, spacy_model: str, tag_type: str = "fine_grained"
+) -> List[str]:
+    nlp = _load_model(spacy_model)
+    doc = nlp(text)
+    pos_tags = []
+    if tag_type is "fine_grained":
+        for token in doc:
+            pos_tags.append((token.text, token.tag_))
+    elif tag_type is "coarse_grained":
+        for token in doc:
+            pos_tags.append((token.text, token.pos_))
+    return pos_tags
+
+
+@op
+async def lemmatizer(text: str, spacy_model: str) -> List[str]:
+    nlp = _load_model(spacy_model)
+    doc = nlp(text)
+    lemma = []
+    for word in doc:
+        lemma.append(word.lemma_)
+    return lemma
+
+
+@op
+async def get_similarity(text_1: str, text_2: str, spacy_model: str) -> float:
+    nlp = _load_model(spacy_model)
+    text_1_doc = nlp(text_1)
+    text_2_doc = nlp(text_2)
+    return text_1_doc.similarity(text_2_doc)
+
+
+@op
+async def get_noun_chunks(text: str, spacy_model: str) -> List[str]:
+    nlp = _load_model(spacy_model)
+    text_doc = nlp(text)
+    noun_chunks = list(text_doc.noun_chunks)
+    return noun_chunks
+
+
+@op
+async def get_sentences(text: str, spacy_model: str) -> List[str]:
+    nlp = _load_model(spacy_model)
+    text_doc = nlp(text)
+    sentences = list(text_doc.sents)
+    return sentences
+
+
+@op
+async def count_vectorizer(
+    text: List[str],
+    encoding: str = "utf-8",
+    decode_error: str = "strict",
+    strip_accents: str = None,
+    lowercase: bool = True,
+    # preprocessor=None,
+    # tokenizer=None,
+    stop_words: str = None,
+    token_pattern: str = "(?u)\\b\\w\\w+\\b",
+    # ngram_range: tuple = (1, 1),
+    analyzer: str = "word",
+    max_df: float = 1.0,
+    min_df: float = 1,
+    max_features: int = None,
+    vocabulary: dict = None,
+    binary: bool = False,
+    # dtype: str = <class 'numpy.int64'>
+) -> List[int]:
+    vectorizer = CountVectorizer(
+        encoding=encoding,
+        decode_error=decode_error,
+        strip_accents=strip_accents,
+        lowercase=lowercase,
+        stop_words=stop_words,
+        token_pattern=token_pattern,
+        analyzer=analyzer,
+        max_df=max_df,
+        min_df=min_df,
+        max_features=max_features,
+        vocabulary=vocabulary,
+        binary=binary,
+    )
+    X = vectorizer.fit_transform(text)
+    return X.toarray()
+
+
+@op
+async def tfidf_vectorizer(
+    text: str,
+    # input="content",
+    encoding: str = "utf-8",
+    decode_error: str = "strict",
+    strip_accents: str = None,
+    lowercase: bool = True,
+    # preprocessor=None,
+    # tokenizer=None,
+    analyzer: str = "word",
+    stop_words: str = None,
+    token_pattern: str = "(?u)\\b\\w\\w+\\b",
+    # ngram_range=(1, 1),
+    max_df: str = 1.0,
+    min_df: str = 1,
+    max_features: str = None,
+    vocabulary: str = None,
+    binary: bool = False,
+    # dtype="<class 'numpy.float64'>'",
+    norm: str = "l2",
+    use_idf: bool = True,
+    smooth_idf: bool = True,
+    sublinear_tf: bool = False,
+) -> List[float]:
+    vectorizer = TfidfVectorizer(
+        encoding=encoding,
+        decode_error=decode_error,
+        strip_accents=strip_accents,
+        lowercase=lowercase,
+        analyzer=analyzer,
+        stop_words=stop_words,
+        token_pattern=token_pattern,
+        max_df=max_df,
+        min_df=min_df,
+        max_features=max_features,
+        vocabulary=vocabulary,
+        binary=binary,
+        norm=norm,
+        use_idf=use_idf,
+        smooth_idf=smooth_idf,
+        sublinear_tf=sublinear_tf,
+    )
+
+    X = vectorizer.fit_transform(text)
+    return X.toarray()
--- operations/nlp/tests/test_operations.py
+++ operations/nlp/tests/test_operations.py
@@ -1,11 +1,23 @@
 from random import randint
 
+import numpy as np
+
 from dffml.df.types import Input, DataFlow
 from dffml.df.memory import MemoryOrchestrator
 from dffml.operation.output import GetSingle
 from dffml.util.asynctestcase import AsyncTestCase
 
-from dffml_operations_nlp.operations import remove_stopwords, get_embedding
+from dffml_operations_nlp.operations import (
+    remove_stopwords,
+    get_embedding,
+    pos_tagger,
+    lemmatizer,
+    get_similarity,
+    get_sentences,
+    get_noun_chunks,
+    count_vectorizer,
+    tfidf_vectorizer,
+)
 
 
 class TestOperations(AsyncTestCase):
@@ -59,3 +71,181 @@ class TestOperations(AsyncTestCase):
                 embeddings[randint(0, len(input_sentence.split()) - 1)].shape,
                 embeddings[randint(0, len(input_sentence.split()) - 1)].shape,
             )
+
+    async def test_pos_tagger(self):
+        input_sentence = (
+            "The end is the beginning , and the beginning is the end"
+        )
+        async for ctx, results in MemoryOrchestrator.run(
+            DataFlow.auto(pos_tagger, GetSingle),
+            [
+                Input(
+                    value=[pos_tagger.op.outputs["result"].name],
+                    definition=GetSingle.op.inputs["spec"],
+                ),
+                Input(
+                    value=input_sentence,
+                    definition=pos_tagger.op.inputs["text"],
+                ),
+                Input(
+                    value="en_core_web_sm",
+                    definition=pos_tagger.op.inputs["spacy_model"],
+                ),
+            ],
+        ):
+            pos_tags = results[pos_tagger.op.outputs["result"].name]
+            words = input_sentence.split()
+            for i, _ in enumerate(words):
+                self.assertEqual(pos_tags[i][0], words[i])
+                self.assertIn(pos_tags[i][1], ["DT", "NN", "VBZ", "CC", ","])
+
+    async def test_lemmatizer(self):
+        input_sentence = (
+            "The end is the beginning , and the beginning is the end"
+        )
+        async for ctx, results in MemoryOrchestrator.run(
+            DataFlow.auto(lemmatizer, GetSingle),
+            [
+                Input(
+                    value=[lemmatizer.op.outputs["result"].name],
+                    definition=GetSingle.op.inputs["spec"],
+                ),
+                Input(
+                    value=input_sentence,
+                    definition=lemmatizer.op.inputs["text"],
+                ),
+                Input(
+                    value="en_core_web_sm",
+                    definition=lemmatizer.op.inputs["spacy_model"],
+                ),
+            ],
+        ):
+            lemma_list = results[lemmatizer.op.outputs["result"].name]
+            self.assertEqual(len(input_sentence.split()), len(lemma_list))
+
+    async def test_get_similarity(self):
+        input_sentence1 = (
+            "The end is the beginning , and the beginning is the end"
+        )
+        input_sentence2 = (
+            "The end was the beginning , and the beginning was the end"
+        )
+        async for ctx, results in MemoryOrchestrator.run(
+            DataFlow.auto(get_similarity, GetSingle),
+            [
+                Input(
+                    value=[get_similarity.op.outputs["result"].name],
+                    definition=GetSingle.op.inputs["spec"],
+                ),
+                Input(
+                    value=input_sentence1,
+                    definition=get_similarity.op.inputs["text_1"],
+                ),
+                Input(
+                    value=input_sentence2,
+                    definition=get_similarity.op.inputs["text_2"],
+                ),
+                Input(
+                    value="en_core_web_sm",
+                    definition=get_similarity.op.inputs["spacy_model"],
+                ),
+            ],
+        ):
+            similarity_score = results[
+                get_similarity.op.outputs["result"].name
+            ]
+            self.assertGreater(similarity_score, 0.9)
+
+    async def test_get_noun_chunks(self):
+        input_sentence = (
+            "The end is the beginning , and the beginning is the end"
+        )
+        async for ctx, results in MemoryOrchestrator.run(
+            DataFlow.auto(get_noun_chunks, GetSingle),
+            [
+                Input(
+                    value=[get_noun_chunks.op.outputs["result"].name],
+                    definition=GetSingle.op.inputs["spec"],
+                ),
+                Input(
+                    value=input_sentence,
+                    definition=get_noun_chunks.op.inputs["text"],
+                ),
+                Input(
+                    value="en_core_web_sm",
+                    definition=get_noun_chunks.op.inputs["spacy_model"],
+                ),
+            ],
+        ):
+            noun_chunks = results[get_noun_chunks.op.outputs["result"].name]
+            self.assertEqual(len(noun_chunks), 4)
+
+    async def test_get_sentences(self):
+        input_sentence = "The end is the beginning. The beginning is the end."
+        async for ctx, results in MemoryOrchestrator.run(
+            DataFlow.auto(get_sentences, GetSingle),
+            [
+                Input(
+                    value=[get_sentences.op.outputs["result"].name],
+                    definition=GetSingle.op.inputs["spec"],
+                ),
+                Input(
+                    value=input_sentence,
+                    definition=get_sentences.op.inputs["text"],
+                ),
+                Input(
+                    value="en_core_web_sm",
+                    definition=get_sentences.op.inputs["spacy_model"],
+                ),
+            ],
+        ):
+            sentences = results[get_sentences.op.outputs["result"].name]
+            self.assertEqual(len(sentences), 2)
+
+    async def test_count_vectorizer(self):
+        input_sentence = [
+            "The end is the beginning. The beginning is the end."
+        ]
+        async for ctx, results in MemoryOrchestrator.run(
+            DataFlow.auto(count_vectorizer, GetSingle),
+            [
+                Input(
+                    value=[count_vectorizer.op.outputs["result"].name],
+                    definition=GetSingle.op.inputs["spec"],
+                ),
+                Input(
+                    value=input_sentence,
+                    definition=count_vectorizer.op.inputs["text"],
+                ),
+            ],
+        ):
+            vectors = results[count_vectorizer.op.outputs["result"].name]
+            self.assertTrue(isinstance(vectors, np.ndarray))
+            unique_tokens = list(
+                set(input_sentence[0].lower().replace(".", "").split())
+            )
+            self.assertEqual(len(vectors[0]), len(unique_tokens))
+
+    async def test_tfidf_vectorizer(self):
+        input_sentence = [
+            "The end is the beginning. The beginning is the end."
+        ]
+        async for ctx, results in MemoryOrchestrator.run(
+            DataFlow.auto(tfidf_vectorizer, GetSingle),
+            [
+                Input(
+                    value=[tfidf_vectorizer.op.outputs["result"].name],
+                    definition=GetSingle.op.inputs["spec"],
+                ),
+                Input(
+                    value=input_sentence,
+                    definition=tfidf_vectorizer.op.inputs["text"],
+                ),
+            ],
+        ):
+            vectors = results[tfidf_vectorizer.op.outputs["result"].name]
+            self.assertTrue(isinstance(vectors, np.ndarray))
+            unique_tokens = list(
+                set(input_sentence[0].lower().replace(".", "").split())
+            )
+            self.assertEqual(len(vectors[0]), len(unique_tokens))
