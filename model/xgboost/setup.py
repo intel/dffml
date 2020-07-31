1bb8dc888299979fb47e5cbb71842cd587044b02
Oliver O'Brien <oliverobrien111@gmail.com>
2020-07-31 18:48:49 +0100

    Update setup.py

--- model/xgboost/setup.py
+++ model/xgboost/setup.py
@@ -10,7 +10,6 @@ common = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(common)
 
 common.KWARGS["install_requires"] += ["xgboost>=1.1.1"]
-common.KWARGS["install_requires"] += ["scikit-learn>=0.23.1"]
 common.KWARGS["install_requires"] += ["joblib>=0.16.0"]
 common.KWARGS["entry_points"] = {
     "dffml.model": [
