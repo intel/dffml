bbd11b7bc0c1586948be8ecd8375e55f94f47a74
Oliver O'Brien <oliverobrien111@gmail.com>
2020-07-31 20:52:31 +0100

    Added config constructor to test

--- model/xgboost/tests/test_model.py
+++ model/xgboost/tests/test_model.py
@@ -14,7 +14,10 @@ from dffml.util.entrypoint import entrypoint
 from dffml.record import Record
 from dffml.util.asynctestcase import AsyncTestCase
 
-from dffml_model_xgboost.xdgregressor import XDGRegressorModel
+from dffml_model_xgboost.xdgregressor import (
+    XDGRegressorModel,
+    XDGRegressorModelConfig,
+)
 
 
 class TestXDGRegressor(AsyncTestCase):
@@ -24,11 +27,13 @@ class TestXDGRegressor(AsyncTestCase):
         cls.model_dir = tempfile.TemporaryDirectory()
         # Create an instance of the model
         cls.model = XDGRegressorModel(
-            features=Features(
-                Feature("Feature1", float, 1), Feature("Feature2")
-            ),
-            predict=Feature("Target", float, 1),
-            directory=cls.model_dir.name,
+            XDGRegressorModelConfig(
+                features=Features(
+                    Feature("Feature1", float, 1), Feature("Feature2")
+                ),
+                predict=Feature("Target", float, 1),
+                directory=cls.model_dir.name,
+            )
         )
         # Generating data f(x1,x2) = 2*x1 + 3*x2
         _n_data = 2000
@@ -59,17 +64,17 @@ class TestXDGRegressor(AsyncTestCase):
         # Remove the temporary directory where the model was stored to cleanup
         cls.model_dir.cleanup()
 
-    async def test_02_train(self):
+    async def test_00_train(self):
         # Train the model on the training data
         await train(self.model, self.trainingsource)
 
-    async def test_03_accuracy(self):
+    async def test_01_accuracy(self):
         # Use the test data to assess the model's accuracy
         res = await accuracy(self.model, self.testsource)
         # Ensure the accuracy is above 80%
         self.assertTrue(0.8 <= res < 1.0)
 
-    async def test_04_predict(self):
+    async def test_02_predict(self):
         # Get the prediction for each piece of test data
         async for i, features, prediction in predict(
             self.model, self.testsource
