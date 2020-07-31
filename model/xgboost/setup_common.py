986ac15f02dadce5ae6da49309e519b8072d547d
Oliver O'Brien <oliverobrien111@gmail.com>
2020-07-31 18:49:12 +0100

    Update setup_common.py

--- model/xgboost/setup_common.py
+++ model/xgboost/setup_common.py
@@ -12,7 +12,6 @@ AUTHOR_EMAIL = "oliverobrien111@gmail.com"
 # Install dffml if it is not installed in development mode
 INSTALL_REQUIRES = (
     ["xgboost>=1.1.1"]
-    + ["scikit-learn>=0.23.1"]
     + ["joblib>=0.16.0"]
     + (
         ["dffml>=0.3.7"]
