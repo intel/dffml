7938d06e5a97d8768cb8a53778464b3edfd1ad1b
John Andersen <johnandersenpdx@gmail.com>
2020-08-03 02:41:38 +0000

    docs: Mon Aug  3 02:41:38 UTC 2020
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- master/_sources/changelog.md.txt
+++ master/_sources/changelog.md.txt
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 ### Added
+- XGBoost Regression Model
+- Pre-Trained PyTorch torchvision Models
+- Spacy model for NER
+- Added ability to rename outputs using GetSingle
 - Tutorial for using NLP operations with models
 - Operations plugin for NLP wrapping spacy and scikit functions
 - Support for default value in a Definition
---
+++ master/webui/precache-manifest.27016bef7a5d4e19aaba0a5cfe00dec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477bc2333ab5bb54747eb4050569bd43",
+    "url": "/dffml/master/webui/index.html"
+  },
+  {
+    "revision": "608485547fa05e738b85",
+    "url": "/dffml/master/webui/static/js/2.e1f3c51c.chunk.js"
+  },
+  {
+    "revision": "5c957da43e9d0adeb8bb75acf8ca36cc",
+    "url": "/dffml/master/webui/static/js/2.e1f3c51c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d9d6754d21cada5b3b8",
+    "url": "/dffml/master/webui/static/js/main.7eb5dba0.chunk.js"
+  },
+  {
+    "revision": "1b58fa33c84c4e9c5341",
+    "url": "/dffml/master/webui/static/js/runtime-main.03e80777.js"
+  }
+]);
