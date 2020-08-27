d0dd83de591f5f7fefc85d206ba6503aea5c2f0c
sakshamarora1 <sakshamarora1001@gmail.com>
2020-08-27 19:49:31 +0000

    model: pytorch: tests: Update test_pytorchnet.py
    Signed-off-by: sakshamarora1 <sakshamarora1001@gmail.com>

--- model/pytorch/tests/test_pytorchnet.py
+++ model/pytorch/tests/test_pytorchnet.py
@@ -1,3 +1,4 @@
+import torch.nn as nn
 import os
 import shutil
 
@@ -7,7 +8,39 @@ from dffml.util.asynctestcase import IntegrationCLITestCase
 from dffml.high_level import train, accuracy, predict
 from dffml import Features, Feature, DirectorySource
 from dffml_model_pytorch import PyTorchNeuralNetwork
-from ..examples.pytorchnet.python_example import RockPaperScissorsModel, Loss
+from dffml_model_pytorch.utils import CrossEntropyLossFunction
+
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=3, out_channels=32, kernel_size=5, padding=2
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1
+        )
+        self.conv3 = nn.Conv2d(
+            in_channels=32, out_channels=16, kernel_size=3, padding=1
+        )
+
+        self.relu = nn.ReLU()
+        self.pooling = nn.MaxPool2d(kernel_size=2)
+
+        self.linear = nn.Linear(in_features=1296, out_features=3)
+
+    def forward(self, x):
+        x = self.pooling(self.relu(self.conv1(x)))
+        x = self.pooling(self.relu(self.conv2(x)))
+        x = self.pooling(self.relu(self.conv2(x)))
+        x = self.pooling(self.relu(self.conv3(x)))
+        x = self.linear(x.view(-1, 1296))
+        return x
+
+
+RockPaperScissorsModel = ConvNet()
+Loss = CrossEntropyLossFunction()
 
 
 def sh_filepath(filename):
