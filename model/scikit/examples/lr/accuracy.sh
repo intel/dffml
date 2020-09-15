975c09f14a277ebd81480f417e3e0b8a9753b7ec
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-09-15 11:54:15 +0000

    model: scikit: examples: add the scorer

--- model/scikit/examples/lr/accuracy.sh
+++ model/scikit/examples/lr/accuracy.sh
@@ -3,5 +3,6 @@ dffml accuracy \
   -model-features Years:int:1 Expertise:int:1 Trust:float:1 \
   -model-predict Salary:float:1 \
   -model-directory tempdir \
+  -scorer mse \
   -sources f=csv \
   -source-filename test.csv
