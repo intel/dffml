659f7706c6c6c3c5fea02f9d0ebbaf42627563eb
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-09-25 05:49:07 +0000

    setup: Add the classification accuracy to the entrypoints

--- setup.py
+++ setup.py
@@ -161,7 +161,8 @@ setup(
         "dffml.secret": ["ini = dffml.secret.ini:INISecret"],
         # Accuracy
         "dffml.accuracy": [
-            "mse = dffml.accuracy.mse:MeanSquaredErrorAccuracy"
+            "mse = dffml.accuracy.mse:MeanSquaredErrorAccuracy",
+            "clfacc = dffml.accuracy.clfacc:ClassificationAccuracy",
         ],
     },
 )
