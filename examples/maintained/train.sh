d287831c2940e4e8f02c6ba540bda21aedc6c5b8
John Andersen <johnandersenpdx@gmail.com>
2020-09-26 13:32:24 -0700

    docs: usage: integration: Update tutorial
    Update the Automating Classification tutorial to use the MySQL, op, and
    DataFlow preprocessing sources. Test it using the consoletest Sphinx
    extension.
    
    Use the op source to pull in records from the GitHub API and randomly
    assign a classification.
    
    Use the DataFlow preprocessing source to run the Git features before
    model training and prediction.
    
    Use the MySQL source to grab records from and update the demo database
    directly.
    
    Fixes: #620
    Fixes: #274
    Fixes: #254
    Fixes: #104
    
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ examples/maintained/train.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# echo command back and exit on error
+set -xe
+
+dffml train all \
+  -model tfdnnc \
+  -model-epochs 400 \
+  -model-steps 4000 \
+  -model-predict maintained:int:1 \
+  -model-classifications 0 1 \
+  -model-directory modeldir \
+  -model-features \
+    authors:int:10 \
+    commits:int:10 \
+    work:int:10 \
+  -sources preprocess=df \
+  -source-preprocess-dataflow dataflow.yaml \
+  -source-preprocess-no_strict \
+  -source-preprocess-record_def URL \
+  -source-preprocess-inputs "$(date +'%Y-%m-%d %H:%M')=quarter_start_date" \
+  -source-preprocess-features maintained:int:1 \
+  -source-preprocess-source mysql \
+  -source-preprocess-source-insecure \
+  -source-preprocess-source-user user \
+  -source-preprocess-source-password pass \
+  -source-preprocess-source-db db \
+  -source-preprocess-source-key key \
+  -source-preprocess-source-records \
+    'SELECT `key`, `maintained` FROM `status`' \
+  -source-preprocess-source-record \
+    'SELECT `key`, `maintained` FROM `status` WHERE `key`=%s' \
+  -source-preprocess-source-update \
+    'INSERT INTO `status` (`key`, `maintained`) VALUES(%s, %s) ON DUPLICATE KEY UPDATE `maintained`=%s' \
+  -source-preprocess-source-features '{"maintained": "maintained"}' \
+  -source-preprocess-source-predictions '{}' \
+  -log debug
