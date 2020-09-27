8072a77b26200f74504970f40db27b29982f5a1b
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-09-27 18:12:01 +0000

    model: tensorflow: examples: tfdnnc: Rename clfacc to clf

--- model/tensorflow/examples/tfdnnc/accuracy.sh
+++ model/tensorflow/examples/tfdnnc/accuracy.sh
@@ -4,7 +4,7 @@ dffml accuracy \
   -model-location tempdir \
   -model-classifications 0 1 2 \
   -model-clstype int \
-  -scorer clfacc \
+  -scorer clf \
   -sources iris=csv \
   -source-filename iris_test.csv \
   -model-features \
