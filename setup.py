60f1d5e26d2f4f32de066c8371d791ac19b068bc
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-09-27 18:10:45 +0000

    setup: Rename clfacc to clf

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ setup(
         # Accuracy
         "dffml.accuracy": [
             "mse = dffml.accuracy.mse:MeanSquaredErrorAccuracy",
-            "clfacc = dffml.accuracy.clfacc:ClassificationAccuracy",
+            "clf = dffml.accuracy.clf:ClassificationAccuracy",
         ],
     },
 )
