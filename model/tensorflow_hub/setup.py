fafceb1237f5f8a6963b6b1156b978accd98641d
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-10-09 08:17:03 +0000

    model: tensorflow_hub: Add the textclf scorer to the entrypoints

--- model/tensorflow_hub/setup.py
+++ model/tensorflow_hub/setup.py
@@ -49,6 +49,9 @@ setup(
     entry_points={
         "dffml.model": [
             "text_classifier = dffml_model_tensorflow_hub.text_classifier:TextClassificationModel"
-        ]
+        ],
+        "dffml.accuracy": [
+            "textclf = dffml_model_tensorflow_hub.text_classifier_accuracy:TextClassifierAccuracy"
+        ],
     },
 )
