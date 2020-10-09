cbdcab30a244dc04aba32fcc77d5fc35170e5f29
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-10-09 08:11:33 +0000

    model: tensorflow_hub: Overide the base Accuracy method

--- model/tensorflow_hub/dffml_model_tensorflow_hub/text_classifier.py
+++ model/tensorflow_hub/dffml_model_tensorflow_hub/text_classifier.py
@@ -304,6 +304,9 @@ class TextClassifierContext(ModelContext):
             )
             yield record
 
+    async def accuracy(self, sources, accuracy_scorer):
+        return Accuracy(await accuracy_scorer.score(self, sources))
+
 
 @entrypoint("text_classifier")
 class TextClassificationModel(Model):
