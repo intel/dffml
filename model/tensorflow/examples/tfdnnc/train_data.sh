fe2d970f797c2a3f2eafc6e025f2b8092e1e62c8
John Andersen <johnandersenpdx@gmail.com>
2020-10-16 15:16:41 -0700

    docs: tutorials: models: Add iris TensorFlow tutorial
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- model/tensorflow/examples/tfdnnc/train_data.sh
+++ model/tensorflow/examples/tfdnnc/train_data.sh
@@ -1,2 +1,3 @@
 wget http://download.tensorflow.org/data/iris_training.csv
+echo '376c8ea3b7f85caff195b4abe62f34e8f4e7aece8bd087bbd746518a9d1fd60ae3b4274479f88ab0aa5c839460d535ef iris_training.csv' | sha384sum -c -
 sed -i 's/.*setosa,versicolor,virginica/SepalLength,SepalWidth,PetalLength,PetalWidth,classification/g' *.csv
