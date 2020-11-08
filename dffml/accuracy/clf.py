692f0f82ca6c3a2acfa680ebcc1d9fbfcfb8d0a6
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-11-08 12:33:53 +0000

    accuracy: clf: Update the score method signature

--- dffml/accuracy/clf.py
+++ dffml/accuracy/clf.py
@@ -1,9 +1,9 @@
-from typing import AsyncIterator, List
-
 from ..base import config
 from ..record import Record
 from ..feature import Feature
+from ..model import ModelContext
 from ..util.entrypoint import entrypoint
+from ..source.source import SourcesContext
 from .accuracy import (
     AccuracyScorer,
     AccuracyContext,
@@ -21,18 +21,16 @@ class ClassificationAccuracyContext(AccuracyContext):
     Classification Accuracy
     """
 
-    async def score(
-        self, records: AsyncIterator[Record], features: List[Feature],
-    ):
-        if len(features) != 1:
+    async def score(self, mctx: ModelContext, sources: SourcesContext):
+        if len([mctx.parent.config.predict]) != 1:
             raise InvalidNumberOfFeaturesError(
                 f"{self.__class__.__qualname__} can only assess accuracy of one feature. features: {features}"
             )
         total = 0
         right_predictions = 0
-        async for record in records:
-            if str(record.feature(features[0].name)) == str(
-                record.prediction(features[0].name).value
+        async for record in mctx.predict(sources):
+            if str(record.feature(mctx.parent.config.predict.name)) == str(
+                record.prediction(mctx.parent.config.predict.name).value
             ):
                 right_predictions += 1
             total += 1
