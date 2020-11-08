ddeb601614fb725909f4f496053c372726e1f4b6
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-11-08 12:34:37 +0000

    accuracy: mse: Update the score method signature

--- dffml/accuracy/mse.py
+++ dffml/accuracy/mse.py
@@ -1,9 +1,9 @@
-from typing import AsyncIterator, List
-
 from ..base import config
 from ..record import Record
 from ..feature import Feature
+from ..model import ModelContext
 from ..util.entrypoint import entrypoint
+from ..source.source import SourcesContext
 from .accuracy import (
     AccuracyScorer,
     AccuracyContext,
@@ -21,18 +21,18 @@ class MeanSquaredErrorAccuracyContext(AccuracyContext):
     Mean Squared Error
     """
 
-    async def score(
-        self, records: AsyncIterator[Record], features: List[Feature],
-    ):
-        if len(features) != 1:
+    async def score(self, mctx: ModelContext, sources: SourcesContext):
+        if len([mctx.parent.config.predict]) != 1:
             raise InvalidNumberOfFeaturesError(
                 f"{self.__class__.__qualname__} can only assess accuracy of one feature. features: {features}"
             )
         y = []
         y_predict = []
-        async for record in records:
-            y.append(record.feature(features[0].name))
-            y_predict.append(record.prediction(features[0].name).value)
+        async for record in mctx.predict(sources):
+            y.append(record.feature(mctx.parent.config.predict.name))
+            y_predict.append(
+                record.prediction(mctx.parent.config.predict.name).value
+            )
         accuracy = sum(
             list(map(lambda x, y: abs(x - y) ** 2, y, y_predict))
         ) / len(y)
