3dbbbcc999046a4ccd29e3160523e2d4ce621e5a
John Andersen <johnandersenpdx@gmail.com>
2020-11-12 16:14:00 -0800

    util: testing: docs: Add run_consoletest for running consoletest on docstrings
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/util/testing/docs.py
+++ dffml/util/testing/docs.py
@@ -1,6 +1,14 @@
 import io
 import doctest
-from typing import Dict, Any
+import inspect
+import pathlib
+import contextlib
+from typing import Dict, Any, Optional, Union, List
+
+from .consoletest.parser import parse_nodes
+from .consoletest.runner import run_nodes
+from .consoletest.commands import ConsoletestCommand
+from .consoletest.util import code_block_to_dict, literalinclude_to_dict
 
 
 def run_doctest(obj, state: Dict[str, Any] = None, check: bool = True):
@@ -28,3 +36,38 @@ def run_doctest(obj, state: Dict[str, Any] = None, check: bool = True):
         results = runner.run(test, out=output.write)
         if results.failed and check:
             raise Exception(output.getvalue())
+
+
+async def run_consoletest(
+    obj: Any,
+    *,
+    repo_root_dir: Optional[Union[str, pathlib.Path]] = None,
+    docs_root_dir: Optional[Union[str, pathlib.Path]] = None,
+    setup: Optional[List[ConsoletestCommand]] = None,
+) -> None:
+    """
+    Run consoletest on the object provided.
+    """
+    if repo_root_dir is None:
+        repo_root_dir = pathlib.Path(__file__).parents[3]
+    if docs_root_dir is None:
+        docs_root_dir = pathlib.Path(inspect.getsourcefile(obj)).parent
+
+    nodes = []
+
+    for node in parse_nodes(inspect.getdoc(obj)):
+        if not node.options.get("test", False):
+            continue
+        if node.directive == "code-block":
+            nodes.append(
+                code_block_to_dict(node.content, node.options, node=node.node)
+            )
+        elif node.directive == "literalinclude":
+            nodes.append(
+                literalinclude_to_dict(node.content, node.options, node.node)
+            )
+
+    with contextlib.ExitStack() as stack:
+        await run_nodes(
+            repo_root_dir, docs_root_dir, stack, nodes, setup=setup,
+        )
