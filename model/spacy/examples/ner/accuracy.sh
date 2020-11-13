5bff3ae4f56c778b89016d7db42093e7e1c9b68f
Yash Lamba <yashlamba2000@gmail.com>
2020-11-13 17:30:55 +0530

    model: spacy: ner: Fix conflicts with cherry pick

--- model/spacy/examples/ner/accuracy.sh
+++ model/spacy/examples/ner/accuracy.sh
@@ -4,7 +4,7 @@ dffml accuracy \
   -source-opimp model.spacy.dffml_model_spacy.ner.utils:parser \
   -source-args train.json False \
   -model-model_name_or_path en_core_web_sm \
-  -model-output_dir temp \
+  -model-directory temp \
   -model-n_iter 5 \
   -scorer mse \
   -log debug
