960aa13ba651d20208e2d7ea8aa9d087a8282eae
John Andersen <johnandersenpdx@gmail.com>
2020-11-15 15:30:45 -0800

    docs: conf: Fix warnings
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -82,16 +82,6 @@ html_context = {
     "display_github": True,
 }
 
-html_theme_options = {
-    "description": "The easiest way to use Machine Learning",
-    "github_url": "https://github.com/intel/dffml/",
-}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
 
 def setup(app):
     app.add_js_file("copybutton.js")
