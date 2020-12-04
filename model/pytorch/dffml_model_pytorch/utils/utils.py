e94b711d8a8647e510e6d5dc6c633a4c59c07f23
nitesh585 <niteshyadav585@gmail.com>
2020-12-04 13:36:44 +0530

    model: pytorch: Change network layer name property to layer_type

--- model/pytorch/dffml_model_pytorch/utils/utils.py
+++ model/pytorch/dffml_model_pytorch/utils/utils.py
@@ -22,14 +22,14 @@ def create_layer(layer_dict):
     for name, layer in layer_dict.items():
         if isinstance(layer, dict):
             parameters = {k: v for k, v in layer.items()}
-            layer_name = parameters.pop("name")
+            layer_name = parameters.pop("layer_type")
 
             sequential_dict.add_module(
                 name, getattr(nn, layer_name)(**parameters)
             )
         else:
             parameters = {k: v for k, v in layer_dict.items()}
-            layer_name = parameters.pop("name")
+            layer_name = parameters.pop("layer_type")
             return getattr(nn, layer_name)(**parameters)
     return sequential_dict
 
