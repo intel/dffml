0b2041b955065a4420b1b99d52dee7857ece275b
John Andersen <johnandersenpdx@gmail.com>
2020-12-29 15:24:28 -0800

    model: daal4py: Convert to consoletest
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- model/daal4py/tests/test_lr_integration.py
+++ model/daal4py/tests/test_lr_integration.py
@@ -4,10 +4,18 @@ import pathlib
 import contextlib
 
 from dffml.cli.cli import CLI
-from dffml.util.asynctestcase import IntegrationCLITestCase
+from dffml import IntegrationCLITestCase, run_consoletest
+
+from dffml_model_daal4py.daal4pylr import DAAL4PyLRModel
 
 
 class TestDAAL4PyLRModel(IntegrationCLITestCase):
+    async def test_consoletest(self):
+        await run_consoletest(
+            DAAL4PyLRModel,
+            docs_root_dir=pathlib.Path(__file__).parents[3] / "docs",
+        )
+
     async def test_run(self):
         # Make a temporary directory to store the model
         directory = self.mktempdir()
