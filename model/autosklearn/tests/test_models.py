b197d3efb279716a9ff56116416e6b86d0b05e80
John Andersen <johnandersenpdx@gmail.com>
2021-01-04 15:26:01 -0800

    model: autosklearn: regressor: tests: Fix consoletest by adding docs_root_dir
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- model/autosklearn/tests/test_models.py
+++ model/autosklearn/tests/test_models.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from dffml import run_consoletest, AsyncTestCase
 
 from dffml_model_autosklearn import AutoSklearnRegressorModel
@@ -5,4 +7,7 @@ from dffml_model_autosklearn import AutoSklearnRegressorModel
 
 class TestAutoSklearnRegressorModel(AsyncTestCase):
     async def test_docstring(self):
-        await run_consoletest(AutoSklearnRegressorModel)
+        await run_consoletest(
+            AutoSklearnRegressorModel,
+            docs_root_dir=pathlib.Path(__file__).parents[3] / "docs",
+        )
