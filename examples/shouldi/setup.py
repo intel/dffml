088dbd0afc047e4f221dfb0f9b81b098a70a53d3
John Andersen <johnandersenpdx@gmail.com>
2021-01-22 22:48:18 -0800

    shouldi: Move to PEP-517/518
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- examples/shouldi/setup.py
+++ examples/shouldi/setup.py
@@ -1,23 +1,3 @@
-import os
-import importlib.util
-from setuptools import setup
+import setuptools
 
-# Boilerplate to load commonalities
-spec = importlib.util.spec_from_file_location(
-    "setup_common", os.path.join(os.path.dirname(__file__), "setup_common.py")
-)
-common = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(common)
-
-common.KWARGS["entry_points"] = {
-    "console_scripts": ["shouldi = shouldi.cli:ShouldI.main"],
-    "dffml.operation": [
-        "run_bandit = shouldi.python.bandit:run_bandit",
-        "safety_check = shouldi.python.safety:safety_check",
-        "pypi_package_json = shouldi.python.pypi:pypi_package_json",
-        "pypi_package_contents = shouldi.python.pypi:pypi_package_contents",
-        "cleanup_pypi_package = shouldi.python.pypi:cleanup_pypi_package",
-    ],
-}
-
-setup(**common.KWARGS)
+setuptools.setup()
