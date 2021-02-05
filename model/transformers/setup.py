7ece36ccf38bcee7ba2241d102f075b63dfc4cf4
SGeetansh <saxenageetansh1@gmail.com>
2021-02-05 14:40:17 +0530

    setup: Modified setup structure

--- model/transformers/setup.py
+++ model/transformers/setup.py
@@ -19,32 +19,6 @@ with open(
 with open(os.path.join(self_path, "README.md"), "r", encoding="utf-8") as f:
     readme = f.read()
 
-INSTALL_REQUIRES = [] + (
-    ["dffml>=0.3.7"]
-    if not any(
-        list(
-            map(
-                os.path.isfile,
-                list(
-                    map(
-                        lambda syspath: os.path.join(
-                            syspath, "dffml.egg-link"
-                        ),
-                        sys.path,
-                    )
-                ),
-            )
-        )
-    )
-    else []
-)
-
-REQUIREMENTS_TXT_PATH = Path(self_path, "requirements.txt")
-if REQUIREMENTS_TXT_PATH.is_file():
-    INSTALL_REQUIRES += list(
-        map(lambda i: i.strip(), REQUIREMENTS_TXT_PATH.read_text().split("\n"))
-    )
-
 setup(
     name="dffml-model-transformers",
     version=version,
@@ -68,8 +42,6 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    install_requires=INSTALL_REQUIRES,
-    packages=find_packages(),
     entry_points={
         "dffml.model": [
             "ner_tagger = dffml_model_transformers.ner.ner_model:NERModel",
