ddcfabdce9cae2c53a886ee662336ebdd6ac26b6
Sudeep Sidhu <sudeepmanilsidhu@gmail.com>
2021-02-06 17:28:44 +0530

    Cast predict to input dtype

--- model/scikit/dffml_model_scikit/scikit_base.py
+++ model/scikit/dffml_model_scikit/scikit_base.py
@@ -148,7 +148,10 @@ class ScikitContext(ModelContext):
             )
             target = self.parent.config.predict.name
             record.predicted(
-                target, self.clf.predict(predict)[0], self.confidence
+                target, 
+                self.parent.config.predict.dtype(self.clf.predict(predict)[0]) if \
+                    self.parent.config.predict.dtype is not str else \
+                    self.clf.predict(predict)[0], self.confidence
             )
             yield record
 
@@ -260,7 +263,10 @@ class ScikitContextUnsprvised(ScikitContext):
                 "Predicted cluster for {}: {}".format(predict, prediction)
             )
             target = self.parent.config.predict.name
-            record.predicted(target, prediction[0], self.confidence)
+            record.predicted(target,
+                self.parent.config.predict.dtype(prediction[0]) if \
+                    self.parent.config.predict.dtype is not str else \
+                    prediction[0], self.confidence)
             yield record
 
 
