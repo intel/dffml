350d184c6f153e9e3cae9dbd2c4cdfa054249731
John Andersen <johnandersenpdx@gmail.com>
2021-02-12 18:54:47 -0800

    shouldi: tests: cli: use: Parse JSON output to check vuln numbers
    - Switch the shouldi use command from printing output to returning a
      mapping of contexts to their static analysis reports.
    
    - Parse the JSON of this report within the tests to check the vuln
      numbers. Remove old check for string in output way of checking.
    
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- examples/shouldi/shouldi/python/bandit.py
+++ examples/shouldi/shouldi/python/bandit.py
@@ -7,7 +7,7 @@ from dffml import Definition
 
 
 @op
-async def run_bandit(pkg: str) -> dict:
+async def run_bandit(self, pkg: str) -> dict:
     """
     CLI usage: dffml service dev run -log debug shouldi.bandit:run_bandit -pkg .
     """
@@ -27,11 +27,13 @@ async def run_bandit(pkg: str) -> dict:
     if len(stdout) == 0:
         raise Exception
     bandit_op = stdout.decode()
+    self.logger.debug("Raw scan results: %s", bandit_op)
     bandit_op = json.loads(bandit_op)
     t_results = bandit_op["results"]
     final_result = bandit_op["metrics"]["_totals"]
     # Count put number of issues that are high confidence
     for level in ["LOW", "MEDIUM", "HIGH"]:
+        level_key = f"CONFIDENCE.HIGH_AND_SEVERITY.{level}"
         high_conf = 0
         for item in t_results:
             if (
@@ -39,5 +41,10 @@ async def run_bandit(pkg: str) -> dict:
                 and item["issue_severity"] == level
             ):
                 high_conf += 1
-        final_result["CONFIDENCE.HIGH_AND_SEVERITY." + level] = high_conf
+                # Add this issue to a list of issues found at this severity level
+                issue_key = f"{level_key}.issues"
+                final_result.setdefault(issue_key, [])
+                final_result[issue_key].append(item)
+        # Set count of the number of issues we found at both this severity level
+        final_result[level_key] = high_conf
     return final_result
