712abf88ef7488ec5bbd931b6a082d605932290e
John Andersen <johnandersenpdx@gmail.com>
2021-03-02 04:24:38 +0000

    docs: Tue Mar  2 04:24:38 UTC 2021
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '616676ea',
+    VERSION: '91172cfe',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
---
+++ master/webui/precache-manifest.ca52171103438ded16ba89b499254dd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e63c2c4282b5549a95b8af76f860ab9",
+    "url": "/dffml/master/webui/index.html"
+  },
+  {
+    "revision": "770c878b93dfe9f5a6c9",
+    "url": "/dffml/master/webui/static/js/2.0bb47bdd.chunk.js"
+  },
+  {
+    "revision": "5c957da43e9d0adeb8bb75acf8ca36cc",
+    "url": "/dffml/master/webui/static/js/2.0bb47bdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e76f4604d1294ba091a",
+    "url": "/dffml/master/webui/static/js/main.760cd0a8.chunk.js"
+  },
+  {
+    "revision": "1b58fa33c84c4e9c5341",
+    "url": "/dffml/master/webui/static/js/runtime-main.03e80777.js"
+  }
+]);
