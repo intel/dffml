2090ec45d29ac87d08a5dbd3280aa494c2852ccb
John Andersen <johnandersenpdx@gmail.com>
2021-03-05 20:40:06 -0800

    scripts: docs: Remove copybutton download
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 
 PYTHON=${PYTHON:-"python3.7"}
@@ -30,12 +30,6 @@ ln -s "${PWD}/service/http/docs/" docs/plugins/service/http
 "${PYTHON}" -c 'import os, pkg_resources; [e.load() for e in pkg_resources.iter_entry_points("console_scripts") if e.name.startswith("sphinx-build")][0]()' -W -b html docs pages \
   || (echo "[ERROR] Failed run sphinx, is it installed (pip install -U .[dev])?" 1>&2 ; exit 1)
 cp -r docs/images pages/
-if [ ! -f pages/_static/copybutton.js ]; then
-  curl -sSL -o pages/_static/copybutton.js "https://raw.githubusercontent.com/python/python-docs-theme/master/python_docs_theme/static/copybutton.js"
-fi
-sha384sum -c - <<EOF
-061b550f64fb65ccb73fbe61ce15f49c17bc5f30737f42bf3c9481c89f7996d0004a11bf283d6bd26cf0b65130fc1d4b pages/_static/copybutton.js
-EOF
 touch pages/.nojekyll
 
 if [ "x${HTTP}" != "x" ]; then
