351ef72c09cca9205f590496677ecd11a9733858
John Andersen <johnandersenpdx@gmail.com>
2021-03-17 18:50:42 -0700

    tests: model: slr: Explict docs_root_dir for consoletest
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- tests/model/test_slr.py
+++ tests/model/test_slr.py
@@ -1,3 +1,4 @@
+import pathlib
 import unittest
 
 from dffml import SLRModel, run_consoletest, AsyncTestCase
@@ -5,4 +6,6 @@ from dffml import SLRModel, run_consoletest, AsyncTestCase
 
 class TestSLRModel(AsyncTestCase):
     async def test_docstring(self):
-        await run_consoletest(SLRModel)
+        await run_consoletest(
+            SLRModel, docs_root_dir=pathlib.Path(__file__).parents[2] / "docs",
+        )
