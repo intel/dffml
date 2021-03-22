bbd1f0564f8fc7bd65ceb4f1fc15cb50d18c93ed
Saahil <programmer290399@gmail.com>
2021-03-22 13:53:03 +0530

    Updated setup information

--- model/orion/setup.py
+++ model/orion/setup.py
@@ -1,3 +1,16 @@
-import setuptools
+import os
+import importlib.util
+from setuptools import setup
 
-setuptools.setup()
+# Boilerplate to load commonalities
+spec = importlib.util.spec_from_file_location(
+    "setup_common", os.path.join(os.path.dirname(__file__), "setup_common.py")
+)
+common = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(common)
+
+common.KWARGS["entry_points"] = {
+    "dffml.model": ["orion = dffml_model_orion.orion_model:OrionModel",]
+}
+
+setup(**common.KWARGS)
