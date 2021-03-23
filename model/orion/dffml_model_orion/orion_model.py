02c386f83aa83d888369e2cc0e9e6bff63d46e01
Saahil <programmer290399@gmail.com>
2021-03-23 10:02:49 +0530

    Training and accuracy working on Orion model

--- model/orion/dffml_model_orion/orion_model.py
+++ model/orion/dffml_model_orion/orion_model.py
@@ -1,9 +1,13 @@
 import os
 import json
 import pathlib
+from itertools import groupby
+from operator import itemgetter
 from typing import AsyncIterator, List, Tuple, Any
 
+import numpy as np
 import pandas as pd
+
 from orion import Orion
 
 from dffml import (
@@ -23,9 +27,6 @@ from dffml import (
 
 @config
 class OrionModelConfig:
-    ts_data: Features = field("Time series data for training the model.")
-    ground_truth_data: Features = field("Ground truth data for testing.")
-    predicted_data: Features = field("Predicted anomalies in the data")
     pipeline_name_or_path: str = field(
         "Name of a available pipeline or path to your custom pipeline json",
         default=None,
@@ -58,14 +59,33 @@ class OrionModelContext(ModelContext):
         )
         self.input_features = ["timestamp", "value"]
         self.test_features = ["start", "end"]
+        self.accuracy_features = self.input_features + ["is_anomaly"]
         self.predicted_features = self.test_features + ["severity"]
-        self.model_path = self.parent.config.save_path / self.model._pipeline
+        self.model_name = self.model._pipeline + ".pkl"
+        self.model_path = self.parent.config.save_path / self.model_name
+
+    def _generate_ground_truth(self, test_data: pd.DataFrame) -> pd.DataFrame:
+        is_anomaly_lst = test_data.is_anomaly.values
+        anomalous_idxs = np.where(is_anomaly_lst == True)[0]
+        ground_truth_dict = {key: [] for key in self.test_features}
+        for _, g in groupby(enumerate(anomalous_idxs), lambda x: x[0] - x[1]):
+            group = map(itemgetter(1), g)
+            group = list(map(int, group))
+            start_idx, end_idx = group[0], group[-1]
+            start_ts = test_data.iloc[start_idx].timestamp
+            end_ts = test_data.iloc[end_idx].timestamp
+            ground_truth_dict[self.test_features[0]].append(start_ts)
+            ground_truth_dict[self.test_features[1]].append(end_ts)
+        ground_truth_df = pd.DataFrame(
+            ground_truth_dict, columns=self.test_features
+        )
+        return ground_truth_df
 
     def _load_model(self, type_):
         action = {"acc": "assessing for accuracy", "pred": "prediction"}[type_]
-        if not self.model_path.isfile():
+        if not self.model_path.is_file():
             raise ModelNotTrained(f"Train model before {action}.")
-        self.model = self.model.load(self.model_path)
+        self.model = Orion.load(self.model_path)
 
     async def _generate_dataframe(
         self, sources: Sources, features_list: List[str]
@@ -75,7 +95,9 @@ class OrionModelContext(ModelContext):
         async for data in data_source:
             for feature_name in features_list:
                 data_dict[feature_name].append(data.feature(feature_name))
-        dataframe = pd.DataFrame(data_dict.items(), columns=features_list)
+        dataframe = pd.DataFrame(data_dict, columns=features_list)
+        # print(dataframe.info())
+        # print(len(dataframe))
         return dataframe
 
     async def train(self, sources: Sources):
@@ -88,12 +110,13 @@ class OrionModelContext(ModelContext):
     async def accuracy(self, sources: SourcesContext) -> Accuracy:
         self._load_model("acc")
         test_data = await self._generate_dataframe(
-            sources, self.input_features
+            sources, self.accuracy_features
         )
-        ground_truth_data = await self._generate_dataframe(
-            sources, self.test_features
+        ground_truth_data = self._generate_ground_truth(test_data)
+
+        scores = self.model.evaluate(
+            test_data[self.input_features], ground_truth_data
         )
-        scores = self.model.evaluate(test_data, ground_truth_data)
 
         return Accuracy(scores.accuracy)
 
