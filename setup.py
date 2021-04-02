1ef5a169327d71baecd5eccae83ad4a9999ccad1
spur19 <shauryapuri11@gmail.com>
2021-04-03 00:37:36 +0530

    source: Added Pandas DataFrame
    Co-authored-by: John Andersen <johnandersenpdx@gmail.com>
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- setup.py
+++ setup.py
@@ -91,6 +91,7 @@ setup(
             "df = dffml.source.df:DataFlowSource",
             "op = dffml.source.op:OpSource",
             "dir = dffml.source.dir:DirectorySource",
+            "dataframe = dffml.source.dataframe:DataFrameSource",
             "iris.training = dffml.source.dataset.iris:iris_training.source",
         ],
         "dffml.port": ["json = dffml.port.json:JSON"],
