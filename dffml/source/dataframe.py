da44dc1469116638452d060897980c30c70b439b
spur19 <shauryapuri11@gmail.com>
2021-04-09 12:47:55 +0530

    source:DataFrame:fixed tests

--- dffml/source/dataframe.py
+++ dffml/source/dataframe.py
@@ -16,22 +16,25 @@ class DataFrameSourceContext(BaseSourceContext):
 
         df = self.parent.config.dataframe
 
+        feature_columns = []
+        for col in df.columns:
+            feature_columns.append(col)
+
         # Store feature data
-        feature_columns = self.parent.config.feature_cols
         feature_data = OrderedDict.fromkeys(feature_columns)
         feature_data.update(record.features(feature_columns))
-
         for col in feature_columns:
             df.loc[record.key, col] = feature_data[col]
 
         # Store prediction
         try:
-            prediction = record.prediction("target_name")
-            prediction_columns = self.parent.config.prediction_cols
-            prediction_data = OrderedDict.fromkeys(prediction_columns)
-            prediction_data.update(prediction.dict())
+            for col in self.parent.config.predictions:
 
-            for col in prediction_columns:
+                prediction = record.prediction(col)
+                prediction_columns = self.parent.config.predictions
+                prediction_data = OrderedDict.fromkeys(prediction_columns)
+                prediction_data.update(prediction.dict())
+
                 df.loc[record.key, col] = prediction_data[col]
 
         except KeyError:
@@ -53,10 +56,7 @@ class DataFrameSourceContext(BaseSourceContext):
 @config
 class DataFrameSourceConfig:
     dataframe: "pandas.DataFrame" = field("The pandas DataFrame to proxy")
-    feature_cols: List[str] = field(
-        "Feature columns whose values we have to update"
-    )
-    prediction_cols: List[str] = field(
+    predictions: List[str] = field(
         "Prediction columns whose values we have to update"
     )
 
--- tests/source/test_dataframe.py
+++ tests/source/test_dataframe.py
@@ -18,11 +18,7 @@ class TestDataFrameSource(AsyncTestCase):
         df = pd.DataFrame(mydict)
 
         source = DataFrameSource(
-            DataFrameSourceConfig(
-                dataframe=df,
-                feature_cols=["A", "B", "C"],
-                prediction_cols=["C"],
-            )
+            DataFrameSourceConfig(dataframe=df, predictions=["C"],)
         )
         # Save some data in the source
         await save(
@@ -36,6 +32,7 @@ class TestDataFrameSource(AsyncTestCase):
 
         self.assertIsInstance(records, list)
         self.assertEqual(len(records), 3)
+        self.assertDictEqual(records[0].features(), {"A": 1, "B": 2, "C": 3})
         self.assertDictEqual(records[1].features(), {"A": 4, "B": 5, "C": 6})
         self.assertDictEqual(records[2].features(), {"A": 7, "B": 8, "C": 9})
 
@@ -45,11 +42,7 @@ class TestDataFrameSource(AsyncTestCase):
         df = pd.DataFrame(mydict)
 
         source = DataFrameSource(
-            DataFrameSourceConfig(
-                dataframe=df,
-                feature_cols=["A", "B", "C"],
-                prediction_cols=["C"],
-            )
+            DataFrameSourceConfig(dataframe=df, predictions=["C"],)
         )
         # Save some data in the source
         await save(
@@ -58,16 +51,14 @@ class TestDataFrameSource(AsyncTestCase):
             Record("2", data={"features": {"A": 7, "B": 8, "C": 9}}),
         )
 
-        async with source:
-            async with source() as ctx:
-                await ctx.update(
-                    record=Record(
-                        "2", data={"features": {"A": 15, "B": 16, "C": 14}}
-                    )
-                )
+        await save(
+            source, Record("2", data={"features": {"A": 15, "B": 16, "C": 14}})
+        )
 
-            async with source() as ctx:
-                records = [record async for record in load(source)]
-                self.assertDictEqual(
-                    records[2].features(), {"A": 15, "B": 16, "C": 14}
-                )
+        records = [record async for record in load(source)]
+        self.assertEqual(len(records), 3)
+        self.assertDictEqual(records[0].features(), {"A": 1, "B": 2, "C": 3})
+        self.assertDictEqual(records[1].features(), {"A": 4, "B": 5, "C": 6})
+        self.assertDictEqual(
+            records[2].features(), {"A": 15, "B": 16, "C": 14}
+        )
