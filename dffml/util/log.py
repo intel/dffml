b61edb925b8f45932d09762914803ec2cc413d4b
Saahil Ali <programmer290399@gmail.com>
2021-04-14 11:23:21 +0530

    util:log: Added log_time decorator

--- dffml/util/log.py
+++ dffml/util/log.py
@@ -1,7 +1,11 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2019 Intel Corporation
 """Logging"""
+import time
 import logging
+import asyncio
+import functools
+from contextlib import contextmanager
 
 LOGGER = logging.getLogger(__package__)
 
@@ -36,3 +40,38 @@ def get_download_logger(root_logger):
     logger.addFilter(duplicate_filter)
 
     return logger
+
+
+def log_time(func):
+    """ 
+    Decorator that can take either coroutine or normal function 
+    
+    Found on Stack Overflow from Mikhail Gerasimov, only modified 
+    slightly to fit our needs.
+    - https://stackoverflow.com/a/44176794/10108465
+    - https://creativecommons.org/licenses/by-sa/3.0/
+    """
+
+    logger = LOGGER.getChild("duration_logger")
+
+    @contextmanager
+    def time_it():
+        start_ts = time.time()
+        yield
+        dur = time.time() - start_ts
+        logger.debug("{} took {:.2} seconds".format(func.__name__, dur))
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if not asyncio.iscoroutinefunction(func):
+            with time_it():
+                return func(*args, **kwargs)
+        else:
+
+            async def tmp():
+                with time_it():
+                    return await func(*args, **kwargs)
+
+            return tmp()
+
+    return wrapper
