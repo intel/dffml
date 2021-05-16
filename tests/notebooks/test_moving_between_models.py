d5c0630d5d98af0666e87c99e58d352e3ecdcec4
mHash1m <hashimchaudry23@gmail.com>
2021-05-16 18:08:30 +0500

    docs: notebooks: Creating notebooks in dffml
    Signed-off-by: mHash1m <hashimchaudry23@gmail.com>

--- tests/notebooks/test_moving_between_models.py
+++ tests/notebooks/test_moving_between_models.py
@@ -3,9 +3,14 @@ import unittest
 
 
 class TestNotebook(unittest.TestCase):
+    # Giving testbook the path to notebook
     @testbook("examples/notebooks/moving_between_models.ipynb")
     def test_stdout(tb, self):
+        # Execute specific cells using a range
         tb.execute_cell(range(0, 6))
+        # Using inject function to insert and execute our own code during the execution of the notebook.
+        # Here, we use it to mock the cached_download() call to change the directory of saving the dataset
+        # to be able to gitignore it later on and avoid spam.
         tb.inject(
             """
             data_path = await cached_download(
@@ -15,10 +20,13 @@ class TestNotebook(unittest.TestCase):
             )
             """
         )
+        # Insert and execute an assertion.
         tb.inject(
             "assert 'tests/notebooks/data/wine_quality.csv' in str(data_path)"
         )
+        # Execute the rest of the cells.
         tb.execute_cell(range(8, 21))
+        # Get the output of specific cells and make assertions.
         assert tb.cell_output_text(13) == "1599 1279 320"
         assert "Accuracy1" and "Accuracy2" in tb.cell_output_text(19)
         assert "confidence" and "value" in tb.cell_output_text(21)
