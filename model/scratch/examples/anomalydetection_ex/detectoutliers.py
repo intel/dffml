d235a6f239949a81cf193389a8624611bcdfd1cc
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2021-06-02 07:07:14 +0000

    model: scratch: examples: anomalydetection: Use the AnomalyDetectionAccuracy for example

--- model/scratch/examples/anomalydetection_ex/detectoutliers.py
+++ model/scratch/examples/anomalydetection_ex/detectoutliers.py
@@ -2,6 +2,9 @@ from dffml import Feature, Features
 from dffml.noasync import accuracy, predict, train
 
 from dffml_model_scratch.anomalydetection import AnomalyModel
+from dffml_model_scratch.anomaly_detection_scorer import (
+    AnomalyDetectionAccuracy,
+)
 
 # Configure the model
 
@@ -16,7 +19,8 @@ model = AnomalyModel(
 train(model, "trainex.csv")
 
 # Assess accuracy for test set
-print("Test set F1 score :", accuracy(model, "testex.csv"))
+scorer = AnomalyDetectionAccuracy()
+print("Test set F1 score :", accuracy(model, scorer, "testex.csv"))
 
 # Assess accuracy for training set
-print("Training set F1 score :", accuracy(model, "trainex.csv"))
+print("Training set F1 score :", accuracy(model, scorer, "trainex.csv"))
