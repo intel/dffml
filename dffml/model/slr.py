fb6e9fd5f9b5125c04b3d15d6b422b4d2b91a5e5
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2021-06-16 12:32:42 +0000

    model: slr: Use mse scorer for cli tests

--- dffml/model/slr.py
+++ dffml/model/slr.py
@@ -97,7 +97,8 @@ class SLRModel(SimpleModel):
             -model-predict ans:int:1 \
             -model-directory tempdir \
             -sources f=csv \
-            -source-filename dataset.csv
+            -source-filename dataset.csv \
+            -scorer mse \
         1.0
 
     Make a prediction
