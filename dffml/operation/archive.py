7d4da8f0f4f74fb714564d9ddc74d357d7760f88
Saahil Ali <programmer290399@gmail.com>
2021-07-14 13:48:50 +0530

    operation: archive: Added outputs

--- dffml/operation/archive.py
+++ dffml/operation/archive.py
@@ -1,3 +1,4 @@
+from os import name
 import tarfile
 import zipfile
 import pathlib
@@ -10,15 +11,20 @@ from ..df.types import Definition
 DIRECTORY = Definition(name="directory", primitive="str")
 ZIP_FILE = Definition(name="zip_file", primitive="str")
 TAR_FILE = Definition(name="tar_file", primitive="str")
+OUTPUT_ZIPFILE_PATH = Definition(name="output_zipfile_path", primitive="str")
+OUTPUT_TARFILE_PATH = Definition(name="output_tarfile_path", primitive="str")
+OUTPUT_DIRECTORY_PATH = Definition(
+    name="output_directory_path", primitive="str"
+)
 
 
 @op(
     inputs={"input_directory_path": DIRECTORY, "output_file_path": ZIP_FILE},
-    outputs={},
+    outputs={"output_path": OUTPUT_ZIPFILE_PATH},
 )
 async def make_zip_archive(
     input_directory_path: str, output_file_path: str,
-):
+) -> dict:
     """
     Creates zip file of a directory.
 
@@ -28,19 +34,25 @@ async def make_zip_archive(
         Path to directory to be archived
     output_file_path : str
         Path where the output archive should be saved (should include file name)
+
+    Returns
+    -------
+    dict
+        Path to the output zip file
     """
     with zipfile.ZipFile(output_file_path, "w") as zip:
         for file in pathlib.Path(input_directory_path).rglob("*"):
             zip.write(file, file.name)
+    return {"output_path": output_file_path}
 
 
 @op(
     inputs={"input_file_path": ZIP_FILE, "output_directory_path": DIRECTORY},
-    outputs={},
+    outputs={"output_path": OUTPUT_DIRECTORY_PATH},
 )
 async def extract_zip_archive(
     input_file_path: str, output_directory_path: str,
-):
+) -> dict:
     """
     Extracts a given zip file.
 
@@ -50,18 +62,24 @@ async def extract_zip_archive(
         Path to the zip file
     output_directory_path : str
         Path where all the files should be extracted
+
+    Returns
+    -------
+    dict
+        Path to the directory where the archive has been extracted
     """
     with zipfile.ZipFile(input_file_path, "r") as zip:
         zip.extractall(output_directory_path)
+    return {"output_path": output_directory_path}
 
 
 @op(
     inputs={"input_directory_path": DIRECTORY, "output_file_path": TAR_FILE},
-    outputs={},
+    outputs={"output_path": OUTPUT_TARFILE_PATH},
 )
 async def make_tar_archive(
     input_directory_path: str, output_file_path: str,
-):
+) -> dict:
     """
     Creates tar file of a directory.
 
@@ -71,19 +89,25 @@ async def make_tar_archive(
         Path to directory to be archived as a tarfile.
     output_file_path : str
         Path where the output archive should be saved (should include file name)
+
+    Returns
+    -------
+    dict
+        Path to the created tar file.
     """
     with tarfile.open(output_file_path, mode="x") as tar:
         for file in pathlib.Path(input_directory_path).rglob("*"):
             tar.add(file, file.name)
+    return {"output_path": output_file_path}
 
 
 @op(
     inputs={"input_file_path": TAR_FILE, "output_directory_path": DIRECTORY},
-    outputs={},
+    outputs={"output_path": OUTPUT_DIRECTORY_PATH},
 )
 async def extract_tar_archive(
     input_file_path: str, output_directory_path: str,
-):
+) -> dict:
     """
     Extracts a given tar file.
 
@@ -93,6 +117,12 @@ async def extract_tar_archive(
         Path to the tar file
     output_directory_path : str
         Path where all the files should be extracted
+
+    Returns
+    -------
+    dict
+        Path to the directory where the archive has been extracted
     """
     with tarfile.open(input_file_path, "r") as tar:
         tar.extractall(output_directory_path)
+    return {"output_path": output_directory_path}
