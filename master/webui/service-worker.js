a8e7d74d6b89213674b019548072e585bf84503f
John Andersen <johnandersenpdx@gmail.com>
2021-07-31 03:32:35 +0000

    docs: Sat Jul 31 03:32:35 UTC 2021
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- master/webui/service-worker.js
+++ master/webui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dffml/master/webui/precache-manifest.d97a3f0ad97bd7115df1ded8f057a365.js"
+  "/dffml/master/webui/precache-manifest.d6c457f8447dd0ed8620b5bccbf41562.js"
 );
 
 self.addEventListener('message', (event) => {
