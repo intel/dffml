c6deb90ea966203442c51a521bb0795e25bbf04e
John Andersen <johnandersenpdx@gmail.com>
2021-08-24 10:51:51 -0700

    plugins: Add operations/data
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/plugins.py
+++ dffml/plugins.py
@@ -50,6 +50,7 @@ CORE_PLUGINS += [
     ("feature", "git"),
     ("feature", "auth"),
     ("operations", "binsec"),
+    ("operations", "data"),
     ("operations", "deploy"),
     ("operations", "image"),
     ("operations", "nlp"),
