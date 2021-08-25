3773718e4618ddc9f741ea120cf0f2e62befee3c
John Andersen <johnandersenpdx@gmail.com>
2021-08-25 14:08:18 +0000

    docs: Wed Aug 25 14:08:17 UTC 2021
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '38390a45e',
+    VERSION: '82455794a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- master/webui/service-worker.js
+++ master/webui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dffml/master/webui/precache-manifest.a15e081fb0e758377ae9c8264b21fc49.js"
+  "/dffml/master/webui/precache-manifest.581c4a432cd49842ab8085bbc7b79b4c.js"
 );
 
 self.addEventListener('message', (event) => {
