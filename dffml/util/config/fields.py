da33b1577311902c1cf92bc0e3a0002e589cd795
John Andersen <johnandersenpdx@gmail.com>
2021-10-01 08:59:05 -0700

    util: config: fields: Do not use file source by default
    Fixes: #1182
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/util/config/fields.py
+++ dffml/util/config/fields.py
@@ -1,20 +1,9 @@
-import pathlib
-
-from ...source.file import FileSourceConfig
-from ...source.json import JSONSource
 from ...source.source import Sources
 from ...base import field
 
 
 FIELD_SOURCES = field(
     "Sources for loading and saving",
-    default_factory=lambda: Sources(
-        JSONSource(
-            FileSourceConfig(
-                filename=pathlib.Path("~", ".cache", "dffml.json")
-            )
-        )
-    ),
+    default_factory=lambda: Sources(),
     labeled=True,
-    required=True,
 )
