f7c7172e9a193b684aa066cfd455b8f075a9d1d5
John Andersen <johnandersenpdx@gmail.com>
2021-11-29 11:12:21 -0800

    df: kubernetes: prerun dataflow for pip install and logging for all containers
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/df/kubernetes_execute_pickled_dataflow_with_inputs.py
+++ dffml/df/kubernetes_execute_pickled_dataflow_with_inputs.py
@@ -1,24 +1,38 @@
 import os
 import json
+import time
 import pickle
+import struct
+import asyncio
 import pathlib
 import logging
 
 import dffml
-import dffml.noasync
 
 
-def main():
+async def main():
     # Turn on logging
-    logging.basicConfig(level=logging.DEBUG, filename=os.environ["LOG_FILE"])
+    logging.basicConfig(level=logging.DEBUG)
+    # Connect to output socket if present
+    output_socket_path = os.environ.get("OUTPUT", "")
+    if output_socket_path:
+        output_socket_path = pathlib.Path(output_socket_path)
+        # TODO Use fanotify or inotify
+        while not output_socket_path.is_socket():
+            logging.debug(f"Waiting for socket file {output_socket_path!r}")
+            time.sleep(0.01)
+        _, output_writer = await asyncio.open_unix_connection(
+            output_socket_path
+        )
     # Assume one context is being run and we want the output of that context
     # [(ctx_as_str, {'product': 36})]
     # So use [0] for first context returned and then [1] to select results
-    print(
-        json.dumps(
-            dffml.export(
-                list(
-                    dffml.noasync.run(
+    output = json.dumps(
+        dffml.export(
+            list(
+                [
+                    result
+                    async for result in dffml.run(
                         dffml.DataFlow._fromdict(
                             **json.loads(
                                 pathlib.Path(
@@ -26,18 +40,31 @@ def main():
                                 ).read_text()
                             )
                         ),
-                        [
-                            dffml.Input._fromdict(**item)
-                            for item in json.loads(
-                                pathlib.Path(os.environ["INPUTS"]).read_bytes()
-                            )
-                        ],
+                        (
+                            [
+                                dffml.Input._fromdict(**item)
+                                for item in json.loads(
+                                    pathlib.Path(
+                                        os.environ["INPUTS"]
+                                    ).read_bytes()
+                                )
+                            ]
+                            if os.environ.get("INPUTS", "")
+                            else []
+                        ),
                     )
-                )[0][1]
-            )
+                ]
+            )[0][1]
         )
     )
+    if output_socket_path:
+        output_writer.write(
+            struct.pack("!Q", int(len(output))) + output.encode(),
+        )
+        await output_writer.drain()
+        output_writer.close()
+        await output_writer.wait_closed()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
