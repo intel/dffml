40ad15bbcb8dd1f3581da1b4eb276db4f9654238
John Andersen <johnandersenpdx@gmail.com>
2021-12-08 22:31:28 -0800

    operation: subprocess: Custom definitions
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/operation/subprocess.py
+++ dffml/operation/subprocess.py
@@ -1,10 +1,29 @@
 from typing import List
 
 from ..df.base import op
-from ..util.subprocess import exec_subprocess
+from ..df.types import Definition
+from ..util.subprocess import Subprocess, exec_subprocess
 
 
-@op
+SUBPROCESS_CMD = Definition(name="subprocess.cmd", primitive="List[str]")
+SUBPROCESS_CWD = Definition(
+    name="subprocess.cwd", primitive="str", default=None,
+)
+SUBPROCESS_STDOUT = Definition(name="subprocess.stdout", primitive="str")
+SUBPROCESS_STDERR = Definition(name="subprocess.stderr", primitive="str")
+SUBPROCESS_RETURN_CODE = Definition(
+    name="subprocess.returncode", primitive="int"
+)
+
+
+@op(
+    inputs={"cmd": SUBPROCESS_CMD, "cwd": SUBPROCESS_CWD},
+    outputs={
+        "stdout": SUBPROCESS_STDOUT,
+        "stderr": SUBPROCESS_STDERR,
+        "returncode": SUBPROCESS_RETURN_CODE,
+    },
+)
 async def subprocess_line_by_line(self, cmd: List[str], cwd: str = None):
     output = {"stdout": "", "stderr": "", "returncode": 1}
     async for event, result in exec_subprocess(cmd, cwd=cwd):
