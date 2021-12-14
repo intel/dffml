bc2ee758c55ea068024453e325c837de26e6ec55
John Andersen <john.s.andersen@intel.com>
2021-12-13 22:51:35 -0800

    shouldi: java: dependency check: Download if not present
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- examples/shouldi/shouldi/java/dependency_check.py
+++ examples/shouldi/shouldi/java/dependency_check.py
@@ -1,16 +1,106 @@
-import json
 import os
+import json
+import pathlib
 import tempfile
+import contextlib
 import urllib.parse
 from pathlib import Path
 from typing import Dict, Any
 
-from dffml import op, Definition, run_command
+from dffml import (
+    op,
+    Definition,
+    run_command,
+    config,
+    field,
+    cached_download_unpack_archive,
+    prepend_to_path,
+)
 
 package_src_dir = Definition(name="package_src_dir", primitive="str")
 dependency_check_output = Definition(
     name="dependency_check_output", primitive="Dict[str, Any]"
 )
+
+
+OPENJDK = (
+    "https://download.java.net/java/GA/jdk17.0.1/2a2082e5a09d4267845be086888add4f/12/GPL/openjdk-17.0.1_linux-x64_bin.tar.gz",
+    "884a8ad424ee1cccc20bd338535064f4223c3421eca62d112ddac871c0a8f8e9ce0c6fb1de81239e4c2776105e298d24",
+)
+DEPENDENCY_CHECK = (
+    "https://github.com/jeremylong/DependencyCheck/releases/download/v6.5.0/dependency-check-6.5.0-release.zip",
+    "fae3191f8ca5c8433e9672daef01dee84cfba84dacd0dacb6b73a6016839337929f3e749214924aae0f2dd02a1cf1258",
+)
+
+
+@config
+class DependencyCheckConfig:
+    openjdk_url: str = field(
+        "URL to OpenJDK to run with if java not in PATH", default=OPENJDK[0]
+    )
+    openjdk_hash: str = field(
+        "Hash of OpenJDK to run with if java not in PATH", default=OPENJDK[1]
+    )
+    dependency_check_url: str = field(
+        "URL to OpenJDK to run with if dependency-check.sh not in PATH",
+        default=DEPENDENCY_CHECK[0],
+    )
+    dependency_check_hash: str = field(
+        "Hash of Dependency Check to run with if dependency-check.sh not in PATH",
+        default=DEPENDENCY_CHECK[1],
+    )
+
+
+def path_to_binary(binary):
+    return [
+        binary_path
+        for binary_path in [
+            pathlib.Path(dirname, binary)
+            for dirname in os.environ.get("PATH", "").split(":")
+        ]
+        if binary_path.exists()
+    ]
+
+
+@contextlib.asynccontextmanager
+async def ensure_java(self):
+    java = path_to_binary("java")
+    if java:
+        yield str(java[0].resolve())
+        return
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir_path = pathlib.Path(tempdir)
+        java = await cached_download_unpack_archive(
+            self.config.openjdk_url,
+            tempdir_path.joinpath("java.tar.gz"),
+            tempdir_path.joinpath("java-download"),
+            self.config.openjdk_hash,
+        )
+        java_path = java.joinpath("jdk-17", "bin", "java")
+        with prepend_to_path(java_path):
+            yield str(java_path)
+
+
+@contextlib.asynccontextmanager
+async def ensure_dependency_check(self):
+    dependency_check = path_to_binary("dependency-check.sh")
+    if dependency_check:
+        yield str(dependency_check[0].resolve())
+        return
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir_path = pathlib.Path(tempdir)
+        dependency_check_path = await cached_download_unpack_archive(
+            self.config.dependency_check_url,
+            tempdir_path.joinpath("dependency_check.zip"),
+            tempdir_path.joinpath("dependency_check-download"),
+            self.config.dependency_check_hash,
+        )
+        dependency_check_path = dependency_check_path.joinpath(
+            "dependency-check", "bin", "dependency-check.sh"
+        )
+        with prepend_to_path(dependency_check_path.parent):
+            dependency_check_path.chmod(0o755)
+            yield str(dependency_check_path)
 
 
 class DependencyCheckError(Exception):
@@ -22,6 +112,11 @@ class DependencyCheckError(Exception):
 @op(
     inputs={"pkg": package_src_dir},
     outputs={"report": dependency_check_output},
+    imp_enter={
+        "depenency_check": ensure_dependency_check,
+        "java": ensure_java,
+    },
+    config_cls=DependencyCheckConfig,
 )
 async def run_dependency_check(self, pkg: str) -> Dict[str, Any]:
     """
@@ -60,19 +155,16 @@ async def run_dependency_check(self, pkg: str) -> Dict[str, Any]:
             kwargs["cwd"] = pkg
         # Run command
         try:
-            await run_command(cmd, **kwargs)
+            await run_command(cmd, logger=self.logger, **kwargs)
         except RuntimeError as e:
             raise DependencyCheckError from e
 
-        with open(
-            os.path.join(
-                os.path.abspath(tempdir), "dependency-check-report.json"
-            )
-        ) as f:
-            dependency_check_op = json.loads(f.read())
+        report_contents = pathlib.Path(tempdir, "dependency-check-report.json").read_text()
+        dependency_check_op = json.loads(report_contents)
 
+    t_result = []
     for items in dependency_check_op["dependencies"]:
-        t_result = items["vulnerabilities"]
+        t_result += items["vulnerabilities"]
 
     final_report = {}
     score = 0
@@ -81,5 +173,6 @@ async def run_dependency_check(self, pkg: str) -> Dict[str, Any]:
         final_report["severity"] = item["severity"]
         score += 1
     final_report["total_CVE"] = score
+    final_report["original_report"] = dependency_check_op
 
     return {"report": final_report}
