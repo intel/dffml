7ec34f2d1a9bdacf3341efb147f3d7cfcd5a6713
John Andersen <johnandersenpdx@gmail.com>
2022-01-07 03:36:55 +0000

    docs: Fri Jan  7 03:36:55 UTC 2022
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ master/webui/precache-manifest.2587ddcf774bfdffb84193273404669c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caac587749d0cb15e6231c9e21d64444",
+    "url": "/dffml/master/webui/index.html"
+  },
+  {
+    "revision": "4d5da06ee4cf1a9f7408",
+    "url": "/dffml/master/webui/static/js/2.84970806.chunk.js"
+  },
+  {
+    "revision": "5c957da43e9d0adeb8bb75acf8ca36cc",
+    "url": "/dffml/master/webui/static/js/2.84970806.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53cd2b981da54eb69b0b",
+    "url": "/dffml/master/webui/static/js/main.85eea1a0.chunk.js"
+  },
+  {
+    "revision": "1b58fa33c84c4e9c5341",
+    "url": "/dffml/master/webui/static/js/runtime-main.03e80777.js"
+  }
+]);
--- master/webui/service-worker.js
+++ master/webui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dffml/master/webui/precache-manifest.9ad8ee2b94b63d1819d66f076f9a41c6.js"
+  "/dffml/master/webui/precache-manifest.2587ddcf774bfdffb84193273404669c.js"
 );
 
 self.addEventListener('message', (event) => {
