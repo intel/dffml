ebe9e8ae1f4a31e35c40037e796e6c02f17081a3
John Andersen <johnandersenpdx@gmail.com>
2022-02-18 07:52:48 -0800

    operations: innersource: Switch to checking for presence of workflows dir
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- operations/innersource/dffml_operations_innersource/operations.py
+++ operations/innersource/dffml_operations_innersource/operations.py
@@ -1,10 +1,16 @@
+import pathlib
 from typing import List
 
 import yaml
 
 from dffml.df.base import op
+from dffml_feature_git.feature.definitions import git_repository
 
 
-@op
-async def parse_github_workflow(contents: str) -> dict:
-    return yaml.safe_load(contents)
+@op(
+    inputs={
+        "repo": git_repository,
+    },
+)
+async def github_workflow_present(repo: git_repository.spec) -> dict:
+    return bool(pathlib.Path(repo.directory, ".github", "workflows").is_dir())
