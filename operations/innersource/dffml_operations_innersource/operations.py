57e0f5ac60995e23133f208074c40b9839aa1831
John Andersen <johnandersenpdx@gmail.com>
2022-02-19 20:35:22 -0800

    operations: innersource: Fix tests to clone and check for workflows using git operations
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- operations/innersource/dffml_operations_innersource/operations.py
+++ operations/innersource/dffml_operations_innersource/operations.py
@@ -13,4 +13,4 @@ from dffml_feature_git.feature.definitions import git_repository
     },
 )
 async def github_workflow_present(repo: git_repository.spec) -> dict:
-    return bool(pathlib.Path(repo.directory, ".github", "workflows").is_dir())
+    return pathlib.Path(repo.directory, ".github", "workflows").is_dir()
