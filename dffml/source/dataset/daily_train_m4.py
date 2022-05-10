a4b651cf092c5ca3bc8cf64cc4c23640fb140049
Abhimanyu Dayal <abhimanyudayal1@gmail.com>
2022-05-11 02:22:42 +0530

    d

--- dffml/source/dataset/daily_train_m4.py
+++ dffml/source/dataset/daily_train_m4.py
@@ -27,12 +27,4 @@ async def daily_train_m4(
         
         original_path = cache_dir / "Daily-train.csv"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         yield CSVSource(filename=str(original_path))
-=======
-        yield CSVSource(filename=str(original_path))
->>>>>>> 72e0abc27 (Create daily_train_m4.py)
-=======
-        yield CSVSource(filename=str(original_path))
->>>>>>> 72e0abc27 (Create daily_train_m4.py)
