d8fe1dededa5cc37886234debc68ae230c15caac
John Andersen <johnandersenpdx@gmail.com>
2022-05-20 07:37:25 -0700

    alice: Empty package
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ entities/alice/setup.py
@@ -0,0 +1,8 @@
+import sys
+import site
+import setuptools
+
+# See https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+setuptools.setup(use_scm_version=True)
