31d36b79c9760ab129e9b1334680c3568b15efee
John Andersen <johnandersenpdx@gmail.com>
2022-05-25 14:54:19 -0700

    operation: github: Playing around with operation as dataflow
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/operation/github.py
+++ dffml/operation/github.py
@@ -1,47 +1,9 @@
 import sys
-import importlib
 import dataclasses
 import importlib.util
 
 from ..util.asynctestcase import AsyncTestCase
 from ..util.config.inspect import make_config_inspect
-
-
-@op(
-    imp_enter={
-        "bs4": lambda: importlib.import_module("bs4"),
-    },
-)
-def bs4_parse_html():
-    soup = self.parent.bs4.BeautifulSoup(html_doc, 'html.parser')
-    return soup.pretitify()
-
-
-def operation_for_sync_urlopen(
-    url: Union[str, urllib.request.Request],
-    protocol_allowlist: List[str] = DEFAULT_PROTOCOL_ALLOWLIST,
-    **kwargs,
-):
-    """
-    Check that ``url`` has a protocol defined in ``protocol_allowlist``, then
-    return the result of calling :py:func:`urllib.request.urlopen` passing it
-    ``url`` and any keyword arguments.
-    """
-    validate_protocol(url, protocol_allowlist=protocol_allowlist)
-    return urllib.request.urlopen(url, **kwargs)
-
-GITHUB_DISCUSSION_DEFAULT_DATAFLOW = DataFlow(
-    # Copy from our remap flow
-)
-
-@config
-class GitHubDiscussionConfig:
-    dataflow: DataFlow = field("Default flow", default_factory=lambda: GITHUB_DISCUSSION_DEFAULT_DATAFLOW)
-
-GitHubDiscussion = NewType("GitHubDiscussion", dict)
-
-async def github_discussion(self, ) -> GitHubDiscussion:
-
 
 
 class TestGitHubOperations(AsyncTestCase):
