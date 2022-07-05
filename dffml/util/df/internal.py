f50179415f818dc8778fdf7ca043b8b747aa50e7
John Andersen <johnandersenpdx@gmail.com>
2022-07-05 14:44:01 -0700

    util: df: internal: object to operations: Move to own file
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ dffml/util/df/internal.py
@@ -0,0 +1,29 @@
+import inspect
+
+from ...df.base import op
+
+
+def object_to_operations(obj, module=None):
+    """
+    Takes an object and creates a list of operations for that object, after
+    wrapping any likely targets (functions, methods) with op.
+    """
+    if module is not None:
+        if not inspect.ismodule(module):
+            raise TypeError(f"{module} is not a module")
+        python_path = f"{module.__name__}"
+    elif inspect.ismodule(obj):
+        return object_to_operations(obj, module=obj)
+    else:
+        python_path = f"{obj.__module__}.{obj.__qualname__}"
+    return [
+        op(name=f"{python_path}:{name}")(method)
+        if not hasattr(method, "imp")
+        else method.imp
+        for name, method in inspect.getmembers(
+            obj,
+            predicate=lambda i: inspect.ismethod(i)
+            or inspect.isfunction(i)
+            and not hasattr(i, "__supertype__"),
+        )
+    ]
