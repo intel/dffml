d38955a629af756bb8824cf22b78f3be6e4950c6
John Andersen <johnandersenpdx@gmail.com>
2022-07-27 21:34:28 -0700

    alice: please: contribute: recommended community standards: overlay: github: permissions checks: Refactor into seperate helper function which takes origin
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- entities/alice/alice/please/contribute/recommended_community_standards/alice/operations/github/permissions_checks.py
+++ entities/alice/alice/please/contribute/recommended_community_standards/alice/operations/github/permissions_checks.py
@@ -13,28 +13,32 @@ from typing import Dict, List, Optional, AsyncIterator, NamedTuple, NewType
 
 
 import dffml
+from dffml_feature_git.feature.definitions import git_remote
 
-from ....recommended_community_standards import AliceGitRepo, AlicePleaseContributeRecommendedCommunityStandards
-from .pull_request import AlicePleaseContributeRecommendedCommunityStandardsOverlayGitHubPullRequest
+from ....recommended_community_standards import (
+    AliceGitRepo,
+    AlicePleaseContributeRecommendedCommunityStandards,
+)
+from .pull_request import (
+    AlicePleaseContributeRecommendedCommunityStandardsOverlayGitHubPullRequest,
+)
 
 
-class AlicePleaseContributeRecommendedCommunityStandardsOverlayGitHubWritableRemotesFromPermissions:
-    async def already_owns_repo(
-        self, repo: AliceGitRepo,
-    ) -> AlicePleaseContributeRecommendedCommunityStandardsOverlayGitHubPullRequest.WriteableGitRemoteOrigin:
-        if repo.URL is None:
-            return
-        origins = {}
-        async for event, result in dffml.run_command_events(
-            ["git", "remote", "-v"],
-            cwd=repo.directory,
-            logger=self.logger,
-            events=[dffml.Subprocess.STDOUT_READLINE],
-        ):
-            if event is dffml.Subprocess.STDOUT_READLINE:
-                origin, url_and_usages = result.decode().strip().split("\t", maxsplit=2)
-                origins[origin] = url_and_usages.split()[0]
-        for origin, url in origins.items():
+async def github_owns_remote(
+    self, directory: str, check_remote: str, *, logger=None,
+) -> bool:
+    remotes = {}
+    async for event, result in dffml.run_command_events(
+        ["git", "remote", "-v"],
+        cwd=directory,
+        logger=logger,
+        events=[dffml.Subprocess.STDOUT_READLINE],
+    ):
+        if event is dffml.Subprocess.STDOUT_READLINE:
+            remote, url_and_usages = result.decode().strip().split("\t", maxsplit=2)
+            if remote != check_remote:
+                continue
+            url = url_and_usages.split()[0]
             async for event, result in dffml.run_command_events(
                 [
                     "gh",
@@ -46,12 +50,21 @@ class AlicePleaseContributeRecommendedCommunityStandardsOverlayGitHubWritableRem
                     "-q",
                     ".viewerPermission",
                 ],
-                logger=self.logger,
+                logger=logger,
                 events=[dffml.Subprocess.STDOUT],
             ):
                 result = result.strip().decode()
-                if event is dffml.Subprocess.STDOUT and result in (
-                    "ADMIN",
-                    "MAINTAIN",
-                ):
-                    return origin
+                if event is dffml.Subprocess.STDOUT and result in ("ADMIN", "MAINTAIN"):
+                    return True
+    return False
+
+
+class AlicePleaseContributeRecommendedCommunityStandardsOverlayGitHubWritableRemotesFromPermissions:
+    async def github_owns_remote(
+        self, repo: AliceGitRepo, remote: git_remote,
+    ) -> AlicePleaseContributeRecommendedCommunityStandardsOverlayGitHubPullRequest.WriteableGitRemoteOrigin:
+        if repo.URL is None or not await github_owns_remote(
+            self, repo.directory, remote, logger=self.logger
+        ):
+            return
+        return remote
