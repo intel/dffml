39e864821886a1bf601c356bd743ca0ceffbea6b
John Andersen <johnandersenpdx@gmail.com>
2023-01-08 16:08:41 -0800

    operations: innersource: enable groovy lint: Overlays for resolution of npm-groovy-lint dependencies
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- operations/innersource/dffml_operations_innersource/operations.py
+++ operations/innersource/dffml_operations_innersource/operations.py
@@ -382,9 +382,134 @@ async def actions_validator(
             return bool(result == 0)
 
 
+class _NPM_GROOVY_LINT_USE_DEFAULT_CACHE_DIR:
+    pass
+
+
 NPMGroovyLintBinary = NewType("NPMGroovyLintBinary", str)
+NPMGroovyLintCacheDir = NewType("NPMGroovyLintCacheDir", str)
 NPMGroovyLintResult = NewType("NPMGroovyLintResult", str)
+
+
+NPM_GROOVY_LINT_DEFAULT_BINARY = "npm-groovy-lint"
+NPM_GROOVY_LINT_USE_DEFAULT_CACHE_DIR = _NPM_GROOVY_LINT_USE_DEFAULT_CACHE_DIR()
+NPM_GROOVY_LINT_DEFAULT_CACHE_DIR_PARTS = (
+    ".tools", "open-architecture", "innersource", ".cache", "npm-groovy-lint",
+)
+
+
+@dffml.op
+async def ensure_npm_groovy_lint(
+    cache_dir: NPMGroovyLintCacheDir = NPM_GROOVY_LINT_USE_DEFAULT_CACHE_DIR,
+    *,
+    env: dict = None,
+    logger: logging.Logger = None,
+) -> NPMGroovyLintBinary:
+    # TODO Take node as arg from ensure_node
+    # The location we'll assume the binary is at, its basename, resolved on exec
+    # to determine correct path.
+    npm_groovy_lint_binary_path = pathlib.Path(NPM_GROOVY_LINT_DEFAULT_BINARY)
+    # Bail out if we already have a copy of the binary available in the path,
+    # aka that subprocess -> fork + exec will succeed.
+    if (
+        npm_groovy_lint_binary_path.exists()
+        or dffml.inpath(npm_groovy_lint_binary_path)
+    ):
+        return npm_groovy_lint_binary_path
+    # Store in given cache directory or create default relative to cwd
+    if cache_dir is NPM_GROOVY_LINT_USE_DEFAULT_CACHE_DIR:
+        cache_dir = pathlib.Path(*NPM_GROOVY_LINT_DEFAULT_CACHE_DIR_PARTS)
+    # Download node
+    node_install_path = await dffml.cached_download_unpack_archive(
+        "https://nodejs.org/dist/v14.2.0/node-v14.2.0-linux-x64.tar.xz",
+        cache_dir.joinpath("node.tar.gz"),
+        cache_dir.joinpath("node-download"),
+        "fa2a9dfa4d0f99a0cc3ee6691518c026887677a0d565b12ebdcf9d78341db2066427c9970c41cbf72776a370bbb42729",
+    )
+    # Find the binary for nodejs
+    node_bin_path = [
+        path.parent
+        for path in node_install_path.rglob("node")
+        if path.parent.name == "bin"
+    ][0]
+    with dffml.prepend_to_path(*node_bin_path.resolve().parts, env):
+        # Run npm to install the package with the binary we are wrapping.
+        # Install to the cache dir.
+        # In this case npm-groovy-lint
+        async for event, result in dffml.run_command_events(
+            [
+                "npm",
+                "i",
+                "npm-groovy-lint",
+            ],
+            cwd=cache_dir,
+            env=env,
+            logger=logger,
+        ):
+            pass
+        # Create the path to the binary we installed
+        node_modules_bin_path = cache_dir.joinpath(
+            "node_modules", ".bin",
+        ).resolve()
+        # Add it to the path. Do not resolve because it might be an exec symlink
+        with dffml.prepend_to_path(*node_modules_bin_path.parts, env):
+            pass
+        return node_modules_bin_path.joinpath("npm-groovy-lint")
+
+
+class _JAVA_USE_DEFAULT_CACHE_DIR:
+    pass
+
+
 JavaBinary = NewType("JavaBinary", str)
+JavaCacheDir = NewType("JavaCacheDir", str)
+
+
+JAVA_USE_DEFAULT_CACHE_DIR = _JAVA_USE_DEFAULT_CACHE_DIR()
+JAVA_DEFAULT_CACHE_DIR_PARTS = (
+    ".tools", "open-architecture", "innersource", ".cache", "java",
+)
+
+
+# TODO Move these ensure_ functions into which can then be overlayed as desired
+# via CLI or via install of ad-hoc blank package with only entry_points.txt to
+# enable them as desired.
+# alice_test.shouldi.contribute.bom_v0_0_0
+# This is our reference flow, which knows how to understand, analyize, work in,
+# and rebuild itself from anywhere. This allows us to get developers developing
+# on alice, or any overlays to alice (being anything else you need to work on or
+# analyze). We communicate via ATProto threads to post replys with verifiable
+# credentials where the crendential manifest had a SCITT receipt which itself
+# appears in a SCITT thread, where the root is the root of trust for that SCITT
+# instance.
+@dffml.op
+async def ensure_java(
+    cache_dir: JavaCacheDir = JAVA_USE_DEFAULT_CACHE_DIR,
+    *,
+    env: dict = None,
+    logger: logging.Logger = None,
+) -> JavaBinary:
+    # Store in given cache directory or create default relative to cwd
+    if cache_dir is JAVA_USE_DEFAULT_CACHE_DIR:
+        cache_dir = pathlib.Path(*JAVA_DEFAULT_CACHE_DIR_PARTS)
+    # TODO Take as args
+    java = await dffml.cached_download_unpack_archive(
+        "https://download.java.net/java/GA/jdk19.0.1/afdd2e245b014143b62ccb916125e3ce/10/GPL/openjdk-19.0.1_linux-x64_bin.tar.gz",
+        cache_dir.joinpath("java.tar.gz"),
+        cache_dir.joinpath("java-download"),
+        "ec79c3f085c295876f96d38bfaece0c565ff89152928d71a8b6bf1baf9eda2f27ce6cd857612a4e73540e67c1c0229b5",
+    )
+    # Find the binary
+    java_bin_path = [
+        path.parent
+        for path in java.rglob("java")
+        if path.parent.name == "bin"
+    ][0]
+    with dffml.prepend_to_path(java_bin_path, env):
+        pass
+    return java_bin_path.joinpath("java")
+
+
 CodeNarcServerProc = NewType("CodeNarcServerProc", object)
 CodeNarcServerReturnCode = NewType("CodeNarcServerReturnCode", int)
 
