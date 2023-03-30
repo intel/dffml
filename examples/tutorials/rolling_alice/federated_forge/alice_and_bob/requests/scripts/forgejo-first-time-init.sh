31ba6e1a017b2fd33569d0a59e89f8ca49a229d6
John Andersen <johnandersenpdx@gmail.com>
2023-03-30 14:02:11 -0700

    examples: tutorials: rolling alice: federated forge: alice and bob: requests: scripts: forgejo first time init: Fix sign up wait for CSRF token to become available
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- examples/tutorials/rolling_alice/federated_forge/alice_and_bob/requests/scripts/forgejo-first-time-init.sh
+++ examples/tutorials/rolling_alice/federated_forge/alice_and_bob/requests/scripts/forgejo-first-time-init.sh
@@ -1,14 +1,26 @@
 echo "awaiting-forgejo";
-until curl -I "${FORGEJO_SERVICE_ROOT}" > /dev/null 2>&1; do sleep 5; done;
+until curl -I "${FORGEJO_SERVICE_ROOT}" > /dev/null 2>&1; do sleep 0.1; done;
 
 echo "checking-if-forgejo-need-first-time-init";
 query_params=$(python3 -c 'import sys, urllib.parse, yaml; print(urllib.parse.urlencode(yaml.safe_load(sys.stdin)))' < /usr/src/forgejo-init/requests/init.yaml);
-curl -v -X POST --data-raw "${query_params}" "${FORGEJO_SERVICE_ROOT}";
+curl -v -X POST --data-raw "${query_params}" "${FORGEJO_SERVICE_ROOT}" > /dev/null;
 echo "forgejo-first-time-init-complete";
 
-CSRF_TOKEN=$(curl "${FORGEJO_SERVICE_ROOT}/user/sign_up" | grep csrfToken | awk '{print $NF}' | sed -e "s/'//g" -e 's/,//g');
-query_params=$(sed -e "s/CSRF_TOKEN/\"${CSRF_TOKEN}\"/g" /usr/src/forgejo-init/requests/sign_up.yaml | python3 -c 'import sys, urllib.parse, yaml; print(urllib.parse.urlencode(yaml.safe_load(sys.stdin)))' <);
-curl -v -X POST --data-raw "${query_params}" "${FORGEJO_SERVICE_ROOT}/user/sign_up";
+get_sign_up_crsf_token() {
+  curl "${1}/user/sign_up" | grep csrfToken | awk '{print $NF}' | sed -e "s/'//g" -e 's/,//g'
+}
+
+echo "creating-forgejo-admin-user";
+CSRF_TOKEN=$(get_sign_up_crsf_token "${FORGEJO_SERVICE_ROOT}");
+while [ "x${CSRF_TOKEN}" == "x" ]; do
+  CSRF_TOKEN=$(get_sign_up_crsf_token "${FORGEJO_SERVICE_ROOT}");
+  sleep 0.1;
+done
+query_params=$(
+  sed -e "s/CSRF_TOKEN/\"${CSRF_TOKEN}\"/g" /usr/src/forgejo-init/requests/sign_up.yaml \
+    | python3 -c 'import sys, urllib.parse, yaml; print(urllib.parse.urlencode(yaml.safe_load(sys.stdin)))'
+)
+curl -v -X POST --data-raw "${query_params}" "${FORGEJO_SERVICE_ROOT}/user/sign_up" > /dev/null
 echo "forgejo-user-sign-up-complete";
 
 echo "forgejo-configured";
