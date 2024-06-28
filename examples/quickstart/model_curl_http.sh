76123227329b754172c5e2477ee63df6ad3ee4f1
John Andersen <johnandersenpdx@gmail.com>
2024-06-28 08:32:38 -0700

    housekeeping: temp fix for pinning
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- examples/quickstart/model_curl_http.sh
+++
@@ -1,4 +0,0 @@
-curl http://localhost:8080/model/mymodel/predict/0 \
-  --header "Content-Type: application/json" \
-  --data '{"0": {"features": {"Expertise": 17, "Trust": 0.9, "Years": 8}}}' \
-  | python -m json.tool
